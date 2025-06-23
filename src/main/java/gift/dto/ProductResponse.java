dc9103d704ed0cfdfb13c4f21c8da5835851288a
Hyun <hyun_7586@naver.com>
2025-06-24 07:18:44 +0900

    refactor: DTO class를 record로 변환

--- src/main/java/gift/dto/ProductResponse.java
+++ src/main/java/gift/dto/ProductResponse.java
@@ -5,34 +5,9 @@ package gift.dto;
  이후에 기능 확장, 필드 추가 등을 고려해 미리 분리해 놓는 게 낫다고 판단했습니다.
 */
 
-import gift.domain.Product;
+public record ProductResponse(String name, int price, String imageUrl){
 
-public class ProductResponse {
-  String name;
-  int price;
-  String imageUrl;
-
-  public ProductResponse(String name, int price, String imageUrl){
-    this.name=name;
-    this.price = price;
-    this.imageUrl=imageUrl;
-  }
-
-  public ProductResponse(Product product){
-    this.name=product.getName();
-    this.price= product.getPrice();
-    this.imageUrl= product.getImageUrl();
-  }
-
-  public String getName(){
-    return this.name;
-  }
-
-  public int getPrice(){
-    return this.price;
-  }
-
-  public String getImageUrl(){
-    return this.imageUrl;
+  public static ProductResponse of(String name, int price, String imageUrl){
+    return new ProductResponse(name, price, imageUrl);
   }
 }
