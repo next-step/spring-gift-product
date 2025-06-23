b39c7ade71d2a6886c08b573f6009c1c941dd885
HyeonJun <osumaniaddict527@gmail.com>
2025-06-24 07:25:09 +0900

    feat(product):특정 상품 조회 기능 구현 특정 상품 조회 기능 구현. 엔드포인트와 응답 Dto를 제작

--- src/main/java/gift/domain/product/controller/ProductController.java
+++ src/main/java/gift/domain/product/controller/ProductController.java
@@ -2,11 +2,14 @@ package gift.domain.product.controller;
 
 import gift.domain.product.model.Product;
 import gift.domain.product.dto.AddProductRequest;
+import gift.domain.product.dto.ProductResponse;
 import java.net.URI;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,6 +25,18 @@ public class ProductController {
     @PostMapping
     public ResponseEntity<Void> addProduct(@RequestBody AddProductRequest productRequest) {
         long id = sequence.incrementAndGet();
+        if (productRequest.getPrice() <= 0) {
+            return ResponseEntity.badRequest().build();
+        }
+
+        if (productRequest.getName() == null || productRequest.getName().isBlank()) {
+            return ResponseEntity.badRequest().build();
+        }
+
+        if (productRequest.getImageUrl() == null || productRequest.getImageUrl().isBlank()) {
+            return ResponseEntity.badRequest().build();
+        }
+
         Product product = new Product(id, 
         productRequest.getName(), 
         productRequest.getPrice(), 
@@ -29,5 +44,14 @@ public class ProductController {
         products.put(id, product);
 
         return ResponseEntity.created(URI.create("/api/products/" + id)).build();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductResponse> getProduct(@PathVariable("id") Long id) {
+        Product product = products.get(id);
+        if (product == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(ProductResponse.from(product));
     }
 }
