faed3f61dfa83773f7df697af6e21edda39dbc5a
xodbs126 <dltvmsskd@naver.com>
2025-06-23 15:23:59 +0900

    entity 설계

---
+++ README1.md
@@ -0,0 +1,9 @@
+# spring-gift-product
+
+## Step 0
+
+- Entity 설계
+- Controller 설계
+  - 생성, 삭제, 수정
+- Service 설계
+- Repository 설계
---
+++ src/main/java/gift/Entity/item.java
@@ -0,0 +1,10 @@
+package gift.Entity;
+
+public class item {
+    private Long id;
+    private String name;
+    private Integer price;
+    private String imageUrl;
+
+
+}
