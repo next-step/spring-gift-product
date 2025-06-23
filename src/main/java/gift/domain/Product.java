4e41108bad2a62e24aef6ef71ab372e0bb22674e
thstkddnr20 <thstkddnr20@naver.com>
2025-06-23 20:54:40 +0900

    feat: 상품 도메인 설계 id를 자동으로 증가시키는 static 변수 count에 대하여 테스트 코드 작성

---
+++ src/main/java/gift/domain/Product.java
@@ -0,0 +1,37 @@
+package gift.domain;
+
+public class Product {
+
+    private static Long count = 1L;
+
+    private Long id;
+
+    private String name;
+    private Integer price;
+    private Integer quantity;
+
+    public Product(String name, Integer price, Integer quantity) {
+        this.id = count;
+        count++;
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+    
+}
