72ca140f962b0a503677f98fe4bb38d509ffed47
happyGogildong <rbgus630@gmail.com>
2025-06-23 23:02:06 +0900

    <refactor, fix, style>(api): 저장소 컴포넌트 외부로 분리, 상품 조회 실패시 상태코드 수정, 상품 수정 HTTP 메서드 변경, K & R style 적용

--- README.md
+++ README.md
@@ -6,3 +6,9 @@
 [x] POST /api/product 상품 추가</br>
 [x] PATCH /api/product/{id} 상품 수정</br>
 [x] DELETE /api/product/{id} 상품 삭제
+
+[x] 기능 요구사항에서 제안한 URL이 맞나요? 이번 기회에 RESTful 도 함께 살펴보는 건 어떨까요?
+[x] 어떤 상태코드가 적절한지는 정답이 없답니다. 여러 케이스들을 살펴보고, 어떤 주장들이 있는지, 기업들은 어떻게들 사용하고 있는지 살펴보는 것도 좋겠어요.
+[x] if 조건분기가 있을 때는 brace를 감싸서 개행해주는게 휴먼에러를 방지할 수 있답니다 / patch와 post, put의 차이에 대해 알고 계신가요?
+[x] Spring으로 서비스를 만들어갈 때 지금과 같이 Collection을 Component의 속성으로 두지 않는 이유를 아시나요?
+    
--- src/main/java/gift/Application.java
+++ src/main/java/gift/Application.java
@@ -5,54 +5,84 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.*;
+
+record Product(long id, String name, int price, String imageURL){
+
+}
+
+@Component
+class ProductRepository {
+    final Map<Long, Product> products = new HashMap<>();
+    private long currentId = 1L; //auto_increment용
+
+    public Product get(long id) {
+        return products.get(id);
+    }
+
+    public boolean containsKey(long id) {
+        return products.containsKey(id);
+    }
+
+    public void add(Product product) {
+        products.put(currentId++, product);
+    }
+
+    public void delete(long id) {
+        products.remove(id);
+    }
+
+    public void update(long id, Product product) {
+        products.put(id, product);
+    }
+}
 
 @SpringBootApplication
 public class Application {
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 }
 
 
-record Product(long id, String name, int price, String imageURL){
-
-}
-
 @RestController
 class ProductController {
     //임시 저장소
-    private final Map<Long, Product> products = new HashMap<>();
-    private long currentId = 1L; //auto_increment용
+    private final ProductRepository products;
+
+    public ProductController(ProductRepository products) {
+        this.products = products;
+    }
 
     //상품 조회
-    @GetMapping("/product")
+    @GetMapping("/api/products")
     public ResponseEntity<Product> getProduct(@RequestParam(value = "id", defaultValue = "") long id) {
         return products.containsKey(id) ?
-                new ResponseEntity<>(products.get(id), HttpStatus.OK) : new ResponseEntity<>(HttpStatus.NOT_FOUND);
+                new ResponseEntity<>(products.get(id), HttpStatus.OK) : new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
     //상품 추가
-    @PostMapping("/product")
+    @PostMapping("/api/products")
     public ResponseEntity<Product> createProduct(@RequestBody Product product) {
-        products.put(currentId, product);
-        currentId++;
+        products.add(product);
         return new ResponseEntity<>(product, HttpStatus.CREATED);
     }
 
     //상품 수정
-    @PatchMapping("/product/{id}")
+    @PutMapping("/api/products/{id}")
     public ResponseEntity<Product> updateProduct(@PathVariable long id, @RequestBody Product product) {
-        if (!products.containsKey(id)) return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        products.put(id, product);
+        if (!products.containsKey(id)) {return new ResponseEntity<>(HttpStatus.NOT_FOUND);}
+        products.update(id, product);
         return new  ResponseEntity<>(product, HttpStatus.OK);
     }
 
     //상품 삭제
-    @DeleteMapping("/product/{id}")
+    @DeleteMapping("/api/products/{id}")
     public ResponseEntity<Product> deleteProduct(@PathVariable long id) {
         if (!products.containsKey(id)) return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        products.remove(id);
+        products.delete(id);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
