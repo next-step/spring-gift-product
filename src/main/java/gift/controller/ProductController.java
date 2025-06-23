bd0bdad0f6a34a003862a7ab6bbd40276d6f4cfb
김진현 Kim Jinhyun <kimjhyun0627@gmail.com>
2025-06-23 16:02:32 +0900

    feat: implement GET /api/products
    - 모든 상품 조회 기능 구현

---
+++ src/main/java/gift/controller/ProductController.java
@@ -0,0 +1,28 @@
+package gift.controller;
+
+import gift.model.Product;
+import jakarta.annotation.PostConstruct;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api")
+public class ProductController {
+	private final Map<Long, Product> products = new HashMap<>();
+
+	@PostConstruct
+	public void init() {
+		products.put(8146027L, new Product(8146027L, "아이스 카페 아메리카노 T", 4500, "https://st.kakaocdn.net/product/gift/product/20231010111814_9a667f9eccc943648797925498bdd8a3.jpg"));
+		System.out.println("initialized size: " + products.size());
+	}
+
+	@GetMapping("/products")
+	public Collection<Product> getAllProducts() {
+		return products.values();
+	}
+}
