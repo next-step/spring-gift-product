616eb8b380fd86f5e5ba251cb5ea2137bcdb532f
happyGogildong <rbgus630@gmail.com>
2025-06-23 16:59:38 +0900

    <feat>(api/product/{id}): 상품 수정 기능 추가

--- src/main/java/gift/Application.java
+++ src/main/java/gift/Application.java
@@ -25,16 +25,27 @@ class ProductController {
     private final Map<Long, Product> products = new HashMap<>();
     private long currentId = 1L; //auto_increment용
 
+    //상품 조회
     @GetMapping("/product")
     public ResponseEntity<Product> getProduct(@RequestParam(value = "id", defaultValue = "") long id) {
         return products.containsKey(id) ?
                 new ResponseEntity<>(products.get(id), HttpStatus.OK) : new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
+    //상품 추가
     @PostMapping("/product")
     public ResponseEntity<Product> createProduct(@RequestBody Product product) {
         products.put(currentId, product);
         currentId++;
         return new ResponseEntity<>(product, HttpStatus.CREATED);
     }
+
+    //상품 수정
+    @PatchMapping("/product/{id}")
+    public ResponseEntity<Product> updateProduct(@PathVariable long id, @RequestBody Product product) {
+        if (!products.containsKey(id)) return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        products.put(id, product);
+        return new  ResponseEntity<>(product, HttpStatus.OK);
+    }
+
 }
