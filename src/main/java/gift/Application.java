4b43c8a41464ab40341a19d6e2a9c31e99bec31c
happyGogildong <rbgus630@gmail.com>
2025-06-23 16:52:36 +0900

    <feat>(api/product): 상품 추가 기능 추가

--- src/main/java/gift/Application.java
+++ src/main/java/gift/Application.java
@@ -23,10 +23,18 @@ record Product(long id, String name, int price, String imageURL){
 class ProductController {
     //임시 저장소
     private final Map<Long, Product> products = new HashMap<>();
+    private long currentId = 1L; //auto_increment용
 
     @GetMapping("/product")
     public ResponseEntity<Product> getProduct(@RequestParam(value = "id", defaultValue = "") long id) {
         return products.containsKey(id) ?
                 new ResponseEntity<>(products.get(id), HttpStatus.OK) : new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @PostMapping("/product")
+    public ResponseEntity<Product> createProduct(@RequestBody Product product) {
+        products.put(currentId, product);
+        currentId++;
+        return new ResponseEntity<>(product, HttpStatus.CREATED);
     }
 }
