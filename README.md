7850270438296bf10ad204fd4c6c3f76c80a8d53
happyGogildong <rbgus630@gmail.com>
2025-06-23 16:39:09 +0900

    <feat>(api): 상품 조회 기능 추가

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # spring-gift-product
+
+### 기능
+------
+[ ] GET /api/product 상품 조회</br> 
+[ ] POST /api/product 상품 추가</br>
+[ ] PATCH /api/product/{id} 상품 수정</br>
+[ ] DELETE /api/product/{id} 상품 삭제
--- src/main/java/gift/Application.java
+++ src/main/java/gift/Application.java
@@ -1,11 +1,32 @@
 package gift;
 
+import java.util.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+}
+
+
+record Product(long id, String name, int price, String imageURL){
+
+}
+
+@RestController
+class ProductController {
+    //임시 저장소
+    private final Map<Long, Product> products = new HashMap<>();
+
+    @GetMapping("/product")
+    public ResponseEntity<Product> getProduct(@RequestParam(value = "id", defaultValue = "") long id) {
+        return products.containsKey(id) ?
+                new ResponseEntity<>(products.get(id), HttpStatus.OK) : new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 }
