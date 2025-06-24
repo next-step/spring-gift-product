80c198a89ca72f73f53e10820d81fbfba6db2b6e
surim <163095347+gosurim@users.noreply.github.com>
2025-06-24 20:17:42 +0900

    강원대 BE_고수림 1단계 - 상품 API (#34)
    * docs(README): step1 기능 목록 및 API 명세서 추가
    
    * docs(README): API 명세서 int -> Integer 타입 수정
    
    - 가격 타입을 int에서 Integer로 수정
    - 컬렉션 사용을 고려하여 수정하였음
    
    * feat(product): product 엔티티 및 DTO 클래스 추가
    
    - Product.java: id, name, price, imageUrl 포함
    - ProductRequestDto.java: 클라이언트 요청 처리용
    - ProductResponseDto.java: 클라이언트 응답 처리용
    
    * feat(product): 상품 생성 기능 구현
    
    * feat(product): 상품 목록 조회 기능 구현
    
    * feat(product): 상품 단건 조회 기능 구현
    
    * feat(product): 상품 수정 기능 구현
    
    * feat(product): 상품 삭제 기능 구현
    
    ---------
    
    Co-authored-by: gosurim <su020900@gmail.com>

---
+++ src/main/java/gift/controller/ProductController.java
@@ -0,0 +1,54 @@
+package gift.controller;
+
+import gift.dto.ProductRequestDto;
+import gift.dto.ProductResponseDto;
+import gift.service.ProductService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+    private final ProductService productService;
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    // 생성
+    @PostMapping // 요청
+    public ResponseEntity<ProductResponseDto> createProduct(
+            @RequestBody ProductRequestDto requestDto) {
+        return new ResponseEntity<>(productService.saveProduct(requestDto), HttpStatus.CREATED);
+    }
+
+    // 목록 조회
+    @GetMapping
+    public List<ProductResponseDto> findAllProducts() {
+        return productService.findAllProducts();
+    }
+
+    // 단건 조회
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductResponseDto> findProductById(@PathVariable Long id) {
+        return new ResponseEntity<>(productService.findProductById(id), HttpStatus.OK);
+    }
+
+    // 수정
+    @PutMapping("/{id}")
+    public ResponseEntity<ProductResponseDto> updateProduct(
+            @PathVariable Long id,
+            @RequestBody ProductResponseDto requestDto
+    ) {
+        return new ResponseEntity<>(productService.updateProduct(id, requestDto.name(), requestDto.price(), requestDto.imageUrl()), HttpStatus.OK);
+    }
+
+    // 삭제
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteProduct(@PathVariable Long id) {
+        productService.deleteProduct(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+}
---
+++ src/main/java/gift/repository/ProductRepository.java
@@ -0,0 +1,41 @@
+package gift.repository;
+
+import gift.dto.ProductResponseDto;
+import gift.entity.Product;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Repository
+public class ProductRepository {
+    private final Map<Long, Product> products = new HashMap<>();
+
+    private Long idCounter = 1L;
+
+    public Product saveProduct(Product product) {
+        product.setId(idCounter++);
+        products.put(product.getId(), product);
+        return product;
+    }
+
+    public List<ProductResponseDto> findAllProducts() {
+        List<ProductResponseDto> allProducts = new ArrayList<>();
+        for (Product product : products.values()) {
+            ProductResponseDto responseDto = new ProductResponseDto(product);
+            allProducts.add(responseDto);
+        }
+
+        return allProducts;
+    }
+
+    public Product findProductById(Long id) {
+        return products.get(id);
+    }
+
+    public void deleteProduct(Long id) {
+        products.remove(id);
+    }
+}
