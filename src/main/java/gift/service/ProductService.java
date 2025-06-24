a13f5195564367bfadce5388a519f4bbb42ae8aa
WonJuN Sun <swj010324@naver.com>
2025-06-24 17:06:05 +0900

    feat(service): implement ProductService, ProductServiceImpl

--- src/main/java/gift/service/ProductService.java
+++ src/main/java/gift/service/ProductService.java
@@ -1,2 +1,15 @@
-package main.java.gift.service;public interface ProductService {
+package gift.service;
+
+import gift.dto.ProductRequestDto;
+import gift.dto.ProductResponseDto;
+
+import java.util.List;
+
+public interface ProductService {
+
+    ProductResponseDto createProduct(ProductRequestDto productRequestDto);
+    List<ProductResponseDto> searchAllProducts();
+    ProductResponseDto searchProductById(Long id);
+    ProductResponseDto updateProduct(Long id, ProductRequestDto productRequestDto);
+    void deleteProduct(Long id);
 }
