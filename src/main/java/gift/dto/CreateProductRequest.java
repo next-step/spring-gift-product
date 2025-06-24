9b27f28dee7975b19e5d9c33e0a87448f701e6c1
jimin Choi <138632648+ji-mim@users.noreply.github.com>
2025-06-24 22:20:51 +0900

    Step1 기능 구현을 완료 했습니다. (#23)
    * Add README.md
    
    * Add Product domain
    
    * Feat save product and find product
    
    Product를 저장하고 조회하는 기능을 추가했습니다. Proudct를 조회할 때 존재하지 않는 id를 조회할 경우 에러를 발생시키고 에러 내용을 클라이언트에게 전달할 수 있도록 GlobalExceptionHandler를 추가했습니다.
    
    * Feat findAll, update, delete Product
    
    상품 목록을 조회하고 수정하고 삭제하는 기능을 추가했습니다. 이에 따른 DTO를 추가했습니다.
    service 레이어에서 레포지토리에 id를 통해 상품이 존재하는지 확인하는 검증 로직을 메서드로 분리했습니다.
    delete 요청에 대한 응답으로 204 No Content 응답을 사용했습니다.
    
    * Refactor: change Product's name to ProductName
    
    객체 지향 생활 체조 원칙 중 모든 원시 값과 문자열을 포장해야 한다는 규칙 3번이 무슨 의미인지 이해되지 않았는데, 도메인 맥락에서 의미 있는 변수는 클래스로 포장해서 책임을 갖게 하는 것이 아닐까 생각돼 Product의 name 변수를 ProductName이라는 클래스를 만들어 포장해봤습니다. 그리고 이 클래스의 생성자에 name에 빈 값이 들어갈 수 없도록 검증 로직을 추가해보았습니다. 이렇게 하니 Product의 name에 빈값이 들어갈 수 없도록 검증 로직이 정상적으로 작동되었지만 update 시 remove가 먼저 실행되어 검증에 실패하면 해당 상품만 삭제되는 상황이 발생해 순서를 바꾸었습니다.

---
+++ src/main/java/gift/dto/CreateProductRequest.java
@@ -0,0 +1,28 @@
+package gift.dto;
+
+public class CreateProductRequest {
+
+    private final String name;
+    private final int price;
+    private final String imageUrl;
+
+    public CreateProductRequest(String name, int price, String imageUrl) {
+        this.name = name;
+        this.price = price;
+        this.imageUrl = imageUrl;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+}
