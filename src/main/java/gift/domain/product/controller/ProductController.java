cd695be88125ad0f828d74932aaad96eb5645e85
최현준 <osumaniaddict527@gmail.com>
2025-06-24 22:48:24 +0900

    강원대 BE_ 최현준 1단계 - 상품 API (#14)
    * docs(README) : README 문서 수정
    기능을 구현하기 전 README.md에 구현할 기능 목록을 정리해 추가하였다.
    
    * feat(product): 상품 추가 기능 구현
    
    * feat(product):특정 상품 조회 기능 구현
    특정 상품 조회 기능 구현. 엔드포인트와 응답 Dto를 제작
    
    * feat(ProductController):상품 전체 조회 기능 구현
    
    * chore(http):API 테스트를 위한 HTTP 요청 파일 추가
    Rest 클라이언트를 통해 상품 api 테스트를 쉽게 하기 위해 http 요청 및
    환경 설정 파일을 추가하였음.
    
    * docs(README): 상품 목록 조회 기능 설명 수정
    
    * feat(product): 상품 수정 API 추가 및 요청 검증 리팩터링
    
    - 상품 정보를 수정하는 `updateProduct` 엔드포인트 추가
    - 기존 `AddProductRequest` DTO를 `ProductRequest`로 대체하여 요청 데이터 클래스 통합
    - 중복된 요청 검증 로직을 `isValidProductRequest` 메서드로 재사용 가능하게 리팩터링
    
    * feat(product): 상품 수정 및 삭제 기능 추가, 요청 URL 관리 리팩터링
    
    - 상품 정보를 수정하는 `updateProduct` API 기능 추가
    - 상품 삭제를 위한 `deleteProduct` API 기능 추가
    - 요청 검증 로직 `isValidProductRequest`로 통합
    - HTTP 클라이언트 URL을 환경 변수로 관리하도록 리팩터링 (`http-client.env.json` 변경)
    - 기존 DTO를 `ProductRequest`로 통일하며 중복 제거 및 코드 간소화
    
    Closes #123
    
    * feat(product): 상품 수정 기능 추가 및 요청 검증 리팩터링
    - API 응답 코드 변경: 수정 완료 시 `204 No Content` 반환. REST API 설계 원칙에 더 부합하도록 HttpCode를 변경
    
    * refact(product): 요청 검증 로직 리팩터링
    - 요청 검증 로직을 `isValidProductRequest` 메서드를 반전 시켜 리팩토링
    
    * test(product): 상품 컨트롤러 테스트 추가
    
    - 상품 추가, 조회, 수정, 삭제에 대한 테스트 케이스 작성
    - `MockMvc`와 `ObjectMapper`를 활용한 REST API 호출 및 응답 검증
    - 각 테스트는 `ProductRequest` 객체를 사용해 데이터 생성 및 확인
    
    * feat(product): 개선된 예외 처리 및 요청 검증 도입
    - 기존의 요청 검증 로직 제거 및 리팩토링
    - 전역 예외 처리 핸들러 `GlobalExceptionHandler` 추가
    - ErrorCode,ErrorField Enum, ErrorDetail,ErrorResponse 클래스 구현
    - `jakarta.validation` 의존성 추가 및 `@Valid` 적용
    - `ProductRequest`에 유효성 검증 어노테이션(`@NotBlank`, `@NotNull`, `@Min`) 추가
    
    * test(exception): 글로벌 예외 처리 테스트 케이스 추가
    
    - `GlobalExceptionHandler`에 대한 통합 테스트 케이스 추가
      - 유효성 검사 예외 (MethodArgumentNotValidException)
      - 잘못된 JSON 처리 (HttpMessageNotReadableException)
      - IllegalArgumentException 처리
      - 필수 파라미터 누락 (MissingServletRequestParameterException)
      - 처리되지 않은 일반 예외 (Exception)
      - (refactor)`createErrorResponse` 메서드의 명명 규칙 업데이트 (field -> errorType)
    
    * refactor(exception): GlobalExceptionHandler 개선
    
    - `GlobalExceptionHandler` 클래스를 `ResponseEntityExceptionHandler`를 확장하도록 리팩터링

---
+++ src/main/java/gift/domain/product/controller/ProductController.java
@@ -0,0 +1,87 @@
+package gift.domain.product.controller;
+
+import gift.domain.product.model.Product;
+import gift.domain.product.dto.ProductRequest;
+import gift.domain.product.dto.ProductResponse;
+import jakarta.validation.Valid;
+import java.net.URI;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+
+    private final Map<Long, Product> products = new ConcurrentHashMap<>();
+    private final AtomicLong sequence = new AtomicLong();
+
+    @PostMapping
+    public ResponseEntity<Void> addProduct(@Valid @RequestBody ProductRequest productRequest) {
+        long id = sequence.incrementAndGet();
+
+        Product product = new Product(id, 
+        productRequest.getName(), 
+        productRequest.getPrice(), 
+        productRequest.getImageUrl());
+        products.put(id, product);
+
+        return ResponseEntity.created(URI.create("/api/products/" + id)).build();
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ProductResponse>> getProducts() {
+        List<ProductResponse> productResponses = products.values().stream()
+            .map(ProductResponse::from)
+            .toList();
+        if (productResponses.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok(productResponses);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductResponse> getProduct(@PathVariable("id") Long id) {
+        Product product = products.get(id);
+        if (product == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(ProductResponse.from(product));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> updateProduct(@PathVariable("id") Long id,
+    @Valid @RequestBody ProductRequest productRequest) {
+        Product product = products.get(id);
+
+        if (product == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        products.put(id, new Product(id, 
+        productRequest.getName(), 
+        productRequest.getPrice(), 
+        productRequest.getImageUrl()));
+        
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteProduct(@PathVariable("id") Long id) {
+        Product removedProduct = products.remove(id);
+        if (removedProduct == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.noContent().build();
+    }
+}
