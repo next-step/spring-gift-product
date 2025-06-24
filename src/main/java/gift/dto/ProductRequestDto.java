642c2c859a99534d1f01004b95bf499494d73b30
Yunjae <50546745+YUNJAEGOONER@users.noreply.github.com>
2025-06-25 02:57:45 +0900

    부산대 BE_이윤재 1단계 - 상품 API (#21)
    * add controller
    
    * feat:create product class
    
    * feat:create product
    
    * feat:전체상품조회 기능 구현
    
    * feat:id를 통해 특정 상품을 조회하는 기능 구현
    
    * docs: 구현을 위한 API문서 작성
    
    * refactor: ModelAttribute -> RequestBody
    
    * style: Google Java Style Guide 적용
    
    * feat: 상품 수정 기능 구현
    
    * feat: 특정 상품을 삭제하는 기능 구현
    
    * refactor: product의 null값을 확인하는 함수 작성
    
    * refactor: id를 활용하여 상품을 찾는 공통 동작에 대한 함수 작성
    
    * refactor: 특정 상품 조회 시, ResponseEntity로 반환하도록 수정
    
    * refactor: 전체 상품 조회 시, ResponseEntity로 반환하도록 수정
    
    * refactor:상품 수정 시, ResponseEntity로 반환하도록 수정
    
    * refactor:상품 삭제 시, ResponseEntity로 반환하도록 수정
    
    ---------
    
    Co-authored-by: YunjaeLee <yunjaelee@YunjaeLeeui-MacBookAir.local>

---
+++ src/main/java/gift/dto/ProductRequestDto.java
@@ -0,0 +1,23 @@
+package gift.dto;
+
+public class ProductRequestDto {
+
+    // private Long id; auto_increment
+    private String name;
+    private Integer price;
+    private String imageUrl;
+
+    //getter
+    public String getName() {
+        return name;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+}
