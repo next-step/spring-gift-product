ec521515cca5a682a00c6a9041004520ca83e800
Junhee <dkfl7190@naver.com>
2025-06-24 12:12:31 +0900

    feat: 상품 단건 조회 기능 구현

--- src/main/java/gift/controller/ProductController.java
+++ src/main/java/gift/controller/ProductController.java
@@ -7,6 +7,8 @@ import java.util.HashMap;
 import java.util.Map;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -34,6 +36,17 @@ public class ProductController {
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .body(new ProductResponseDto(product));
+    }
+
+    // 상품 단건 조회
+    @GetMapping("/{productId}")
+    public ResponseEntity<ProductResponseDto> findProduct(@PathVariable long productId) {
+        Product product = products.get(productId);
+        if (product == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+
+        return ResponseEntity.ok(new ProductResponseDto(product));
     }
 
 }
