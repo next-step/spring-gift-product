edab674fae6af2de16defc897a924d4c37b4ed35
Lmmhhhh <lmh10087215@gmail.com>
2025-06-25 02:01:10 +0900

    feat: 상품 등록 API 구현

---
+++ src/main/java/gift/controller/ProductController.java
@@ -0,0 +1,27 @@
+package gift.controller;
+
+import gift.domain.Product;
+import gift.dto.request.ProductRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.*;
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+
+    private final Map<Long, Product> products = new HashMap<>();
+    private final AtomicLong idgen = new AtomicLong(1);
+
+    @PostMapping
+    public ResponseEntity<Product> register(@RequestBody ProductRequest request) {
+        Long id = idgen.getAndIncrement();
+        Product product = new Product(id, request.getName(), request.getPrice(), request.getImageUrl());
+        products.put(id, product);
+        return ResponseEntity
+                .created(null)
+                .body(product);
+    }
+}
