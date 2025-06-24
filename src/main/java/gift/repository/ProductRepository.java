be0fc608ceb92695dd047b2d25d5d704de586b1c
hayun <127305788+dev-sohyeon@users.noreply.github.com>
2025-06-24 23:24:44 +0900

    경북대 BE_ 박소현 1단계 - 상품 API (#13)
    * docs : 구현할 기능 목록 및 API 명세서
    
    * feat: 상품 추가 기능 및 예외 처리 구현
    
    * feat: 상품 수정 기능 및 예외 처리 구현
    
    * feat: 상품 조회 기능 구현
    
    * feat: 상품 삭제 기능 구현

---
+++ src/main/java/gift/repository/ProductRepository.java
@@ -0,0 +1,40 @@
+package gift.repository;
+
+import gift.entity.Product;
+import org.springframework.stereotype.Repository;
+
+import java.util.*;
+import java.util.concurrent.atomic.AtomicLong;
+
+@Repository
+public class ProductRepository {
+
+    private static final Map<Long, Product> store = new HashMap<>();
+    private static final AtomicLong sequence = new AtomicLong(0L);
+
+    public Product save(Product product) {
+        product.setId(sequence.incrementAndGet());
+        store.put(product.getId(), product);
+        return product;
+    }
+
+    public Optional<Product> findById(Long id) {
+        return Optional.ofNullable(store.get(id));
+    }
+
+    public void update(Long id, Product updateParam) {
+        Product findProduct = findById(id).get();
+        findProduct.setName(updateParam.getName());
+        findProduct.setPrice(updateParam.getPrice());
+        findProduct.setImageURL(updateParam.getImageURL());
+    }
+
+    public List<Product> findAll() {
+        return new ArrayList<>(store.values());
+    }
+
+    public void delete(Long id) {
+        store.remove(id);
+    }
+
+}
---
+++ src/main/java/gift/service/ProductService.java
@@ -0,0 +1,79 @@
+package gift.service;
+
+import gift.dto.ProductRequest;
+import gift.dto.ProductResponse;
+import gift.entity.Product;
+import gift.exception.InvalidProductException;
+import gift.repository.ProductRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+@Service
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public ProductResponse addProduct(ProductRequest request) {
+        validate(request);
+
+        Product product = request.toEntity();
+        Product savedProduct = productRepository.save(product);
+        return ProductResponse.from(savedProduct);
+    }
+
+    public ProductResponse updateProduct(Long id, ProductRequest request) {
+        productRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 ID의 상품을 찾을 수 없습니다."));
+
+        validate(request);
+
+        Product updateParam = request.toEntity();
+        productRepository.update(id, updateParam);
+
+        Product updatedProduct = productRepository.findById(id).get();
+        return ProductResponse.from(updatedProduct);
+    }
+
+    public List<ProductResponse> findAllProducts() {
+        List<Product> products = productRepository.findAll();
+        return products.stream()
+                .map(ProductResponse::from)
+                .collect(Collectors.toList());
+    }
+
+    public ProductResponse findProductById(Long id) {
+        Product product = productRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 ID의 상품을 찾을 수 없습니다."));
+
+        return ProductResponse.from(product);
+    }
+
+    private void validate(ProductRequest request) {
+        if (request.getName() == null || request.getName().trim().isEmpty()) {
+            throw new InvalidProductException("상품 이름은 필수입니다.");
+        }
+
+        if (request.getPrice() <= 0) {
+            throw new InvalidProductException("상품 가격은 1원 이상이어야 합니다.");
+        }
+
+        if (request.getImageURL() == null || request.getImageURL().trim().isEmpty()) {
+            throw new InvalidProductException("상품 이미지 URL은 필수입니다.");
+        }
+
+    }
+
+    public void deleteProduct(Long id) {
+        productRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 ID의 상품을 찾을 수 없습니다."));
+
+        productRepository.delete(id);
+    }
+}
