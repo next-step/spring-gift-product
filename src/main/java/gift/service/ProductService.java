759723c9a9ab4cdce08ad5299f81e5f0db5600ca
jihwan38 <sss203800@gmail.com>
2025-06-25 08:54:25 +0900

    feat: Service 레이어 구현

---
+++ src/main/java/gift/service/ProductService.java
@@ -0,0 +1,61 @@
+package gift.service;
+
+import gift.dto.ProductRequestDto;
+import gift.dto.ProductResponseDto;
+import gift.entity.Product;
+import gift.repository.ProductRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class ProductService {
+    private final ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository){
+        this.productRepository = productRepository;
+    }
+
+    public ProductResponseDto addProduct(ProductRequestDto requestDto){
+        Product product = requestDto.toEntity();
+        Product savedProduct = productRepository.save(product);
+        return new ProductResponseDto(savedProduct);
+    }
+
+    public List<ProductResponseDto> getProducts(){
+        List<Product> products = productRepository.findAll();
+
+        List<ProductResponseDto> responseDtos = new ArrayList<>();
+
+        for(Product product : products){
+            ProductResponseDto dto = new ProductResponseDto(product);
+            responseDtos.add(dto);
+        }
+        return responseDtos;
+    }
+
+    public ProductResponseDto getProduct(Long id){
+        Product product = productRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 상품이 없습니다. id = " + id));
+        return new ProductResponseDto(product);
+    }
+    public ProductResponseDto updateProduct(Long id, ProductRequestDto requestDto) {
+        Product product = productRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 상품이 없습니다. id=" + id));
+
+        product.update(
+                requestDto.getName(),
+                requestDto.getPrice(),
+                requestDto.getImageUrl()
+        );
+        return new ProductResponseDto(product);
+    }
+
+    public void deleteProduct(Long id) {
+        productRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 상품이 없습니다. id=" + id));
+        productRepository.deleteById(id);
+    }
+}
+
