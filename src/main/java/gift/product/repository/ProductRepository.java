8251b634c7b84e804030e1e664cac7b956d253d4
milk-stone <ywshero@naver.com>
2025-06-24 22:44:12 +0800

    feat(repository): 인 메모리 환경에 상품 등록 후, 등록 완료 여부를 반환하는 상품 정보 저장 메서드 구현

---
+++ src/main/java/gift/product/repository/ProductRepository.java
@@ -0,0 +1,18 @@
+package gift.product.repository;
+
+import gift.product.Product;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ProductRepository {
+    private final Map<Long, Product> products = new HashMap<>();
+
+    public boolean save(Product product){
+        if (products.containsKey(product.getId())){
+            return false;
+        }
+        products.put(product.getId(), product);
+        return true;
+    }
+}
