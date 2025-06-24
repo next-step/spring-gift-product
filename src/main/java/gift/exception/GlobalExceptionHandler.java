84735bb8e396adcd9760cbac5f440be6013aa7cd
seongwon02 <seongwon2303@gmail.com>
2025-06-24 21:03:33 +0900

    feat: 상품 단건 조회 API 구현

---
+++ src/main/java/gift/exception/GlobalExceptionHandler.java
@@ -0,0 +1,23 @@
+package gift.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResponseStatusException.class)
+    public ResponseEntity<Map<String, String>>
+    handleResponseStatusError(ResponseStatusException e){
+        Map<String, String> errors = new HashMap<>();
+        errors.put("message", e.getReason());
+
+        return ResponseEntity.status(e.getStatusCode()).body(errors);
+    }
+
+}
--- src/main/java/gift/repository/ProductRepository.java
+++ src/main/java/gift/repository/ProductRepository.java
@@ -7,4 +7,5 @@ import java.util.List;
 public interface ProductRepository {
 
     List<ProductResponseDto> findAllProducts();
+    ProductResponseDto findProductByIdElseThrow(Long id);
 }
