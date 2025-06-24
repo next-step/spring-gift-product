e04456e623734e6652225f65433ca6bffe58c37e
Junhee <68316269+jjunii@users.noreply.github.com>
2025-06-24 23:36:25 +0900

    경북대 BE_박준희 1단계 - 상품 API (#48)
    * docs(readme): 기능 목록 추가
    
    * feat: 상품 생성 기능 구현
    
    * feat: 상품 단건 조회 기능 구현
    
    * feat: 상품 수정 기능 구현
    
    * feat: 상품 삭제 기능 구현
    
    * feat: 상품 목록 조회 기능 구현

---
+++ src/main/java/gift/controller/ProductController.java
@@ -0,0 +1,95 @@
+package gift.controller;
+
+import gift.dto.ProductRequestDto;
+import gift.dto.ProductResponseDto;
+import gift.entity.Product;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+
+    // 임시 DB
+    private final Map<Long, Product> products = new HashMap<>();
+
+    // id값
+    private long nextId = 1;
+
+    // 상품 생성
+    @PostMapping
+    public ResponseEntity<ProductResponseDto> createProduct(
+            @RequestBody ProductRequestDto productRequestDto) {
+        long productId = nextId++;
+        Product product = new Product(productId, productRequestDto.getName(),
+                productRequestDto.getPrice(), productRequestDto.getImageUrl());
+        products.put(productId, product);
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(new ProductResponseDto(product));
+    }
+
+    // 상품 단건 조회
+    @GetMapping("/{productId}")
+    public ResponseEntity<ProductResponseDto> findProduct(@PathVariable long productId) {
+        Product product = products.get(productId);
+        if (product == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+
+        return ResponseEntity.ok(new ProductResponseDto(product));
+    }
+
+    // 상품 수정
+    @PutMapping("/{productId}")
+    public ResponseEntity<ProductResponseDto> updateProduct(
+            @PathVariable long productId,
+            @RequestBody ProductRequestDto productRequestDto) {
+
+        Product product = products.get(productId);
+        if (product == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+
+        product.update(
+                productRequestDto.getName(),
+                productRequestDto.getPrice(),
+                productRequestDto.getImageUrl()
+        );
+
+        return ResponseEntity.ok(new ProductResponseDto(product));
+    }
+
+    // 상품 삭제
+    @DeleteMapping("/{productId}")
+    public ResponseEntity<Void> deleteProduct(@PathVariable long productId) {
+        Product removed = products.remove(productId);
+        if (removed == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
+    // 상품 목록 조회
+    @GetMapping
+    public ResponseEntity<List<ProductResponseDto>> findAllProducts() {
+        List<ProductResponseDto> productsList = products.values().stream()
+                                                        .map(ProductResponseDto::new)
+                                                        .toList();
+        return ResponseEntity.ok(productsList);
+    }
+
+}
