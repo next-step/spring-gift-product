40222df61a975756c62a2e4a5d7bce69da2da4da
milk-stone <ywshero@naver.com>
2025-06-24 22:43:21 +0800

    feat(controller): 상품 등록 API PostMapping 엔드포인트 구현

---
+++ src/main/java/gift/product/controller/ProductController.java
@@ -0,0 +1,26 @@
+package gift.product.controller;
+
+import gift.product.dto.ProductRequest;
+import gift.product.dto.ProductResponse;
+import gift.product.service.ProductService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @PostMapping
+    public ResponseEntity<ProductResponse> createProduct(@RequestBody ProductRequest productRequest) {
+        return productService.addProduct(productRequest);
+    }
+
+
+
+}
