9077b37f9ab9f6024c2a2e10554b33fc10654a66
BSY3510 <baeksy3510@gmail.com>
2025-06-24 15:31:17 +0900

    feat(product): add Product record for data modeling

--- src/main/java/gift/Product.java
+++ src/main/java/gift/Product.java
@@ -1,4 +1,8 @@
 package gift;
 
-public record Product() {
+public record Product(Long id, String name, Integer price, String imageUrl) {
+
+    public String showProductInfo() {
+        return "상품명: %s, 가격: %d".formatted(name, price);
+    }
 }
