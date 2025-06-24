b1de6da42457521424a554c712605ab84e855e26
kimpro4214 <qnfghk555@naver.com>
2025-06-24 14:56:01 +0900

    feat: implement POST /api/products

--- src/main/java/gift/controller/ProductController.java
+++ src/main/java/gift/controller/ProductController.java
@@ -1,8 +1,10 @@
 package gift.controller;
 
 import gift.entity.Product;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
 import java.util.*;
 
 @RestController
@@ -10,14 +12,20 @@ import java.util.*;
 public class ProductController {
 
     private final Map<Long, Product> products = new HashMap<>();
-
-    public ProductController() {
-
-    }
+    private long nextId = 1;
 
     @GetMapping
     public List<Product> getProducts() {
         return new ArrayList<>(products.values());
     }
-}
 
+    @PostMapping
+    public ResponseEntity<Void> addProduct(@RequestBody Product product) {
+        long id = nextId++;
+        Product newProduct = new Product(id, product.getName(), product.getPrice(), product.getImageUrl());
+        products.put(id, newProduct);
+
+        // 201 Created + Location 헤더 설정
+        return ResponseEntity.created(URI.create("/api/products/" + id)).build();
+    }
+}
