61c7706a1ce9d2dc2cc1d664406f8bca21db6d1f
eastgyu11 <eastgyu11>
2025-06-24 16:18:31 +0900

    feat: 상품 등록 기능 추가

--- src/main/java/gift/controller/ProductController.java
+++ src/main/java/gift/controller/ProductController.java
@@ -9,6 +9,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,5 +28,11 @@ public class ProductController {
   public ResponseEntity<Product> findProductById(@PathVariable Long id){
     Product findByIdProduct=productsMap.get(id);
     return new ResponseEntity<>(findByIdProduct,HttpStatus.OK);
+  }
+
+  @PostMapping("/products")
+  public ResponseEntity<Product> createProduct(@RequestBody Product product){
+    productsMap.put(product.getId(), product);
+    return new ResponseEntity<>(product,HttpStatus.OK);
   }
 }
