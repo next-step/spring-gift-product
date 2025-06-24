b4c302b71732c109fa6906a686c2116bb4affc13
Yunseong <dev.yunseong@gmail.com>
2025-06-24 14:26:08 +0900

    feature ProductRepository interface and MemoryProductRepository(implemented to use memory) and custemed Exception for not found

--- src/main/java/gift/repository/ProductRepository.java
+++ src/main/java/gift/repository/ProductRepository.java
@@ -1,2 +1,15 @@
-package gift.repository;public class ProductRepository {
+package gift.repository;
+
+import gift.entity.Product;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ProductRepository {
+    Long saveProduct(String name, Integer price, String imageUrl);
+    Product findProductById(Long id);
+    void deleteProductById(Long id);
+    void updateProduct(Product product);
+    List<Product> findAllProducts();
 }
