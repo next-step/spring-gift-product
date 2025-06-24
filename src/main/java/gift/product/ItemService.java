fb6e50cce6d5daf66a1002729ab44f5a5115aa06
Lee Si Woong <silverttthin@naver.com>
2025-06-24 16:39:21 +0900

    feat: 원활한 테스트 위해 목업데이터 항상 2개는 서버 실행마다 채워놓기 - 처음엔 메인함수에서 서비스 객체 만들어서 실행시켜놨음 - 근데 그건 스프링빈 서비스 객체와 별개라 진짜 독립적인 db임 - 따라서 PostConstruct로 빈 DI 직후 초기화 작업에서 실행되게 세팅

--- src/main/java/gift/product/ItemService.java
+++ src/main/java/gift/product/ItemService.java
@@ -1,6 +1,7 @@
 package gift.product;
 
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -11,6 +12,13 @@ import java.util.List;
 public class ItemService {
 
 	private final List<Item> db = new ArrayList<>();
+
+	// 테스트위해 서버 재실행마다 항상 2개는 채워놓기
+	@PostConstruct
+	public void init() {
+		db.add(new Item("egg", 7900, "url1"));
+		db.add(new Item("Nvidia Geforce 5060ti", 700000, "url2"));
+	}
 
 	public Long createItem(ItemRequest req) {
 		Item item = new Item(req.name(), req.price(), req.imageUrl());
@@ -29,5 +37,4 @@ public class ItemService {
 			.findFirst()
 			.orElseThrow(() -> new RuntimeException("해당 ID는 존재하지 않습니다"));
 	}
-
 }
