7b23c99ce1c784e9f8871fe63eb8812b83c2322a
Yunseong <dev.yunseong@gmail.com>
2025-06-24 14:28:44 +0900

    feature ProductController : implement Controller for Product CRUD and exception handling

--- src/main/java/gift/controller/ProductController.java
+++ src/main/java/gift/controller/ProductController.java
@@ -1,2 +1,60 @@
-package gift.controller;public class ProductController {
+package gift.controller;
+
+import gift.dto.ProductRequestDto;
+import gift.dto.ProductResponseDto;
+import gift.exception.NotFoundByIdException;
+import gift.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class ProductController {
+
+    private final ProductService productService;
+
+    @Autowired
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @DeleteMapping("/api/products/{productId}")
+    public ResponseEntity<Void> deleteProduct(@PathVariable Long productId) {
+        productService.deleteProductById(productId);
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/api/products")
+    public ResponseEntity<Long> createProduct(@RequestBody ProductRequestDto productRequestDto) {
+        Long productId = productService.saveProduct(productRequestDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(productId);
+    }
+
+    @PutMapping("/api/products/{productId}")
+    public ResponseEntity<Void> updateProduct(
+            @PathVariable Long productId,
+            @RequestBody ProductRequestDto productRequestDto
+    ) {
+        productService.updateProduct(productId, productRequestDto);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/api/products")
+    public ResponseEntity<List<ProductResponseDto>> getAllProducts() {
+        return ResponseEntity.ok(productService.findAllProducts());
+    }
+
+    @GetMapping("/api/products/{productId}")
+    public ResponseEntity<ProductResponseDto> getProduct(@PathVariable Long productId) {
+        return ResponseEntity.ok(productService.findProductById(productId));
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<String> handleNotFoundByIdException(NotFoundByIdException e) {
+        return ResponseEntity.status(HttpStatus.NO_CONTENT)
+                .body("Not Found by ID: " + e.getMessage());
+    }
 }
