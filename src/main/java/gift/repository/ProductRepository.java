1e14742933dfd31878e9788977dbb6bd3717c57f
WonJuN Sun <swj010324@naver.com>
2025-06-24 17:04:58 +0900

    feat(repository): implement ProductRepository, MemoryProductRepository

--- src/main/java/gift/repository/ProductRepository.java
+++ src/main/java/gift/repository/ProductRepository.java
@@ -1,2 +1,16 @@
-package main.java.gift.repository;public class ProductRepository {
+package gift.repository;
+
+import gift.dto.ProductResponseDto;
+import gift.entity.Product;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ProductRepository {
+
+    ProductResponseDto createProduct(Product product);
+    List<ProductResponseDto> searchAllProducts();
+    Optional<Product> searchProductById(Long id);
+    Product updateProduct(Long id, String name, Integer price, String imageUrl);
+    void deleteProduct(Long id);
 }
