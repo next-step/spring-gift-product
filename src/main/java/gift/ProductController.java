123cc5c42231a20154beca86e9faaa6c7e3e3ef7
OreoFlavor <80496872+OreoFlavor@users.noreply.github.com>
2025-06-24 20:59:54 +0900

    경북대 BE_오성식 1단계-상품 API (#59)
    * docs: README.md 작성
    feat: 상품 추가 기능 구현
    
    * feat: 상품 단건 조회 기능 구현
    
    * feat: 상품 목록 조회 기능 구현
    
    * feat: 상품 수정 기능 구현
    
    * feat: 상품 삭제 기능 구현

---
+++ src/main/java/gift/ProductController.java
@@ -0,0 +1,54 @@
+package gift;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.*;
+
+@RestController
+@RequestMapping("/api")
+public class ProductController {
+    private final Map<Long, Product> products = new HashMap<>();
+
+    @PostMapping("/product")
+    public String createProduct(@RequestBody Product product) {
+        products.put(product.getId(), product);
+        return "상품 생성: " + product.getId();
+    }
+
+    @GetMapping("/product/{id}")
+    public Product getProduct(@PathVariable Long id) {
+        Product product = products.get(id);
+        return product;
+    }
+
+    @GetMapping("/products")
+    public Collection<Product> getAllProducts() {
+        Collection<Product> productList = products.values();
+        return productList;
+    }
+
+    @PatchMapping("/product/{id}")
+    public Product updateProduct(@PathVariable Long id, @RequestBody Product updateProduct) {
+        Product oldProduct = products.get(id);
+
+        if(updateProduct.getName() != null) {
+            oldProduct.setName(updateProduct.getName());
+        }
+
+        if(updateProduct.getPrice() != 0) {
+            oldProduct.setPrice(updateProduct.getPrice());
+        }
+
+        if(updateProduct.getImageUrl() != null) {
+            oldProduct.setImageUrl(updateProduct.getImageUrl());
+        }
+
+        return oldProduct;
+    }
+
+    @DeleteMapping("/product/{id}")
+    public String deleteProduct(@PathVariable Long id) {
+        products.remove(id);
+        return "상품 삭제: " + id;
+    }
+}
