2e2342802cee9f6742b8bfb9946ca27f2aa8d57c
happyGogildong <rbgus630@gmail.com>
2025-06-24 19:59:36 +0900

    <feat, refactor>(/api/products): 관리자 페이지 상품 조회, 상품 추가 기능 구현
    Thymeleaf 사용으로 RESTapi -> MVC로 변경함
    상품 조회 메서드 수정 : "products.html" 사용
    상품 추가 메서드 수정 : HTML 폼 사용
    
    Breaks template에 products.html 추가
    ProductController를 @RestController에서
    @Controller로 변경
    상품 추가 메서드를 Id, name, price, imageURL을 입력받게 변경

--- src/main/java/gift/Application.java
+++ src/main/java/gift/Application.java
@@ -5,7 +5,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @SpringBootApplication
 public class Application {
@@ -13,20 +17,77 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 }
+@Component
+class ProductRepository {
+    final Map<Long, Product> products = new HashMap<>();
 
+    public Product get(long id) {
+        return products.get(id);
+    }
+
+    public boolean containsKey(long id) {
+        return products.containsKey(id);
+    }
+
+    public void add(Product product) {
+        products.put(product.id(), product);
+    }
+
+    public void delete(long id) {
+        products.remove(id);
+    }
+
+    public void update(long id, Product product) {
+        products.put(id, product);
+    }
+
+    public Collection<Product> findAll() {
+        return products.values();
+    }
+}
 
 record Product(long id, String name, int price, String imageURL){
 
 }
 
-@RestController
+@Controller
 class ProductController {
     //임시 저장소
-    private final Map<Long, Product> products = new HashMap<>();
+    private final ProductRepository products;
 
-    @GetMapping("/product")
-    public ResponseEntity<Product> getProduct(@RequestParam(value = "id", defaultValue = "") long id) {
-        return products.containsKey(id) ?
-                new ResponseEntity<>(products.get(id), HttpStatus.OK) : new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    public ProductController(ProductRepository products) {
+        this.products = products;
+    }
+
+    //상품 조회
+    @GetMapping("/api/products")
+    public String getProduct(@RequestParam(value = "id", defaultValue = "0") long id, Model model) {
+
+        model.addAttribute("allProducts",products.findAll());
+        return "products";
+    }
+
+    //상품 추가
+    @PostMapping("/api/products")
+    public String createProduct(@RequestParam long id, @RequestParam String name, @RequestParam int price, @RequestParam String imageURL) {
+        Product product = new Product(id, name, price, imageURL);
+        products.add(product);
+        return "redirect:/api/products";
+    }
+
+    //상품 수정
+    @PatchMapping("/api/products/{id}")
+    public ResponseEntity<Product> updateProduct(@PathVariable long id, @RequestBody Product product) {
+        if (!products.containsKey(id)) {return new ResponseEntity<>(HttpStatus.NOT_FOUND);}
+        products.update(id, product);
+        return new ResponseEntity<>(product, HttpStatus.OK);
+    }
+
+    //상품 삭제
+    @DeleteMapping("/api/products/{id}")
+    public ResponseEntity<Product> deleteProduct(@PathVariable long id) {
+        if (!products.containsKey(id)) {return new ResponseEntity<>(HttpStatus.NOT_FOUND);}
+        products.delete(id);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
