cd9fbb276fe58987f2ab405a6130b9fe2f770add
jihwan38 <sss203800@gmail.com>
2025-06-24 20:37:26 +0900

    feats: 상품 클래스 구현

---
+++ src/main/java/gift/entity/Product.java
@@ -0,0 +1,41 @@
+package gift.entity;
+
+public class Product {
+    private Long id;
+    private String name;
+    private Long price;
+    private String imageURL;
+
+    public Product(){}
+    public Product(Long id, String name, Long price, String imageURL){
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.imageURL = imageURL;
+    }
+    public Long getId(){
+        return id;
+    }
+    public void setId(Long id){
+        this.id = id;
+    }
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public Long getPrice(){
+        return price;
+    }
+    public void setPrice(Long price){
+        this.price = price;
+    }
+    public String getImageURL(){
+        return imageURL;
+    }
+    public void setImageURL(){
+        this.imageURL = imageURL;
+    }
+
+}
