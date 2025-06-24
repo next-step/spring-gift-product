6bf28fa59f6398244b365014758dd7d1d2bef8d6
sylimi2r2 <108124934+sylimi2r2@users.noreply.github.com>
2025-06-24 18:57:05 +0900

    Step1 (#37)
    * feat: 상품 조회 기능 추가
    
    * feat: 상품 등록 기능 추가 및 Product 생성자 추가
    
    * feat: 상품 수정 기능 추가
    
    * feat: 상품 삭제 기능 추가
    
    * fix: 상품 삭제 기능 HTTP 상태코드, 상품 전체 조회 기능 반환 수정
    
    * docs: README API 명세 작성

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # spring-gift-product
+
+- 1단계 - 상품 API 기능 요구 사항
+
+| 기능명      | Method | URL                              | Request                 | Response                    | 상태코드             |
+| -------- | ------ | -------------------------------- |-------------------------| --------------------------- | ---------------- |
+| 전체 상품 조회 | GET    | `/api/products`              | 없음                      | 다건 응답 정보 | `200 OK`         |
+| 상품 단건 조회 | GET    | `/api/products/{id}` | Path Variable           | 단건 응답 정보       | `200 OK`         |
+| 상품 추가    | POST   | `/api/products`              | 요청 body                 | 등록 정보       | `201 Created`    |
+| 상품 수정    | PATCH  | `/api/products/{id}` | 요청 body + Path Variable | 수정 정보       | `200 OK`         |
+| 상품 삭제    | DELETE | `/api/products/{id}` | 요청 body + Path Variable | 없음                          | `204 No Content` |
---
+++ src/main/java/gift/controller/ProductController.java
@@ -0,0 +1,71 @@
+package gift.controller;
+
+import gift.entity.Product;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.*;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+
+    private final Map<Long, Product> products = new HashMap<>();
+
+    // 상품 전체 조회
+    @GetMapping
+    public ResponseEntity<List<Product>> getAllProducts() {
+        return new ResponseEntity<>(new ArrayList<>(products.values()), HttpStatus.OK);
+    }
+
+    // 상품 id 기반 조회
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> getProductById(@PathVariable Long id) {
+        Product product = products.get(id);
+
+        if (product == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity<>(product, HttpStatus.OK);
+    }
+
+    // 상품 등록
+    @PostMapping
+    public ResponseEntity<Product> postProduct(@RequestBody Product product) {
+        Long productId = products.isEmpty() ? 1 : Collections.max(products.keySet()) + 1;
+        product.setId(productId);
+        products.put(productId, product);
+
+        return new ResponseEntity<>(product, HttpStatus.CREATED);
+    }
+
+    // 상품 수정
+    @PutMapping("/{id}")
+    public ResponseEntity<Product> updateProduct(@PathVariable Long id, @RequestBody Product updateProduct) {
+        Product product = products.get(id);
+
+        if (product == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        product.setName(updateProduct.getName());
+        product.setPrice(updateProduct.getPrice());
+        product.setImageUrl(updateProduct.getImageUrl());
+
+        return new ResponseEntity<>(product, HttpStatus.OK);
+    }
+
+    // 상품 삭제
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteProduct(@PathVariable Long id) {
+        if (products.containsKey(id)) {
+            products.remove(id);
+
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+}
