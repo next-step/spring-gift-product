4b63074e5018e8c3a76fff4e7660f815ec656700
eastgyu11 <eastgyu11>
2025-06-24 16:05:23 +0900

    feat: 모든 상품 조회 기능 추가

---
+++ src/main/java/gift/controller/ProductController.java
@@ -0,0 +1,24 @@
+package gift.controller;
+
+import gift.entity.Product;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class ProductController {
+  private final Map<Long, Product> productsMap = new HashMap<>();
+
+
+  @GetMapping("/products")
+  public ResponseEntity<List<Product>> findAllProduct(){
+    return new ResponseEntity<>(new ArrayList<>(productsMap.values()), HttpStatus.OK);
+  }
+}
