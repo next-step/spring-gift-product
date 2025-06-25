412ac30e3cef2f79b0580c9d29958b8a350a2680
hardjava <fkdlcn123@kumoh.ac.kr>
2025-06-25 15:00:14 +0900

    docs(README): 2단계 관리자 화면 구성 및 기술 스택 정리

--- src/main/java/gift/dto/ProductResponseDto.java
+++ src/main/java/gift/dto/ProductResponseDto.java
@@ -2,13 +2,37 @@ package gift.dto;
 
 import gift.entity.Product;
 
-public record ProductResponseDto(
-        Long id,
-        String name,
-        Long price,
-        String imageUrl
-) {
+public class ProductResponseDto {
+    private final Long id;
+    private final String name;
+    private final Long price;
+    private final String imageUrl;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Long getPrice() {
+        return price;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public ProductResponseDto(Long id, String name, Long price, String imageUrl) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.imageUrl = imageUrl;
+    }
+
     public static ProductResponseDto from(Product product) {
+
         return new ProductResponseDto(
                 product.getId(),
                 product.getName(),
