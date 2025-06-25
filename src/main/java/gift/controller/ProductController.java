88e04fc2f8ac5374055e3170ddf566358460b3fd
eastgyu11 <eastgyu11>
2025-06-25 21:35:43 +0900

    docs: README에 step2 구현사항 작성

--- src/main/java/gift/controller/ProductController.java
+++ src/main/java/gift/controller/ProductController.java
@@ -19,32 +19,35 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api")
 public class ProductController {
+
   private final Map<Long, Product> productsMap = new HashMap<>();
 
   @GetMapping("/products")
-  public ResponseEntity<List<Product>> findAllProduct(){
+  public ResponseEntity<List<Product>> findAllProduct() {
     return new ResponseEntity<>(new ArrayList<>(productsMap.values()), HttpStatus.OK);
   }
 
   @GetMapping("/products/{id}")
-  public ResponseEntity<Product> findProductById(@PathVariable Long id){
-    Product findByIdProduct=productsMap.get(id);
-    return new ResponseEntity<>(findByIdProduct,HttpStatus.OK);
+  public ResponseEntity<Product> findProductById(@PathVariable Long id) {
+    Product findByIdProduct = productsMap.get(id);
+    return new ResponseEntity<>(findByIdProduct, HttpStatus.OK);
   }
 
   @PostMapping("/products")
-  public ResponseEntity<Product> createProduct(@RequestBody Product product){
+  public ResponseEntity<Product> createProduct(@RequestBody Product product) {
     productsMap.put(product.getId(), product);
-    return new ResponseEntity<>(product,HttpStatus.OK);
+    return new ResponseEntity<>(product, HttpStatus.OK);
   }
 
   @PatchMapping("/products/{id}")
-  public ResponseEntity<Product> updateProduct(@PathVariable Long id, @RequestBody Product newProduct){
-    productsMap.replace(id,newProduct);
-    return new ResponseEntity<>(newProduct,HttpStatus.OK);
+  public ResponseEntity<Product> updateProduct(@PathVariable Long id,
+      @RequestBody Product newProduct) {
+    productsMap.replace(id, newProduct);
+    return new ResponseEntity<>(newProduct, HttpStatus.OK);
   }
+
   @DeleteMapping("/products/{id}")
-  public ResponseEntity<Product> deleteProduct(@PathVariable Long id){
+  public ResponseEntity<Product> deleteProduct(@PathVariable Long id) {
     productsMap.remove(id);
     return new ResponseEntity<>(HttpStatus.OK);
   }
