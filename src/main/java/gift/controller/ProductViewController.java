9bb60924560a49b62ecd7000f01c58b1abcd1b42
khwon0407 <126738028+khwon0407@users.noreply.github.com>
2025-06-25 14:54:46 +0900

    feat: 메인 화면 추가 및 최초 연결 구현

--- src/main/java/gift/controller/ProductViewController.java
+++ src/main/java/gift/controller/ProductViewController.java
@@ -1,10 +1,15 @@
 package gift.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/products")
 public class ProductViewController {
-
+    
+    @GetMapping
+    public String showList() {
+        return "product-list";
+    }
 }
