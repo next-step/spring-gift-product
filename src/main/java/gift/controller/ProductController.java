a35c968e3873af90e33b00476e00ee7ed9aa8672
hydrationn <suhwaparkk1031@gmail.com>
2025-06-26 01:00:42 +0900

    refact: ProductController에서 구현체 의존 제거 및 인터페이스로 변경

--- src/main/java/gift/controller/ProductController.java
+++ src/main/java/gift/controller/ProductController.java
@@ -2,7 +2,7 @@ package gift.controller;
 
 import gift.dto.ProductRequestDto;
 import gift.dto.ProductResponseDto;
-import gift.service.ProductServiceImpl;
+import gift.service.ProductService;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,28 +12,28 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/products")
 public class ProductController {
-    private final ProductServiceImpl productServiceImpl;
+    private final ProductService productService;
 
-    public ProductController(ProductServiceImpl productServiceImpl) {
-        this.productServiceImpl = productServiceImpl;
+    public ProductController(ProductService productService) {
+        this.productService = productService;
     }
 
     @PostMapping
     public ResponseEntity<ProductResponseDto> createProduct(
             @RequestBody @Valid ProductRequestDto requestDto) {
-        ProductResponseDto response = productServiceImpl.createProduct(requestDto);
+        ProductResponseDto response = productService.createProduct(requestDto);
         return ResponseEntity.ok(response);
     }
 
     @GetMapping
     public ResponseEntity<List<ProductResponseDto>> findAllProducts() {
-        List<ProductResponseDto> list = productServiceImpl.findAllProducts();
+        List<ProductResponseDto> list = productService.findAllProducts();
         return ResponseEntity.ok(list);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<ProductResponseDto> findProductById(@PathVariable Long id) {
-        ProductResponseDto response = productServiceImpl.findProductById(id);
+        ProductResponseDto response = productService.findProductById(id);
         return ResponseEntity.ok(response);
     }
 
@@ -41,13 +41,13 @@ public class ProductController {
     public ResponseEntity<ProductResponseDto> updateProduct(
             @PathVariable Long id,
             @RequestBody @Valid ProductRequestDto requestDto) {
-        ProductResponseDto response = productServiceImpl.updateProduct(id, requestDto);
+        ProductResponseDto response = productService.updateProduct(id, requestDto);
         return ResponseEntity.ok(response);
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteProduct(@PathVariable Long id) {
-        productServiceImpl.deleteProduct(id);
+        productService.deleteProduct(id);
         return ResponseEntity.noContent().build();
     }
 
