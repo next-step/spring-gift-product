4ca32520db00248ad08b53ae408008f307498405
jyhyt1567 <jyhyt1567@gmail.com>
2025-06-25 20:00:14 +0900

    refactor: 코드 포맷 수정

--- src/main/java/gift/service/ProductServiceImpl.java
+++ src/main/java/gift/service/ProductServiceImpl.java
@@ -11,19 +11,22 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
 @Service
-public class ProductServiceImpl implements ProductService{
+public class ProductServiceImpl implements ProductService {
+
     private ProductRepository productRepository;
     private Long id = 1L;
 
-    public ProductServiceImpl(ProductRepository productRepository){
+    public ProductServiceImpl(ProductRepository productRepository) {
         this.productRepository = productRepository;
     }
 
     @Override
     public ProductResponseDto createProduct(CreateProductRequestDto requestDto) {
-        Product newProduct = new Product(id++, requestDto.getName(),requestDto.getPrice(),requestDto.getImageUrl());
+        Product newProduct = new Product(id++, requestDto.getName(), requestDto.getPrice(),
+                requestDto.getImageUrl());
         Product createdProduct = productRepository.createProduct(newProduct);
-        return new ProductResponseDto(createdProduct.getId(), createdProduct.getName(), createdProduct.getPrice(), createdProduct.getImageUrl());
+        return new ProductResponseDto(createdProduct.getId(), createdProduct.getName(),
+                createdProduct.getPrice(), createdProduct.getImageUrl());
     }
 
     @Override
@@ -35,29 +38,30 @@ public class ProductServiceImpl implements ProductService{
     public ProductResponseDto findProductById(Long id) {
         Product find = productRepository.findProductById(id);
 
-        if(find == null){
+        if (find == null) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND);
         }
 
         ProductResponseDto responseDto = new ProductResponseDto(find.getId(), find.getName(),
-            find.getPrice(), find.getImageUrl());
+                find.getPrice(), find.getImageUrl());
         return responseDto;
     }
 
     @Override
     public ProductResponseDto updateProductById(Long id, CreateProductRequestDto requestDto) {
         Product product = productRepository.findProductById(id);
-        if(product == null){
+        if (product == null) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND);
         }
         product.update(requestDto);
-        return new ProductResponseDto(product.getId(), product.getName(), product.getPrice(), product.getImageUrl());
+        return new ProductResponseDto(product.getId(), product.getName(), product.getPrice(),
+                product.getImageUrl());
     }
 
     @Override
     public void deleteProductById(Long id) {
         Product product = productRepository.findProductById(id);
-        if(product == null){
+        if (product == null) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND);
         }
         productRepository.deleteProductById(id);
