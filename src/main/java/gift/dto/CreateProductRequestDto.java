33220efb0663744598ae36ad6b8b9c1443ebbd9f
hardjava <fkdlcn123@kumoh.ac.kr>
2025-06-25 16:20:43 +0900

    feat(admin): 관리자 상품 페이지 및 등록 구현

--- src/main/java/gift/dto/CreateProductRequestDto.java
+++ src/main/java/gift/dto/CreateProductRequestDto.java
@@ -4,32 +4,45 @@ import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
-public class CreateProductRequestDto {
+//public class CreateProductRequestDto {
+//
+//    @NotBlank(message = "상품 이름은 비어 있을 수 없습니다.")
+//    private final String name;
+//
+//    @NotNull(message = "가격은 필수 입력입니다.")
+//    @Min(value = 0, message = "가격은 0 이상이어야 합니다.")
+//    private final Long price;
+//
+//    private final String imageUrl;
+//
+//    public CreateProductRequestDto(String name, Long price, String imageUrl) {
+//        this.name = name;
+//        this.price = price;
+//        this.imageUrl = imageUrl;
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public Long getPrice() {
+//        return price;
+//    }
+//
+//    public String getImageUrl() {
+//        return imageUrl;
+//    }
+//}
 
-    @NotBlank(message = "상품 이름은 비어 있을 수 없습니다.")
-    private final String name;
+public record CreateProductRequestDto(
+        @NotBlank(message = "상품 이름은 비어 있을 수 없습니다.")
+        String name,
 
-    @NotNull(message = "가격은 필수 입력입니다.")
-    @Min(value = 0, message = "가격은 0 이상이어야 합니다.")
-    private final Long price;
+        @NotNull(message = "가격은 필수 입력입니다.")
+        @Min(value = 0, message = "가격은 0 이상이어야 합니다.")
+        Long price,
 
-    private final String imageUrl;
+        String imageUrl
+) {
 
-    public CreateProductRequestDto(String name, Long price, String imageUrl) {
-        this.name = name;
-        this.price = price;
-        this.imageUrl = imageUrl;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public Long getPrice() {
-        return price;
-    }
-
-    public String getImageUrl() {
-        return imageUrl;
-    }
 }
