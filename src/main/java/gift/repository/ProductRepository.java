d42e26e52e938d62612095fc3552a8d1c9df84d3
No_way <74843137+thdgmlwns1@users.noreply.github.com>
2025-06-25 23:19:09 +0900

    Step1_ 상품 API (#50)
    * Feat: entitiy 설계
    
    * Feat: Product 생성
    
    * Feat: Product 모두 조회
    
    * Feat: Product 수정
    
    * Feat: Product 삭제
    
    * docs: README.md update
    
    * Feat:피드백 반영(에러 핸들러, 불필요한 코드 제거 등)

---
+++ src/main/java/gift/repository/ProductRepository.java
@@ -0,0 +1,34 @@
+package gift.repository;
+
+import gift.entity.Product;
+import org.springframework.stereotype.Repository;
+
+import java.util.*;
+
+@Repository
+public class ProductRepository {
+    private final Map<Long, Product> products = new HashMap<>();
+    private long Id = 1;
+
+    public Product save(Product product) {
+        if (product.getId() == null) {
+            product.setId(Id++);
+        }
+        products.put(product.getId(), product);
+        return product;
+    }
+
+    public List<Product> findAll() {
+        return new ArrayList<>(products.values());
+    }
+
+
+    public Optional<Product> findById(Long id) {
+        return Optional.ofNullable(products.get(id));
+    }
+
+
+    public void deleteById(Long id) {
+        products.remove(id);
+    }
+}
