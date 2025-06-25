b8bf080b6c5dc0e177711c97d97f501a4160cbd4
thstkddnr20 <thstkddnr20@naver.com>
2025-06-25 15:57:36 +0900

    feat: 상품 추가 폼 구현

--- src/main/java/gift/controller/ProductManageController.java
+++ src/main/java/gift/controller/ProductManageController.java
@@ -1,10 +1,13 @@
 package gift.controller;
 
+import gift.dto.product.CreateProductRequest;
 import gift.dto.product.ProductManageResponse;
 import gift.service.ProductService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -24,5 +27,18 @@ public class ProductManageController {
         List<ProductManageResponse> products = productService.getAllProductsManagement();
         model.addAttribute("products", products);
         return "/management/productList";
+    }
+
+    @GetMapping("/new")
+    public String createProduct(Model model) {
+        model.addAttribute("request", new CreateProductRequest(null, null, null));
+        return "/management/productCreate";
+    }
+
+
+    @PostMapping
+    public String createProduct(@ModelAttribute CreateProductRequest request) {
+        productService.saveProduct(request);
+        return "redirect:/management/products";
     }
 }
