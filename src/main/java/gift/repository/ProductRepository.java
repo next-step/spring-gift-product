9142db2c6600bd0a8e114211bc25dab814b2ac90
WonJuN Sun <swj010324@naver.com>
2025-06-26 02:22:10 +0900

    style(repository) modifications according to google java coding conventions

--- src/main/java/gift/repository/ProductRepository.java
+++ src/main/java/gift/repository/ProductRepository.java
@@ -2,15 +2,18 @@ package gift.repository;
 
 import gift.dto.ProductResponseDto;
 import gift.entity.Product;
-
 import java.util.List;
 import java.util.Optional;
 
 public interface ProductRepository {
 
-    ProductResponseDto createProduct(Product product);
-    List<ProductResponseDto> searchAllProducts();
-    Optional<Product> searchProductById(Long id);
-    Product updateProduct(Long id, String name, Integer price, String imageUrl);
-    void deleteProduct(Long id);
+  ProductResponseDto createProduct(Product product);
+
+  List<ProductResponseDto> searchAllProducts();
+
+  Optional<Product> searchProductById(Long id);
+
+  Product updateProduct(Long id, String name, Integer price, String imageUrl);
+
+  void deleteProduct(Long id);
 }
