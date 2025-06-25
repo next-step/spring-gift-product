b7a9a8fc8321dac469f6cd40ca2eee461d324542
jihwan38 <sss203800@gmail.com>
2025-06-25 16:37:38 +0900

    refactor: ProductResponeDto를 record 타입으로 전환

--- src/main/java/gift/dto/ProductResponseDto.java
+++ src/main/java/gift/dto/ProductResponseDto.java
@@ -2,33 +2,15 @@ package gift.dto;
 
 import gift.entity.Product;
 
-public class ProductResponseDto {
-    private final Long id;
-    private final String name;
-    private final Long price;
-    private final String imageUrl;
 
-    public ProductResponseDto(Product product){
-        this.id = product.getId();
-        this.name = product.getName();
-        this.price = product.getPrice();
-        this.imageUrl = product.getImageUrl();
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public Long getPrice() {
-        return price;
-    }
-
-    public String getImageUrl() {
-        return imageUrl;
+public record ProductResponseDto(Long id, String name, Long price, String imageUrl) {
+    public static ProductResponseDto from(Product product) {
+        return new ProductResponseDto(
+                product.getId(),
+                product.getName(),
+                product.getPrice(),
+                product.getImageUrl()
+        );
     }
 }
 
