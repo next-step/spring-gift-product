e0940fbef25cf996187b1033714391baa51be440
jyhyt1567 <jyhyt1567@gmail.com>
2025-06-25 19:43:53 +0900

    feat: 관리자 화면 템플릿, 컨트롤러 생성

---
+++ src/main/java/gift/controller/AdminController.java
@@ -0,0 +1,17 @@
+package gift.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+
+    @GetMapping
+    public String showAdminPage(Model model) {
+        model.addAttribute("data", "asd");
+        return "admin";
+    }
+}
