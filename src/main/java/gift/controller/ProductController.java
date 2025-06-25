9f2aab16a1604237279176a2732de23320d9d2eb
jihwan38 <sss203800@gmail.com>
2025-06-25 09:01:25 +0900

    fix: Controller에 필요 없는 import 삭제

--- src/main/java/gift/controller/ProductController.java
+++ src/main/java/gift/controller/ProductController.java
@@ -3,7 +3,6 @@ package gift.controller;
 import gift.dto.ProductRequestDto;
 import gift.dto.ProductResponseDto;
 import gift.service.ProductService;
-import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
