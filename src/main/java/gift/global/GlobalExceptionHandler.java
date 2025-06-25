6ec8494b8929e50718fc295258bfae05ee14f832
KNU-K <knukang334@gmail.com>
2025-06-26 02:50:50 +0900

    refactor(exception) - GlobalException 에서 ErrorResponse 객체를 통해 응답하여, 안정성 처리

--- src/main/java/gift/global/GlobalExceptionHandler.java
+++ src/main/java/gift/global/GlobalExceptionHandler.java
@@ -1,12 +1,11 @@
 package gift.global;
 
+import gift.dto.common.ErrorResponse;
 import gift.exception.BusinessException;
 import gift.exception.ErrorCode;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.time.LocalDateTime;
-import java.util.HashMap;
-import java.util.Map;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -15,27 +14,26 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(BusinessException.class)
-    public ResponseEntity<Map<String, Object>> handleBusinessException(BusinessException ex) {
+    public ResponseEntity<ErrorResponse> handleBusinessException(BusinessException ex) {
         return buildErrorResponse(ex.getErrorCode(), ex);
     }
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<Map<String, Object>> handleException(Exception ex) {
+    public ResponseEntity<ErrorResponse> handleException(Exception ex) {
         return buildErrorResponse(ErrorCode.UNKNOWN_ERROR, ex);
     }
 
-    private ResponseEntity<Map<String, Object>> buildErrorResponse(ErrorCode errorCode,
-            Exception ex) {
-        Map<String, Object> errorBody = new HashMap<>();
-        errorBody.put("timestamp", LocalDateTime.now());
-        errorBody.put("errorCode", errorCode.name());
-        errorBody.put("message", errorCode.getDescription());
-
-        errorBody.put("trace", getStackTraceString(ex));
+    private ResponseEntity<ErrorResponse> buildErrorResponse(ErrorCode errorCode, Exception ex) {
+        ErrorResponse errorResponse = new ErrorResponse(
+                LocalDateTime.now(),
+                errorCode.name(),
+                errorCode.getDescription(),
+                getStackTraceString(ex)
+        );
 
         return ResponseEntity
                 .status(errorCode.getStatus())
-                .body(errorBody);
+                .body(errorResponse);
     }
 
     private String getStackTraceString(Exception ex) {
