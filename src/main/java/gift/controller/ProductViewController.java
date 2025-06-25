5344ca68477d1a60b0036a90b4cdc03893dc10f2
khwon0407 <126738028+khwon0407@users.noreply.github.com>
2025-06-25 15:23:35 +0900

    feat: 최초 목록 화면 구현

--- src/main/java/gift/controller/ProductViewController.java
+++ src/main/java/gift/controller/ProductViewController.java
@@ -1,6 +1,10 @@
 package gift.controller;
 
+import gift.dto.FindProductResponseDto;
+import gift.service.ProductService;
+import java.util.List;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,8 +12,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/products")
 public class ProductViewController {
     
+    private final ProductService productService;
+    
+    public ProductViewController(ProductService productService) {
+        this.productService = productService;
+    }
+    
     @GetMapping
-    public String showList() {
+    public String showList(Model model) {
+        List<FindProductResponseDto> products = productService.findAllProducts();
+        model.addAttribute("products", products);
         return "product-list";
     }
 }
