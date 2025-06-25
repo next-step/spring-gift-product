07e602b861357f008bc51cdf96de4dc18d8e83f0
JAEIL1999 <pkyj040410@gmail.com>
2025-06-25 10:49:52 +0900

    상품 전체 조회 구현

--- src/main/java/gift/controller/ProductController.java
+++ src/main/java/gift/controller/ProductController.java
@@ -1,5 +1,21 @@
 package gift.controller;
 
+import gift.dto.Product;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@RestController
+@RequestMapping("/products")
 public class ProductController {
+    private final Map<Long, Product> products = new ConcurrentHashMap<>();
+
+    @GetMapping
+    public Collection<Product> getProducts() {
+        return products.values();
+    }
+
 
 }
