9c9264cfbb41b09270e3d8b6c8b3e554c6fb5fbd
Junhee <dkfl7190@naver.com>
2025-06-26 08:47:46 +0900

    feat: 관리자 상품 추가 구현

--- src/main/java/gift/controller/AdminController.java
+++ src/main/java/gift/controller/AdminController.java
@@ -1,10 +1,13 @@
 package gift.controller;
 
+import gift.dto.ProductRequestDto;
 import gift.service.ProductService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -34,5 +37,19 @@ public class AdminController {
         }
         return "admin/products/detail";
     }
+
+    // 상품 추가
+    @GetMapping("/new")
+    public String newProduct(Model model) {
+        model.addAttribute("product", new ProductRequestDto());
+        return "admin/products/new";
+    }
+
+    @PostMapping
+    public String addProduct(@ModelAttribute ProductRequestDto productRequestDto) {
+        productService.saveProduct(productRequestDto);
+        return "redirect:/admin/products";
+    }
+
 
 }
--- src/main/java/gift/dto/ProductRequestDto.java
+++ src/main/java/gift/dto/ProductRequestDto.java
@@ -17,4 +17,17 @@ public class ProductRequestDto {
     public String getImageUrl() {
         return imageUrl;
     }
+
+    // @ModelAttribute: 세터 기반 바인딩
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
 }
