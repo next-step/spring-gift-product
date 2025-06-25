0137866c80dbd206ab1cb444d34ab20bdeca9b09
gosurim <su020900@gmail.com>
2025-06-25 15:18:12 +0900

    style: 코드 컨벤션 관련 설정 수정

--- src/main/java/gift/controller/ProductController.java
+++ src/main/java/gift/controller/ProductController.java
@@ -3,22 +3,30 @@ package gift.controller;
 import gift.dto.ProductRequestDto;
 import gift.dto.ProductResponseDto;
 import gift.service.ProductService;
+import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/products")
 public class ProductController {
+
     private final ProductService productService;
+
     public ProductController(ProductService productService) {
         this.productService = productService;
     }
 
     // 생성
-    @PostMapping // 요청
+    @PostMapping
     public ResponseEntity<ProductResponseDto> createProduct(
             @RequestBody ProductRequestDto requestDto) {
         return new ResponseEntity<>(productService.saveProduct(requestDto), HttpStatus.CREATED);
@@ -42,7 +50,9 @@ public class ProductController {
             @PathVariable Long id,
             @RequestBody ProductResponseDto requestDto
     ) {
-        return new ResponseEntity<>(productService.updateProduct(id, requestDto.name(), requestDto.price(), requestDto.imageUrl()), HttpStatus.OK);
+        return new ResponseEntity<>(
+                productService.updateProduct(id, requestDto.name(), requestDto.price(),
+                        requestDto.imageUrl()), HttpStatus.OK);
     }
 
     // 삭제
