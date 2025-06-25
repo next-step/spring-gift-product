d009dfbcd051945e9b8806c7c98b0ca40bd37c97
ji-mim <194115@jnu.ac.kr>
2025-06-25 17:47:11 +0900

    Feat(Add Product): feat controller and addForm.html for add Product

--- src/main/java/gift/controller/ProductController.java
+++ src/main/java/gift/controller/ProductController.java
@@ -2,12 +2,16 @@ package gift.controller;
 
 
 import gift.domain.Product;
+import gift.dto.CreateProductRequest;
 import gift.service.ProductService;
 import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/api/products")
@@ -24,5 +28,17 @@ public class ProductController {
         List<Product> products = service.findAll();
         model.addAttribute("products", products);
         return "/products";
+    }
+
+    @GetMapping("/add")
+    public String addProduct() {
+        return "/addForm";
+    }
+
+
+    @PostMapping("/add")
+    public String addProduct(@ModelAttribute CreateProductRequest request, RedirectAttributes redirectAttributes) {
+        service.save(request);
+        return "redirect:/api/products";
     }
 }
