240d5c27525cd11c223e6617c50d940ff800c0c1
KimHaejoong1 <hjkim2124@gmail.com>
2025-06-25 16:40:42 +0900

    feat(view): 상품 조회 구현

--- src/main/java/gift/controller/ProductViewController.java
+++ src/main/java/gift/controller/ProductViewController.java
@@ -1,8 +1,14 @@
 package gift.controller;
 
+import gift.dto.ProductResponseDTO;
 import gift.service.ProductService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/products")
@@ -11,4 +17,13 @@ public class ProductViewController {
     public ProductViewController(ProductService productService) {
         this.productService = productService;
     }
+
+    @GetMapping
+    public String getAllProducts(Model model) {
+        List<ProductResponseDTO> products = productService.getAll();
+        model.addAttribute("products", products);
+        return "products";
+    }
+
+
 }
