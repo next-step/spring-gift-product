700fc33f33c4d9f0d5ff0f293617cae41cf977c3
ㄱㄷㄱ <101164269+eastgyu963@users.noreply.github.com>
2025-06-25 13:22:04 +0900

    경북대 BE 김동규 1단계 - 상품 API (#40)
    * docs: README.md에 구현할 기능 목록 정리
    
    * feat: 모든 상품 조회 기능 추가
    
    * feat: 특정 상품 조회 기능 추가
    
    * feat: 상품 등록 기능 추가
    
    * feat: 상품 업데이트 기능 추가
    
    * fix: 상품 업데이트 기능에 pathvariable 수정
    
    * fix: 상품 업데이트 기능 오류 고침
    
    * feat: 상품 삭제 기능 추가
    
    ---------
    
    Co-authored-by: eastgyu11 <eastgyu11>

---
+++ src/main/java/gift/controller/ProductController.java
@@ -0,0 +1,51 @@
+package gift.controller;
+
+import gift.entity.Product;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class ProductController {
+  private final Map<Long, Product> productsMap = new HashMap<>();
+
+  @GetMapping("/products")
+  public ResponseEntity<List<Product>> findAllProduct(){
+    return new ResponseEntity<>(new ArrayList<>(productsMap.values()), HttpStatus.OK);
+  }
+
+  @GetMapping("/products/{id}")
+  public ResponseEntity<Product> findProductById(@PathVariable Long id){
+    Product findByIdProduct=productsMap.get(id);
+    return new ResponseEntity<>(findByIdProduct,HttpStatus.OK);
+  }
+
+  @PostMapping("/products")
+  public ResponseEntity<Product> createProduct(@RequestBody Product product){
+    productsMap.put(product.getId(), product);
+    return new ResponseEntity<>(product,HttpStatus.OK);
+  }
+
+  @PatchMapping("/products/{id}")
+  public ResponseEntity<Product> updateProduct(@PathVariable Long id, @RequestBody Product newProduct){
+    productsMap.replace(id,newProduct);
+    return new ResponseEntity<>(newProduct,HttpStatus.OK);
+  }
+  @DeleteMapping("/products/{id}")
+  public ResponseEntity<Product> deleteProduct(@PathVariable Long id){
+    productsMap.remove(id);
+    return new ResponseEntity<>(HttpStatus.OK);
+  }
+}
