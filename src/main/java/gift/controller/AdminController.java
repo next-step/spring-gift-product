06aa2c8d1fb6a7351b99c120f9a406724c550f30
jyhyt1567 <jyhyt1567@gmail.com>
2025-06-25 19:55:56 +0900

    feat: 관리자 화면에 상품 조회 기능 구현

--- src/main/java/gift/controller/AdminController.java
+++ src/main/java/gift/controller/AdminController.java
@@ -1,5 +1,8 @@
 package gift.controller;
 
+import gift.dto.ProductResponseDto;
+import gift.service.ProductService;
+import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,9 +12,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/admin")
 public class AdminController {
 
+    private final ProductService productService;
+
+    public AdminController(ProductService productService) {
+        this.productService = productService;
+    }
+
     @GetMapping
     public String showAdminPage(Model model) {
-        model.addAttribute("data", "asd");
+        List<ProductResponseDto> products = productService.findAllProducts();
+        model.addAttribute("products", products);
         return "admin";
     }
 }
