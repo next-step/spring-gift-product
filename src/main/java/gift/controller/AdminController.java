2d53489ceb1823a1de03cb4b7796ca982699b2cf
eastgyu11 <eastgyu11>
2025-06-25 23:26:06 +0900

    feat: 상품 등록 기능 구현

--- src/main/java/gift/controller/AdminController.java
+++ src/main/java/gift/controller/AdminController.java
@@ -1,11 +1,13 @@
 package gift.controller;
 
+import gift.dto.ProductRequestDto;
 import gift.dto.ProductResponseDto;
 import gift.service.ProductService;
 import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -23,5 +25,17 @@ public class AdminController {
     List<ProductResponseDto> responseDtoList = service.findAllProduct();
     model.addAttribute("responseDtoList", responseDtoList);
     return "list";
+  }
+
+  @GetMapping("/new")
+  public String createForm(Model model) {
+    model.addAttribute("requestDto", new ProductRequestDto());
+    return "createProductForm";
+  }
+
+  @PostMapping("/new")
+  public String create(ProductRequestDto requestDto) {
+    service.createProduct(requestDto);
+    return "redirect:/admin/products";
   }
 }
