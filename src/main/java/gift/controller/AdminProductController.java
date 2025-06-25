177831f7e00c72cdc3d544356a29e7d4b0cfde82
gosurim <su020900@gmail.com>
2025-06-25 18:53:01 +0900

    feat(admin-controller): 관리자 상품 등록 기능 추가 및 확인용 form.html 구현

--- src/main/java/gift/controller/AdminProductController.java
+++ src/main/java/gift/controller/AdminProductController.java
@@ -1,11 +1,14 @@
 package gift.controller;
 
+import gift.dto.ProductRequestDto;
 import gift.dto.ProductResponseDto;
 import gift.service.ProductService;
 import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -24,5 +27,19 @@ public class AdminProductController {
         List<ProductResponseDto> products = productService.findAllProducts();
         model.addAttribute("products", products);
         return "product/list";
+    }
+
+    // 상품 등록
+    @GetMapping("/new")
+    public String showCreateForm(Model model) {
+        model.addAttribute("product", new ProductRequestDto("", 0, ""));
+        return "product/form";
+    }
+
+    // 등록 처리
+    @PostMapping
+    public String createProduct(@ModelAttribute ProductRequestDto requestDto) {
+        productService.saveProduct(requestDto);
+        return "redirect:/admin/products";
     }
 }
