5b74e6893d76500b32d8029bb7aecd292cc61a96
yeongseo1225 <yeongseoseo1225@gmail.com>
2025-06-26 15:49:27 +0900

    feat(admin): add product list view

---
+++ src/main/java/gift/Controller/AdminProductController.java
@@ -0,0 +1,28 @@
+package gift.Controller;
+
+import gift.domain.Product;
+import gift.service.ProductService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/admin/products")
+public class AdminProductController {
+
+    private final ProductService productService;
+
+    public AdminProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping
+    public String list(Model model) {
+        List<Product> products = productService.getAll();
+        model.addAttribute("products", products);
+        return "admin/product-list";
+    }
+}
