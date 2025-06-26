6317c815fe70fb08e7d027197cd646c0739057c2
HyunJunCho00 <catlove5478@naver.com>
2025-06-26 22:49:53 +0900

    feat: JbdcTemplate 기본 구조 및 SimpleJdbcInsert 설정 추가

--- src/main/java/gift/repository/ProductRepository.java
+++ src/main/java/gift/repository/ProductRepository.java
@@ -1,33 +1,24 @@
 package gift.repository;
 
 import gift.entity.Product;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 
 import java.util.*;
 
 @Repository
 public class ProductRepository {
-    private final Map<Long, Product> store = new HashMap<>();
-    private long sequence = 1L;
-
-    public List<Product> findAll() {
-        return new ArrayList<>(store.values());
+    private final JdbcTemplate jdbcTemplate;
+    private final SimpleJdbcInsert insert;
+    public ProductRepository(JdbcTemplate jdbcTemplate)
+    {
+        this.jdbcTemplate = jdbcTemplate;
+        this.insert = new SimpleJdbcInsert(jdbcTemplate)
+                .usingGeneratedKeyColumns("id")
+                .withTableName("products");
     }
 
-    public Optional<Product> findById(Long id) {
-        return Optional.ofNullable(store.get(id));
-    }
-    public Product save(Product product) {
-        product.setId(sequence++);
-        store.put(product.getId(), product);
-        return product;
-    }
-    public void update(Long id, Product updated) {
-        store.put(id, updated);
-    }
-    public void delete(Long id) {
-        store.remove(id);
-    }
 }
 
 
