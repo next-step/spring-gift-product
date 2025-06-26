e40d2066b518dbb7c71202fdb1b7a5de639d4c18
hydrationn <suhwaparkk1031@gmail.com>
2025-06-26 16:29:54 +0900

    fix: 상품 조회 시 상품명, 가격 조회 오류 수정

--- src/main/java/gift/dto/ProductRequestDto.java
+++ src/main/java/gift/dto/ProductRequestDto.java
@@ -27,4 +27,16 @@ public class ProductRequestDto {
     public String getImageUrl() {
         return imageUrl;
     }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
 }
