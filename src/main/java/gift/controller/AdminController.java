0ebc6fd880e5ac8433f965afd78dc68560b632dd
sanghyeon1225 <oopt1234@naver.com>
2025-06-26 16:26:32 +0900

    feats: 상품 추가 기능 구현

--- src/main/java/gift/controller/AdminController.java
+++ src/main/java/gift/controller/AdminController.java
@@ -1,11 +1,16 @@
 package gift.controller;
 
+import gift.dto.ProductRequestDto;
 import gift.dto.ProductResponseDto;
 import gift.service.ProductService;
+import jakarta.validation.Valid;
 import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -23,5 +28,24 @@ public class AdminController {
     List<ProductResponseDto> products = productService.findAllProduct();
     model.addAttribute("products", products);
     return "admin/list"; // templates/admin/list.html
+  }
+
+  @GetMapping("/add")
+  public String addForm(Model model) {
+    model.addAttribute("product", new ProductRequestDto("", 0, ""));
+    model.addAttribute("mode", "add");
+    return "admin/form";
+  }
+
+  @PostMapping("/add")
+  public String addProduct(@Valid @ModelAttribute("product") ProductRequestDto productRequestDto,
+      BindingResult bindingResult,
+      Model model) {
+    if (bindingResult.hasErrors()) {
+      model.addAttribute("mode", "add");
+      return "admin/form";
+    }
+    productService.addProduct(productRequestDto);
+    return "redirect:/admin";
   }
 }
