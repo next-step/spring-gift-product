686831cf990c4464b555e47e88ccf57655afeb80
Jinho Lee <76112855+treasure-sky@users.noreply.github.com>
2025-06-26 12:42:54 +0900

    강원대_이진호_Step1 기능 구현 (#54)
    * docs(README): step1 구현 목록 작성
    
    * feat: 상품 entity 구현
    
    * feat: 단일 상품 조회 API 구현
    
    * feat: 전체 상품 조회 API 구현
    
    * refactor: ItemService 주입시 접근제어자 추가
    
    * docs(README): 추가 구현 기능 목록 추가
    
    * feat: 상품 생성 API 구현
    
    * feat: 상품 수정 API 구현
    
    * feat: 상품 삭제 API 구현
    
    * fix: 상품 수정 API 버그 수정
    
    * feat: NPE 예외처리 로직 구현
    
    - 기존 코드는 요청에 대한 자원의 존재 여부를 체크하지 않기 때문에 NPE 발생 가능성이 존재하였음.
    
    - 이를 전역 예외처리를 통해 처리하였음.
    
    * refactor: 예외 응답 dto의 필드명을 명확하게 변경
    
    * refactor: dto를 class에서 record로 변경

---
+++ src/main/java/gift/item/repository/ItemRepository.java
@@ -0,0 +1,18 @@
+package gift.item.repository;
+
+import gift.item.Item;
+import java.util.List;
+
+public interface ItemRepository {
+
+    Item findById(Long id);
+
+    List<Item> findAll();
+
+    Item save(Item item);
+
+    Item update(Item item);
+
+    void remove(Long id);
+
+}
