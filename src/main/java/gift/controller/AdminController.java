1fdd3eaefe84f7713a9e9d36206e95ecd4bb0866
doorcs <itsdoorcs@gmail.com>
2025-06-26 14:52:09 +0900

    feat: 상품 추가 관리자 페이지 구현

--- src/main/java/gift/controller/AdminController.java
+++ src/main/java/gift/controller/AdminController.java
@@ -5,8 +5,11 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import gift.dto.CreateProductRequest;
 import gift.dto.ReadProductResponse;
 import gift.service.ProductService;
 
@@ -25,5 +28,18 @@ public class AdminController {
         List<ReadProductResponse> products = productService.getAllProducts();
         model.addAttribute("products", products);
         return "admin";
+    }
+
+    @GetMapping("/add")
+    public String addPage() {
+        return "addProduct";
+    }
+
+    @PostMapping
+    public String addProduct(
+        @ModelAttribute CreateProductRequest request
+    ) {
+        productService.createProduct(request);
+        return "redirect:/admin/products";
     }
 }
