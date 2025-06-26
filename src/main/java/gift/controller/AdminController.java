024f5f8d0160162c310c7417751bb166ad4dac1d
doorcs <itsdoorcs@gmail.com>
2025-06-26 15:08:08 +0900

    feat: 상품 수정 관리자 페이지 구현

--- src/main/java/gift/controller/AdminController.java
+++ src/main/java/gift/controller/AdminController.java
@@ -6,11 +6,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import gift.dto.CreateProductRequest;
 import gift.dto.ReadProductResponse;
+import gift.dto.UpdateProductRequest;
 import gift.service.ProductService;
 
 @Controller
@@ -40,6 +42,25 @@ public class AdminController {
         @ModelAttribute CreateProductRequest request
     ) {
         productService.createProduct(request);
+        return "redirect:/admin/products";
+    }
+
+    @GetMapping("/{id}/edit")
+    public String editPage(
+        @PathVariable Long id,
+        Model model
+    ) {
+        ReadProductResponse product = productService.getProductById(id);
+        model.addAttribute("product", product);
+        return "editProduct";
+    }
+
+    @PostMapping("/{id}")
+    public String updateProduct(
+        @PathVariable Long id,
+        @ModelAttribute UpdateProductRequest request
+    ) {
+        productService.updateProduct(id, request);
         return "redirect:/admin/products";
     }
 }
