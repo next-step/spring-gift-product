eb737a804118fbdd8884d7ab4a5defedd0db5414
thdgmlwns1 <xhrl233@naver.com>
2025-06-26 13:46:29 +0900

    Feat: 상품 등록 화면

--- src/main/java/gift/controller/AdminProductController.java
+++ src/main/java/gift/controller/AdminProductController.java
@@ -1,10 +1,13 @@
 package gift.controller;
 
+import gift.dto.request.ProductRequestDto;
 import gift.entity.Product;
 import gift.service.ProductService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -24,4 +27,23 @@ public class AdminProductController {
         model.addAttribute("products", products);
         return "admin/product-list";
     }
+
+
+    /// /////////////등록///////////////
+
+    @GetMapping("/new")
+    public String  showCreateProductForm(Model model) {
+        model.addAttribute("product", new ProductRequestDto());
+        return "admin/product-form";
+    }
+
+    @PostMapping
+    public String addProduct(@ModelAttribute ProductRequestDto productRequestDto) {
+        productService.createProduct(productRequestDto);
+        return "redirect:/admin/products";
+    }
+
+
+
+
 }
