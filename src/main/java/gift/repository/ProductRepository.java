3bd8b144263f8da075032da6617c80e653f3069e
hhseo9519 <hhseo9519@naver.com>
2025-06-26 11:38:39 +0900

    최종수정

--- src/main/java/gift/repository/ProductRepository.java
+++ src/main/java/gift/repository/ProductRepository.java
@@ -5,26 +5,29 @@ import org.springframework.stereotype.Repository;
 
 import java.util.HashMap;
 import java.util.Map;
+
 @Repository
 public class ProductRepository {
-    private final Map<Long, Product> productMap = new HashMap<>();
-    private Long nextId=1L;
 
-    public Product addProduct(Product product){
+    private final Map<Long, Product> productMap = new HashMap<>();
+    private Long nextId = 1L;
+
+    public Product addProduct(Product product) {
         product.setId(nextId++);
         productMap.put(product.getId(), product);
         return product;
     }
 
-    public Product findProduct(Long id){
+    public Product findProduct(Long id) {
         return productMap.get(id);
     }
 
-    public Product updateProduct(Product product){
-        productMap.put(product.getId(),product);
+    public Product updateProduct(Product product) {
+        productMap.put(product.getId(), product);
         return product;
     }
-    public void deleteProduct(Long id){
+
+    public void deleteProduct(Long id) {
         productMap.remove(id);
     }
 }
