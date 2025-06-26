a2e002f3a2edc841e91c583e4bb2f47ff0d687ff
flareseek <flareseek@gmail.com>
2025-06-26 15:46:18 +0900

    feat(admin): 상품 update 추가

--- src/main/java/gift/controller/AdminController.java
+++ src/main/java/gift/controller/AdminController.java
@@ -1,11 +1,15 @@
 package gift.controller;
 
+import gift.dto.ProductResponseDTO;
 import gift.entity.Product;
 import gift.service.ProductService;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -30,5 +34,12 @@ public class AdminController {
         List<Product> products = productService.findAllProducts();
         model.addAttribute("products", products);
         return "admin/createProduct";
+    }
+
+    @GetMapping("/update/{id}")
+    public String update(@PathVariable Long id, Model model) {
+        ProductResponseDTO product = productService.findProductById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "id를 찾을 수 없습니다."));
+        model.addAttribute("product", product);
+        return "admin/updateProduct";
     }
 }
