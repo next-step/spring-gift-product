b633675b3c29c7e63dbf01b15c546f1a4f0e1aa6
xodbs126 <dltvmsskd@naver.com>
2025-06-26 12:48:59 +0900

    feat(template): HTML CSS 추가

--- src/main/java/gift/dto/ItemDTO.java
+++ src/main/java/gift/dto/ItemDTO.java
@@ -41,7 +41,9 @@ public class ItemDTO {
 
     /***
      * 왜 setter를 설정해놓으니까, HTML 폼에 저장이 될까???
+     * -> Spring은 @ModelAttribute 바인딩 시, setter를 사용하여 바인딩한다.
      *
+     * 그렇다면 왜 Lombok을 쓰지 말라고 하셨을까? 지금 lombok을 사용 안했을 뿐이지, 그냥 평소대로 Getter / Setter 남발하고 있는데
      */
     public void setId(Long id) {
         this.id = id;
