2e80bc29f64255b7a54ecafab4e654169b1c552c
treasure-sky <ych0486666@gmail.com>
2025-06-26 17:33:01 +0900

    refactor: 관리자 API 일관성 개선
    - /admin/items 하위로 URL 패턴을 통일하였음.
    - form에서 HTTP 메서드 오버라이드가 가능하도록 하였음.
      - 상품 수정 시 POST -> PUT 변경
      - 상품 삭제 시 POST -> DELETE 변경

--- src/main/java/gift/admin/AdminItemController.java
+++ src/main/java/gift/admin/AdminItemController.java
@@ -7,12 +7,16 @@ import gift.item.service.ItemService;
 import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/admin/items")
 public class AdminItemController {
 
     private final ItemService itemService;
@@ -23,14 +27,14 @@ public class AdminItemController {
 
     // 페이지 뷰 반환
 
-    @GetMapping("/admin/items")
+    @GetMapping()
     public String adminItemPage(Model model) {
         List<ItemResponseDto> items = itemService.findAll();
         model.addAttribute("items", items);
         return "admin-item";
     }
 
-    @GetMapping("/admin/items/new")
+    @GetMapping("/new")
     public String newItemPage(Model model) {
         // dto가 record 형태라 생성시 값이 필요함.
         // 임의의 데이터를 넣어도 클라이언트에서 제출 시 새로운 객체가 생성됨.
@@ -38,7 +42,7 @@ public class AdminItemController {
         return "admin-item-new";
     }
 
-    @GetMapping("/admin/items/update/{id}")
+    @GetMapping("/update/{id}")
     public String updateItemPage(@PathVariable("id") Long id, Model model) {
         ItemResponseDto item = itemService.findItem(id);
         ItemUpdateDto updateDto = new ItemUpdateDto(item.name(), item.price(), item.imageUrl());
@@ -49,13 +53,13 @@ public class AdminItemController {
 
     // 상품 요청 처리
 
-    @PostMapping("/admin/items")
+    @PostMapping()
     public String newItem(@ModelAttribute ItemCreateDto item) {
         itemService.createItem(item);
         return "redirect:/admin/items";
     }
 
-    @PostMapping("/admin/items/{id}")
+    @PutMapping("/{id}")
     public String updateItem(
         @PathVariable("id") Long id,
         @ModelAttribute ItemUpdateDto item
@@ -64,7 +68,7 @@ public class AdminItemController {
         return "redirect:/admin/items";
     }
 
-    @PostMapping("/admin/items/{id}/delete")
+    @DeleteMapping("/{id}")
     public String deleteItem(@PathVariable("id") Long id) {
         itemService.deleteItem(id);
         return "redirect:/admin/items";
