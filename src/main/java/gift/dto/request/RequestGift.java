a6b60b3d6b7c75760fd3cff10768d79d21b23ca3
Sunwoo-An <110546006+Sunja-An@users.noreply.github.com>
2025-06-26 17:49:19 +0900

    Feat: Gift Dto 피드백에 따른 수정

--- src/main/java/gift/dto/request/RequestGift.java
+++ src/main/java/gift/dto/request/RequestGift.java
@@ -3,8 +3,9 @@ package gift.dto.request;
 import gift.entity.Gift;
 
 public record RequestGift(Long id, String name, Integer price, String imageUrl) {
-    public static Gift from (RequestGift requestGift) {
+    public static Gift toEntity (RequestGift requestGift) {
         return new Gift(
+                requestGift.id(),
                 requestGift.name(),
                 requestGift.price(),
                 requestGift.imageUrl()
