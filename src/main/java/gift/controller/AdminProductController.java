3f89f625fc72884e65ab0b3d9bb4e9c58a7b6491
kimpro4214 <qnfghk555@naver.com>
2025-06-26 15:21:46 +0900

    feat : 관리자 상품 수정 처리 구현

--- src/main/java/gift/controller/AdminProductController.java
+++ src/main/java/gift/controller/AdminProductController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 
 import java.util.List;
@@ -40,5 +41,21 @@ public class AdminProductController {
         productService.addProduct(requestDto);
         return "redirect:/admin/products";
     }
+
+    @GetMapping("/{id}/edit")
+    public String showEditForm(@PathVariable Long id, Model model) {
+        ProductResponseDto product = productService.getProductById(id);
+        model.addAttribute("product", product);
+        return "admin/edit";
+    }
+
+    @PostMapping("/{id}/edit")
+    public String updateProduct(@PathVariable Long id,
+                                @ModelAttribute ProductRequestDto requestDto) {
+        productService.updateProduct(id, requestDto);
+        return "redirect:/admin/products";
+    }
+
+
 
 }
