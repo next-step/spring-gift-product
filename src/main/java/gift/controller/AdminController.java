e12841a5a8b78459019e0b0497a0ffa56e3913e7
jyhyt1567 <jyhyt1567@gmail.com>
2025-06-26 14:00:18 +0900

    feat: 관리자 화면에 상품 등록 기능 구현

--- src/main/java/gift/controller/AdminController.java
+++ src/main/java/gift/controller/AdminController.java
@@ -1,15 +1,19 @@
 package gift.controller;
 
+import gift.dto.CreateProductRequestDto;
 import gift.dto.ProductResponseDto;
 import gift.service.ProductService;
 import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/admin")
+@RequestMapping("/admin/boards")
 public class AdminController {
 
     private final ProductService productService;
@@ -22,6 +26,21 @@ public class AdminController {
     public String showAdminPage(Model model) {
         List<ProductResponseDto> products = productService.findAllProducts();
         model.addAttribute("products", products);
-        return "admin";
+        return "dashboard";
     }
+
+    @GetMapping("/add")
+    public String showCreatePage(Model model) {
+        CreateProductRequestDto requestDto = new CreateProductRequestDto();
+        model.addAttribute("requestDto", requestDto);
+        return "createForm";
+    }
+
+    @PostMapping
+    public String createProduct(@ModelAttribute CreateProductRequestDto requestDto) {
+        System.out.println(requestDto.getName());
+        productService.createProduct(requestDto);
+        return "redirect:/admin/boards";
+    }
+
 }
--- src/main/java/gift/dto/CreateProductRequestDto.java
+++ src/main/java/gift/dto/CreateProductRequestDto.java
@@ -10,11 +10,23 @@ public class CreateProductRequestDto {
         return name;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public Long getPrice() {
         return price;
     }
 
+    public void setPrice(Long price) {
+        this.price = price;
+    }
+
     public String getImageUrl() {
         return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
     }
 }
