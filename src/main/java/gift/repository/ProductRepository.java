1637143d7fab4cef5f283d1f9660e76712d52837
YoonGangHoon <yghun021007@naver.com>
2025-06-26 17:03:37 +0900

    fix: git 로그 문제로 변경된 파일 수정

--- src/main/java/gift/repository/ProductRepository.java
+++ src/main/java/gift/repository/ProductRepository.java
@@ -1,20 +1,65 @@
 package gift.repository;
 
-import gift.dto.ProductResponseDto;
 import gift.entity.Product;
 import org.springframework.stereotype.Repository;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 @Repository
 public class ProductRepository {
+
     private final Map<Long, Product> products = new HashMap<>();
     Long nextId = 1L;
 
-    public ProductResponseDto save(Product product) {
-        Long id  =  nextId++;
+    public Product save(Product product) {
+
+        Long id = nextId++;
+        product.setId(id);
         products.put(id, product);
-        return new ProductResponseDto(id, product.getName(), product.getPrice(), product.getImageUrl());
+
+        return product;
+    }
+
+    public Optional<Product> findById(Long productId) {
+        return Optional.ofNullable(products.get(productId));
+    }
+
+    public Optional<Product> update(Long id, String name, Integer price, String imageUrl) {
+        Product existingProduct = products.get(id);
+        if (existingProduct == null) {
+            return Optional.empty();
+        }
+
+        existingProduct.setName(name);
+        existingProduct.setPrice(price);
+        existingProduct.setImageUrl(imageUrl);
+
+        return Optional.of(existingProduct);
+    }
+
+    public Optional<Product> deleteById(Long id) {
+        return Optional.ofNullable(products.remove(id));
+    }
+
+    public List<Product> findAll(int page, int size, String sortField, String sortDir) {
+        Comparator<Product> comparator;
+
+        if ("name".equals(sortField)) {
+            comparator = Comparator.comparing(Product::getName, String.CASE_INSENSITIVE_ORDER);
+        } else if ("price".equals(sortField)) {
+            comparator = Comparator.comparing(Product::getPrice);
+        } else {
+            comparator = Comparator.comparing(Product::getId);
+        }
+
+        if ("desc".equalsIgnoreCase(sortDir)) {
+            comparator = comparator.reversed();
+        }
+
+        return products.values().stream()
+                .sorted(comparator)
+                .skip((long) page * size) // 페이지 지정
+                .limit(size) // 개수 지정
+                .toList();
     }
 }
