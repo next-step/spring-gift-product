ecf3af15abf33c74509b4879e2f59d889460a365
ji-mim <194115@jnu.ac.kr>
2025-06-26 13:39:22 +0900

    Feat(수정, 삭제 기능): 컨트롤러와 템플릿에 수정, 삭제 관련된 기능 추가

--- src/main/java/gift/controller/ProductController.java
+++ src/main/java/gift/controller/ProductController.java
@@ -3,15 +3,12 @@ package gift.controller;
 
 import gift.domain.Product;
 import gift.dto.CreateProductRequest;
+import gift.dto.UpdateProductRequest;
 import gift.service.ProductService;
 import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/api/products")
@@ -37,8 +34,27 @@ public class ProductController {
 
 
     @PostMapping("/add")
-    public String addProduct(@ModelAttribute CreateProductRequest request, RedirectAttributes redirectAttributes) {
+    public String addProduct(@ModelAttribute CreateProductRequest request) {
         service.save(request);
+        return "redirect:/api/products";
+    }
+
+    @GetMapping("/{id}/edit")
+    public String updateProduct(@PathVariable Long id, Model model) {
+        Product findProduct = service.findById(id);
+        model.addAttribute("product", findProduct);
+        return "/editForm";
+    }
+
+    @PostMapping("/{id}/edit")
+    public String updateProduct(@PathVariable Long id, @ModelAttribute UpdateProductRequest request) {
+        service.update(id, request);
+        return "redirect:/api/products";
+    }
+
+    @PostMapping("/{id}/delete")
+    public String deleteProduct(@PathVariable Long id) {
+        service.delete(id);
         return "redirect:/api/products";
     }
 }
