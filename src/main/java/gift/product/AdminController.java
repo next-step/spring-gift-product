37c4bc6276b070e7fd14bb0afc04607f62e863e8
Lee Si Woong <silverttthin@naver.com>
2025-06-26 15:12:28 +0900

    feat: 관리자페이지 생성, 수정 구현 - 생성과 수정기능은 입력으로 들어가는 아이템의 pk의 유무만 다르고 나머진 거의 동일 - 따라서 thymeleaf html에서 editId 데이터에 따라 생성과 수정의 로직을 이분했음

--- src/main/java/gift/product/AdminController.java
+++ src/main/java/gift/product/AdminController.java
@@ -3,8 +3,7 @@ package gift.product;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,12 +18,45 @@ public class AdminController {
 		this.itemService = itemService;
 	}
 
-	/** 1) 목록 페이지 */
+	// 1. 목록 조회
 	@GetMapping
 	public String list(Model model) {
 		List<Item> products = itemService.getAllItems();
 		model.addAttribute("products", products);
 		return "list";
 	}
+
+	// 2. 게시글 생성 페이지 이동
+	@GetMapping("/new")
+	public String createPage(Model model) {
+		// 페이지 이동 시 itemRequest 빈 객체를 같이 갖고 들어감
+		model.addAttribute("itemRequest", new ItemRequest("",0,""));
+		return "form";
+	}
+
+	// 3. 게시글 등록처리
+	@PostMapping
+	public String create(@ModelAttribute ItemRequest req) {
+		itemService.createItem(req);
+		return "redirect:/admin/products";
+	}
+
+	// 4. 게시글 수정 페이지 이동
+	// 생성과 동일한 form.html로 이동하지만 이미 존재하는 아이템값을 서비스에서 가져와 모델에 박아넣고 이동하는 중요한 차이점 존재
+	@GetMapping("/{id}/edit")
+	public String edit(@PathVariable Long id, Model model) {
+		Item item = itemService.getItem(id);
+		model.addAttribute("itemRequest", new ItemRequest(item.getName(), item.getPrice(), item.getImageUrl()));
+		model.addAttribute("editId", item.getId()); // 생성인지 수정인지를 가르는 키 값
+		return "form";
+	}
+
+	// 5. 게시글 수정처리
+	@PostMapping("/{id}/edit")
+	public String update(@PathVariable Long id, @ModelAttribute ItemRequest req) {
+		itemService.updateItem(id, req);
+		return "redirect:/admin/products";
+	}
+
 
 }
