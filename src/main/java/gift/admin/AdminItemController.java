ce53d170ca49410882090883ddbf7514c3612849
treasure-sky <ych0486666@gmail.com>
2025-06-26 15:38:49 +0900

    feat: 관리자 페이지 추가
    - 관리자 페이지 접속시 전체 아이템 리스트를 볼 수 있도록 구현하였음.

---
+++ src/main/java/gift/admin/AdminItemController.java
@@ -0,0 +1,25 @@
+package gift.admin;
+
+import gift.item.dto.ItemResponseDto;
+import gift.item.service.ItemService;
+import java.util.List;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AdminItemController {
+
+    private final ItemService itemService;
+
+    public AdminItemController(ItemService itemService) {
+        this.itemService = itemService;
+    }
+
+    @GetMapping("/admin/item")
+    public String adminItemPage(Model model) {
+        List<ItemResponseDto> items = itemService.findAll();
+        model.addAttribute("items", items);
+        return "admin-item";
+    }
+}
