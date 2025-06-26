4f59213dd0e7ed9f8008ea26d47019ceda35bb4d
HJH <jijinny26@gmail.com>
2025-06-26 17:18:34 +0900

    feat(admin): 상품 추가 기능 추가

--- src/main/java/gift/controller/AdminController.java
+++ src/main/java/gift/controller/AdminController.java
@@ -1,16 +1,17 @@
 package gift.controller;
 
+import gift.dto.ProductRequestDto;
 import gift.dto.ProductResponseDto;
+import gift.entity.Product;
 import gift.service.ProductService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @Controller
-@RequestMapping("/admin/products")
+@RequestMapping("/admin")
 public class AdminController {
     private final ProductService productService;
 
@@ -23,6 +24,18 @@ public class AdminController {
         List<ProductResponseDto> ProductResponseDtoList = productService.findAllProducts();
         model.addAttribute("products", ProductResponseDtoList);
         return "admin/product-list";
+    }
+
+    @GetMapping("/products-add")
+    public String showAddProductForm(Model model) {
+        model.addAttribute("product", new Product());
+        return "admin/product-add";
+    }
+
+    @PostMapping("/products-add")
+    public String createProduct(@ModelAttribute ProductRequestDto dto) {
+        productService.saveProduct(dto);
+        return "redirect:/admin";
     }
 
 }
