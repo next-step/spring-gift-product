39a3a66edebd515c07cc53d62341cec608be8111
treasure-sky <ych0486666@gmail.com>
2025-06-26 16:30:55 +0900

    feat: 관리자 페이지에서 상품 생성 기능 추가
    - 관리자 상품 생성 페이지를 추가하였음.
    - 관리자 페이지에서 위 페이지로 이동하는 링크를 추가하였음.

--- src/main/java/gift/admin/AdminItemController.java
+++ src/main/java/gift/admin/AdminItemController.java
@@ -1,11 +1,14 @@
 package gift.admin;
 
+import gift.item.dto.ItemCreateDto;
 import gift.item.dto.ItemResponseDto;
 import gift.item.service.ItemService;
 import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class AdminItemController {
@@ -16,10 +19,29 @@ public class AdminItemController {
         this.itemService = itemService;
     }
 
+    // 페이지 뷰 반환
+
     @GetMapping("/admin/items")
     public String adminItemPage(Model model) {
         List<ItemResponseDto> items = itemService.findAll();
         model.addAttribute("items", items);
         return "admin-item";
     }
+
+    @GetMapping("admin/items/new")
+    public String newItemPage(Model model) {
+        // dto가 record 형태라 생성시 값이 필요함.
+        // 임의의 데이터를 넣어도 클라이언트에서 제출 시 새로운 객체가 생성됨.
+        model.addAttribute("item", new ItemCreateDto("", 0, ""));
+        return "admin-item-new";
+    }
+
+    // 상품 요청 처리
+
+    @PostMapping("/admin/items")
+    public String newItem(@ModelAttribute ItemCreateDto item) {
+        itemService.createItem(item);
+        return "redirect:/admin/items";
+    }
+
 }
