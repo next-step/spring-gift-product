6e3a6e8644764ed262da7acf5a3952664d7d80f5
flareseek <flareseek@gmail.com>
2025-06-26 15:24:37 +0900

    feat(admin): 상품 등록 페이지 추가

---
+++ src/main/java/gift/controller/AdminController.java
@@ -0,0 +1,27 @@
+package gift.controller;
+
+import gift.entity.Product;
+import gift.service.ProductService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+    private final ProductService productService;
+
+    public AdminController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping("/create")
+    public String create(Model model) {
+        List<Product> products = productService.findAllProducts();
+        model.addAttribute("products", products);
+        return "admin/createProduct";
+    }
+}
