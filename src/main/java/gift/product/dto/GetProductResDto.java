dc082f4be2a237b75fcde28d197b839da14aef43
lwj0831 <wjl0831@gmail.com>
2025-06-26 16:40:06 +0900

    refactor(product): GetProductResDto에 price 추가

--- src/main/java/gift/product/dto/GetProductResDto.java
+++ src/main/java/gift/product/dto/GetProductResDto.java
@@ -5,10 +5,11 @@ import gift.product.domain.Product;
 public record GetProductResDto (
     Long id,
     String name,
+    int price,
     String description
 ) {
 
     public static GetProductResDto from(Product product) {
-        return new GetProductResDto(product.getId(), product.getName(), product.getDescription());
+        return new GetProductResDto(product.getId(), product.getName(), product.getPrice(), product.getDescription());
     }
 }
