f3a3ebe76b8fedadddee2ad178ae9cec4bbe7489
Lee Si Woong <silverttthin@naver.com>
2025-06-26 16:00:43 +0900

    refactor: db 자료구조를 해시맵으로 수정

--- src/main/java/gift/product/ItemService.java
+++ src/main/java/gift/product/ItemService.java
@@ -5,58 +5,58 @@ import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 
 @Service
 public class ItemService {
 
-	private final List<Item> db = new ArrayList<>();
+	private final Map<Long, Item> db = new HashMap<>();
 
 	// 테스트위해 서버 재실행마다 항상 2개는 채워놓기
 	@PostConstruct
 	public void init() {
-		db.add(new Item("egg", 7900, "url1"));
-		db.add(new Item("Nvidia Geforce 5060ti", 700000, "url2"));
+		Item item1 = new Item("egg", 7900, "url1");
+		Item item2 = new Item("Nvidia Geforce 5060ti", 700000, "url2");
+
+		db.put(item1.getId(), item1);
+		db.put(item2.getId(), item2);
 	}
 
 	public Long createItem(ItemRequest req) {
 		Item item = new Item(req.name(), req.price(), req.imageUrl());
-		db.add(item);
+		db.put(item.getId(), item);
 		return item.getId();
 	}
 
 	public List<Item> getAllItems() {
-		return db;
+		return db.values().stream().toList();
 	}
 
 
 	public Item getItem(Long itemId) {
-		return db.stream()
-			.filter(item -> item.getId() == itemId)
-			.findFirst()
-			.orElseThrow(() -> new RuntimeException("해당 ID는 존재하지 않습니다"));
+		Item item = db.get(itemId);
+		if (item == null) {
+			throw new RuntimeException("해당 ID는 존재하지 않습니다!");
+		}
+		return item;
 	}
 
 
 	public Item updateItem(Long itemId, ItemRequest req) {
-		return db.stream()
-			.filter(item -> item.getId() == itemId)
-			.findFirst()
-			.map(item -> {
-				item.setName(req.name());
-				item.setPrice(req.price());
-				item.setImageUrl(req.imageUrl());
-				return item;
-			})
-			.orElseThrow(() -> new RuntimeException("해당 ID는 존재하지 않습니다"));
+		Item item = getItem(itemId);
+		item.setName(req.name());
+		item.setPrice(req.price());
+		item.setImageUrl(req.imageUrl());
+		return item;
 	}
 
 
 	public void deleteItem(Long itemId) {
-		boolean removed = db.removeIf(item -> item.getId() == itemId);
-		if (!removed)
-			throw new RuntimeException("해당 ID는 존재하지 않습니다.");
+		Item item = getItem(itemId);
+		db.remove(itemId);
 	}
 
 }
