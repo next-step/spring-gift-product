0b4eade09c6c68323b39a485e9e4f780c9c163c3
장완중 <tjdrj530@naver.com>
2025-06-26 15:16:01 +0900

    feat : 관리자페이지 상품 삭제 추가

--- src/main/resources/static/admin.js
+++ src/main/resources/static/admin.js
@@ -22,3 +22,21 @@ document.getElementById("create-product-form").addEventListener("submit", async 
         alert("등록에 실패했습니다.");
     }
 });
+
+document.getElementById("delete-btn").addEventListener('click', async function (event) {
+
+    const productId = event.target.dataset.productId;
+    confirm(`정말로 ${productId}번 상품을 삭제하시겠습니까?`);
+
+    const response = await fetch(`/api/products/${productId}`, {
+        method: "DELETE",
+    })
+
+    if (response.ok) {
+        alert("상품이 삭제되었습니다.");
+        window.location.reload();
+    } else {
+        alert("삭제를 실패했습니다.");
+    }
+
+});
