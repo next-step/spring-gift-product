21a77ce4d7cfc959aff501e7924c5512fb973930
YoonGangHoon <yghun021007@naver.com>
2025-06-26 16:36:19 +0900

    refactor: ModelAndView 객체 사용

--- src/main/java/gift/controller/ProductViewController.java
+++ src/main/java/gift/controller/ProductViewController.java
@@ -3,8 +3,11 @@ package gift.controller;
 import gift.dto.ProductRequestDto;
 import gift.service.ProductService;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/products")
@@ -17,44 +20,48 @@ public class ProductViewController {
     }
 
     @GetMapping
-    public String list(Model model) {
-        model.addAttribute("products", productService.findAll(0, 100, "id, asc"));
-        return "product/list";
+    public ModelAndView list() {
+        Map<String, Object> model = new HashMap<>();
+        model.put("products", productService.findAll(0, 100, "id, asc"));
+        return new ModelAndView("product/list", model);
     }
 
     @GetMapping("/new")
-    public String createForm(Model model) {
-        model.addAttribute("product", new ProductRequestDto());
-        return "product/create";
+    public ModelAndView createForm() {
+        Map<String, Object> model = new HashMap<>();
+        model.put("product", new ProductRequestDto());
+        return new ModelAndView("product/create", model);
     }
 
     @PostMapping
-    public String create(@ModelAttribute ProductRequestDto requestDto) {
+    public ModelAndView create(@ModelAttribute ProductRequestDto requestDto) {
         productService.create(requestDto);
-        return "redirect:/products";
+        return new ModelAndView("redirect:/products");
     }
 
     @GetMapping("/{id}")
-    public String detail(@PathVariable Long id, Model model) {
-        model.addAttribute("product", productService.find(id));
-        return "product/detail";
+    public ModelAndView detail(@PathVariable Long id) {
+        Map<String, Object> model = new HashMap<>();
+        model.put("product", productService.find(id));
+        return new ModelAndView("product/detail", model);
     }
 
     @GetMapping("/edit/{id}")
-    public String editForm(@PathVariable Long id, Model model) {
-        model.addAttribute("product", productService.find(id));
-        return "product/edit";
+    public ModelAndView editForm(@PathVariable Long id) {
+        Map<String, Object> model = new HashMap<>();
+        model.put("product", productService.find(id));
+        return new ModelAndView("product/edit", model);
     }
 
     @PostMapping("/{id}")
-    public String update(@PathVariable Long id, @ModelAttribute ProductRequestDto requestDto) {
+    public ModelAndView update(@PathVariable Long id, @ModelAttribute ProductRequestDto requestDto) {
         productService.update(id, requestDto);
-        return "redirect:/products";
+        return new ModelAndView("redirect:/products");
     }
 
     @PostMapping("/{id}/delete")
-    public String delete(@PathVariable Long id) {
+    public ModelAndView delete(@PathVariable Long id) {
         productService.delete(id);
-        return "redirect:/products";
+        return new ModelAndView("redirect:/products");
     }
 }
