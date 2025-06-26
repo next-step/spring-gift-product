8c141a39f06531fb4b21a7d651c08dae5adad46d
kwh <105961568+kmwh@users.noreply.github.com>
2025-06-26 20:22:51 +0900

    강원대 BE 김원호 2단계 - 관리자 화면 (#124)
    * chore: 1단계 리뷰 수정 반영
    
    * refactor: ID 자동 생성 방식으로 수정
    
    * feat: 관리 페이지 html 추가
    
    * feat: 관리 페이지 컨트롤러 추가
    
    * feat: 생성 및 수정 페이지 html 추가
    
    * fix: 표기, id 겹침, redirect 버그 수정
    
    * docs: 2단계 구현 기능 목록 정리
    
    * style: 테이블 디자인 수정
    
    * style: 테이블 헤더, ID 디자인 수정

--- src/main/java/gift/repository/ProductRepositoryImpl.java
+++ src/main/java/gift/repository/ProductRepositoryImpl.java
@@ -3,10 +3,12 @@ package gift.repository;
 import gift.dto.ProductRequestDto;
 import gift.dto.ProductResponseDto;
 import gift.entity.Product;
+import gift.exception.ProductNotFoundException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.server.ResponseStatusException;
@@ -14,10 +16,12 @@ import org.springframework.web.server.ResponseStatusException;
 @Repository
 public class ProductRepositoryImpl implements ProductRepository {
     private final Map<Long, Product> products = new HashMap<>();
+    private final Random random = new Random();
+    private Long nextId = 1L;
 
     @Override
     public ProductResponseDto createProduct(ProductRequestDto requestDto) {
-        Long id = requestDto.id();
+        Long id = nextId++; // jpa 사용 전까지 임시 번호 부여(여러 스레드 사용시 문제 발생 가능)
         String name = requestDto.name();
         Integer price = requestDto.price();
         String imageUrl = requestDto.imageUrl();
@@ -41,6 +45,9 @@ public class ProductRepositoryImpl implements ProductRepository {
     @Override
     public ProductResponseDto findProductById(Long id) {
         Product product = products.get(id);
+        if (product == null) {
+            throw new ProductNotFoundException(id);
+        }
 
         return ProductResponseDto.from(product);
     }
@@ -60,7 +67,7 @@ public class ProductRepositoryImpl implements ProductRepository {
     @Override
     public void deleteProduct(Long id) {
         if (products.remove(id) == null) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+            throw new ProductNotFoundException(id);
         }
     }
 }
