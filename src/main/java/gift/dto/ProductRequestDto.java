3113f5d51de97e8524b70235e9b98a9291ffd62b
HJH <jijinny26@gmail.com>
2025-06-26 15:55:02 +0900

    refactor(product): Step1 코드 리뷰 반영

--- src/main/java/gift/dto/ProductRequestDto.java
+++ src/main/java/gift/dto/ProductRequestDto.java
@@ -2,41 +2,8 @@ package gift.dto;
 
 import gift.entity.Product;
 
-public class ProductRequestDto {
-    private String name;
-    private Long price;
-    private String imageUrl;
-
-    public ProductRequestDto() {}
-
+public record ProductRequestDto(String name, Long price, String imageUrl) {
     public ProductRequestDto(Product product) {
-        this.name = product.getName();
-        this.price = product.getPrice();
-        this.imageUrl = product.getImageUrl();
+        this(product.getName(), product.getPrice(), product.getImageUrl());
     }
-
-    public String getName() {
-        return name;
-    }
-
-    public Long getPrice() {
-        return price;
-    }
-
-    public String getImageUrl() {
-        return imageUrl;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setPrice(Long price) {
-        this.price = price;
-    }
-
-    public void setImageUrl(String imageUrl) {
-        this.imageUrl = imageUrl;
-    }
-
 }
--- src/main/java/gift/service/ProductService.java
+++ src/main/java/gift/service/ProductService.java
@@ -2,12 +2,10 @@ package gift.service;
 
 import gift.dto.ProductRequestDto;
 import gift.dto.ProductResponseDto;
-
 import java.util.List;
 
 public interface ProductService {
     List<ProductResponseDto> findAllProducts();
-
     ProductResponseDto saveProduct(ProductRequestDto dto);
     ProductResponseDto findProductById(Long id);
     ProductResponseDto updateProduct(Long id, ProductRequestDto dto);
