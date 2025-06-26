f00a822724e209da937ceac64adcb8de2a10b29c
oh010 <oh010224@gmail.com>
2025-06-26 14:59:20 +0900

    feat: 상품 전체 목록 조회 화면 구성

--- src/main/java/gift/ProductController.java
+++ src/main/java/gift/ProductController.java
@@ -1,6 +1,8 @@
 package gift;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -8,20 +10,22 @@ import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicLong;
 
-@RestController
+@Controller
 @RequestMapping("/api")
 public class ProductController {
     private final Map<Long, Product> products = new ConcurrentHashMap<>();
     private final AtomicLong idGenerator = new AtomicLong(1);
 
+    @ResponseBody
     @PostMapping("/product")
-    public String createProduct(@RequestBody ProductDTO productdto) {
+    public Product createProduct(@RequestBody ProductDTO productdto) {
         long id = idGenerator.getAndIncrement();
         Product product = new Product(id, productdto);
         products.put(product.getId(), product);
-        return "상품 생성: " + product.getId();
+        return product;
     }
 
+    @ResponseBody
     @GetMapping("/product/{id}")
     public Product getProduct(@PathVariable Long id) {
         Product product = products.get(id);
@@ -29,11 +33,13 @@ public class ProductController {
     }
 
     @GetMapping("/products")
-    public Collection<Product> getAllProducts() {
+    public String listProducts(Model model) {
         Collection<Product> productList = products.values();
-        return productList;
+        model.addAttribute("products", productList);
+        return "products";
     }
 
+    @ResponseBody
     @PatchMapping("/product/{id}")
     public Product updateProduct(@PathVariable Long id, @RequestBody ProductDTO updateProductdto) {
         if(!products.containsKey(id)) {
@@ -45,11 +51,10 @@ public class ProductController {
     }
 
     @DeleteMapping("/product/{id}")
-    public String deleteProduct(@PathVariable Long id) {
+    public void deleteProduct(@PathVariable Long id) {
         if(!products.containsKey(id)) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "해당 데이터가 존재하지 않습니다.");
         }
         products.remove(id);
-        return "상품 삭제: " + id;
     }
 }
