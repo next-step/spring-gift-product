e2437465f1e49e19fc8d4400898849f290c3eeaa
Kiyeong Choi <132523350+gogumalatte@users.noreply.github.com>
2025-06-28 01:52:21 +0900

    경북대 BE_최기영 2단계 - 관리자 화면 (#115)
    * docs(README): 1단계 상품 API 요구사항 세부화 및 구현 제약사항 명시
    
    * refactor: POST 상품 생성 응답에 Location 헤더 추가
    
    * refactor: 상품 엔티티에서 코드 중복 제거
    
    * docs(README): 상품 관리자 화면 구현 요구 사항 작성
    
    * feat: 상품 목록 관리 페이지 구현
    
    * feat: 상품 등록 폼 구현
    
    * feat: 상품 상세 조회 페이지 구현
    
    * feat: 상품 수정 폼 및 기능 구현
    
    * feat: 상품 삭제 기능 구현
    
    * docs(README): 완료한 기능 체크
    
    * fix: 유효성 검사의 책임 명확화

--- src/main/java/gift/entity/Item.java
+++ src/main/java/gift/entity/Item.java
@@ -8,11 +8,8 @@ public class Item {
     private String imageUrl;
 
     public Item(Long id, String name, int price, String imageUrl) {
-        if (name == null || name.trim().isEmpty()) {
-            throw new IllegalArgumentException("상품 이름은 비어있을 수 없습니다.");
-        }
-        if (price < 0) {
-            throw new IllegalArgumentException("가격은 음수일 수 없습니다.");
+        if (id == null) {
+            throw new IllegalArgumentException("상품 ID는 비어있을 수 없습니다.");
         }
         this.id = id;
         this.name = name;
@@ -37,12 +34,6 @@ public class Item {
     }
 
     public void updateItemInfo(String name, int price, String imageUrl) {
-        if (name == null || name.trim().isEmpty()) {
-            throw new IllegalArgumentException("상품 이름은 비어 있을 수 없습니다.");
-        }
-        if (price < 0) {
-            throw new IllegalArgumentException("가격은 음수일 수 없습니다.");
-        }
         this.name = name;
         this.price = price;
         this.imageUrl = imageUrl;
