0c99a314af2a94a9d1081fc46ceb6d4982647e14
ming0511 <alsrbdhkalsw@naver.com>
2025-06-27 19:53:19 +0900

    refactor: imageUrl Not Null 추가(H2 DB)

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -2,5 +2,5 @@ CREATE TABLE products (
                           productId BIGINT PRIMARY KEY AUTO_INCREMENT,
                           name VARCHAR(255) NOT NULL,
                           price INT NOT NULL,
-                          imageUrl VARCHAR(1000)
+                          imageUrl VARCHAR(1000) NOT NULL
 );
