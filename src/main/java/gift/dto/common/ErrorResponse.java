69c5aa34c1fdd35ad1f9fac693f3abb1c65b9d55
KNU-K <knukang334@gmail.com>
2025-06-27 16:42:39 +0900

    refactor: ErrorResponse에서 errorCode 필드 제거

--- src/main/java/gift/dto/common/ErrorResponse.java
+++ src/main/java/gift/dto/common/ErrorResponse.java
@@ -2,7 +2,7 @@ package gift.dto.common;
 
 import java.time.LocalDateTime;
 
-public record ErrorResponse(LocalDateTime timestamp, String errorCode, String message,
+public record ErrorResponse(LocalDateTime timestamp, String message,
                             String trace) {
 
 }
