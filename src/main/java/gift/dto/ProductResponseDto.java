4fbcfe33997f652ea147bb8df40bce3cd280180b
eastgyu11 <eastgyu11>
2025-06-27 20:48:04 +0900

    feat: jdbcrepository 모든 상품 조회 기능 추가

--- src/main/java/gift/dto/ProductResponseDto.java
+++ src/main/java/gift/dto/ProductResponseDto.java
@@ -16,6 +16,13 @@ public class ProductResponseDto {
     this.imageUrl = product.getImageUrl();
   }
 
+  public ProductResponseDto(Long id, String name, Long price, String imageUrl) {
+    this.id = id;
+    this.name = name;
+    this.price = price;
+    this.imageUrl = imageUrl;
+  }
+
   public Long getId() {
     return id;
   }
--- src/main/java/gift/repository/ProductJdbcRepositoryImpl.java
+++ src/main/java/gift/repository/ProductJdbcRepositoryImpl.java
@@ -3,12 +3,29 @@ package gift.repository;
 import gift.dto.ProductRequestDto;
 import gift.dto.ProductResponseDto;
 import java.util.List;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class ProductJdbcRepositoryImpl implements ProductRepository {
+
+  private JdbcTemplate jdbcTemplate;
+
+  public ProductJdbcRepositoryImpl(JdbcTemplate jdbcTemplate) {
+    this.jdbcTemplate = jdbcTemplate;
+  }
 
   @Override
   public List<ProductResponseDto> findAllProduct() {
-    return List.of();
+    String sql = "select * from products";
+    return jdbcTemplate.query(sql, (rs, rowNum) ->
+        new ProductResponseDto(
+            rs.getLong("id"),
+            rs.getString("name"),
+            rs.getLong("price"),
+            rs.getString("imageUrl")
+        )
+    );
   }
 
   @Override
