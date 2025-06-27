614d17847199d987d4e36aa9f99eeef76b6ef1d8
khwon0407 <126738028+khwon0407@users.noreply.github.com>
2025-06-27 15:38:18 +0900

    feat: db 설정, repository layer hashmap을 db로 교체

--- src/main/java/gift/repository/ProductRepositoryImpl.java
+++ src/main/java/gift/repository/ProductRepositoryImpl.java
@@ -4,67 +4,115 @@ import gift.dto.AddProductResponseDto;
 import gift.dto.FindProductResponseDto;
 import gift.dto.ModifyProductResponseDto;
 import gift.entity.Product;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.Optional;
 import org.springframework.http.HttpStatus;
+import org.springframework.jdbc.core.simple.JdbcClient;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.server.ResponseStatusException;
 
 @Repository
 public class ProductRepositoryImpl implements ProductRepository {
     
-    //DB 역할을 해줄 Map
-    private final Map<Long, Product> products = new HashMap<>();
+    //DB
+    private final JdbcClient products;
+    private static Long recentId = 0L;
+    
+    //생성자 주입
+    public ProductRepositoryImpl(JdbcClient products) {
+        this.products = products;
+    }
     
     @Override
     public AddProductResponseDto addProduct(Product product) {
+        var sql = """
+            insert into products(name, price, imageUrl)
+            values (:name, :price, :imageUrl);
+            """;
         
-        Long dbId = product.getId();
-        products.put(dbId, product);
+        products.sql(sql)
+            .param("name", product.getName())
+            .param("price", product.getPrice())
+            .param("imageUrl", product.getImageUrl())
+            .update();
+        
+        recentId++;
         
         return new AddProductResponseDto(product);
     }
     
     @Override
     public List<FindProductResponseDto> findAllProducts() {
-        List<FindProductResponseDto> responseDtoList = new ArrayList<>();
-        for (Product product : products.values()) {
-            responseDtoList.add(
-                new FindProductResponseDto(product)
-            );
-        }
-        return responseDtoList;
+        var sql = """
+            select id, name, price, imageUrl from products;
+            """;
+        
+        return products.sql(sql)
+            .query((rs, rowNum) -> new FindProductResponseDto(
+                rs.getLong("id"),
+                rs.getString("name"),
+                rs.getLong("price"),
+                rs.getString("imageUrl")
+            )).list();
     }
     
     @Override
     public Product findProductWithId(Long id) {
-        return Optional.ofNullable(products.get(id))
+        var sql = """
+            select id, name, price, imageUrl from products where id = :id;
+            """;
+        
+        return products.sql(sql)
+            .param("id", id)
+            .query((rs, rowNum) -> new Product(
+                rs.getLong("id"),
+                rs.getString("name"),
+                rs.getLong("price"),
+                rs.getString("imageUrl")
+            ))
+            .optional()
             .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
     }
     
     @Override
     public ModifyProductResponseDto modifyProductWithId(Long id, Product newProduct) {
-        products.put(id, newProduct);
+        var sql = """
+            update products set name = :name, price = :price, imageUrl = :imageUrl where id = :id;
+            """;
+        
+        products.sql(sql)
+            .param("id", newProduct.getId())
+            .param("name", newProduct.getName())
+            .param("price", newProduct.getPrice())
+            .param("imageUrl", newProduct.getImageUrl())
+            .update();
+        
         return new ModifyProductResponseDto(newProduct);
     }
     
     @Override
     public void deleteProductWithId(Long id) {
-        products.remove(id);
+        var sql = """
+            delete from products where id = :id;
+            """;
+        
+        products.sql(sql)
+            .param("id", id)
+            .update();
     }
     
     @Override
     public Long getRecentId() {
-        if (products.isEmpty()) {
-            return 1L;
+        if (recentId == 0L) {
+            var sql = """
+                select count(*) from products;
+                """;
+            
+            recentId = products.sql(sql).query(Long.class).single() + 1;
         }
-        return Collections.max(products.keySet()) + 1;
+        
+        return recentId;
     }
-    //기존 구현은 db size 의존 -> 겹치는 id 생성 가능
-    //어떻게 할 것인가?
-    //key 목록에서 가장 큰 숫자 + 1로 하면 되지 않을까?
+    //hashmap에서는 가장 큰 key 값을 활용하여 최신 키를 구했지만
+    //db와 함께 auto increment를 적용하면서 더이상 적용 불가
+    //repository layer 자체에서 key를 관리하도록 만들어볼까?
 }
