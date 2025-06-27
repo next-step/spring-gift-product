39adcd832bc963eeca10132a2592b61846185500
thstkddnr20 <thstkddnr20@gmail.com>
2025-06-27 17:45:14 +0900

    fix: data.sql 쿼리 컬럼 지정 후 id 제거 id를 수동으로 집어넣어주는데 id의 증가를 auto_increment를 사용하므로 어플리케이션과 충돌 발생 id를 직접 집어넣지 않도록 수정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
-insert into product values(1, '핸드폰 케이스', 10000, 10);
-insert into product values(2, '휴대용 선풍기', 20000, 4);
-insert into product values(3, '펩시콜라', 2000, 200);
+insert into product(name, price, quantity) values('핸드폰 케이스', 10000, 10);
+insert into product(name, price, quantity) values('휴대용 선풍기', 20000, 4);
+insert into product(name, price, quantity) values('펩시콜라', 2000, 200);
