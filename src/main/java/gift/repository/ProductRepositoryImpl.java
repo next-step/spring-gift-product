7ff45ccd8e807bb76b16adeeb062132ab8e4ebc8
ming0511 <alsrbdhkalsw@naver.com>
2025-06-27 15:38:20 +0900

    refactor: 상품 추가 API H2 데이터베이스 연결(단건 상품 추가)

--- src/main/java/gift/repository/ProductRepositoryImpl.java
+++ src/main/java/gift/repository/ProductRepositoryImpl.java
@@ -2,26 +2,30 @@ package gift.repository;
 
 import gift.dto.response.ProductGetResponseDto;
 import gift.entity.Product;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class ProductRepositoryImpl implements ProductRepository {
 
+    // 제거
     private final Map<Long, Product> products = new HashMap<>();
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public ProductRepositoryImpl(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
 
     @Override
     public Product saveProduct(Product product) {
 
-        long productId = products.isEmpty() ? 1 : Collections.max(products.keySet()) + 1;
-
-        product.setProductId(productId);
-
-        products.put(productId, product);
+        String sql = "INSERT INTO products(name, price, image_url) VALUES(?,?,?)";
+        jdbcTemplate.update(sql, product.getName(), product.getPrice(), product.getImageUrl());
 
         return product;
     }
