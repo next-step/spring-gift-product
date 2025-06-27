38e0ce03791f1eb0dd2e39d11a10b1cd5f2535bf
xqqldir <2jihye10@kangwon.ac.kr>
2025-06-27 15:15:12 +0900

    refactor: record 사용

--- src/main/java/gift/dto/ProductRequestDto.java
+++ src/main/java/gift/dto/ProductRequestDto.java
@@ -1,29 +1,8 @@
 package gift.dto;
 
-public class ProductRequestDto {
-
-    private String name;
-    private Long price;
-    private String imageUrl;
-
-    public ProductRequestDto() {
-    }
-
-    public ProductRequestDto(String name, Long price, String imageUrl) {
-        this.name = name;
-        this.price = price;
-        this.imageUrl = imageUrl;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public Long getPrice() {
-        return price;
-    }
-
-    public String getImageUrl() {
-        return imageUrl;
-    }
+public record ProductRequestDto(
+        String name,
+        Long price,
+        String imageUrl
+) {
 }
