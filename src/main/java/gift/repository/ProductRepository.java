f89cec64f3b198cf1e31750c3e668ab9f423bb14
thdgmlwns1 <xhrl233@naver.com>
2025-06-27 14:59:29 +0900

    Feat: jdbcClient를 사용하여 상품 등록

--- src/main/java/gift/repository/ProductRepository.java
+++ src/main/java/gift/repository/ProductRepository.java
@@ -1,6 +1,7 @@
 package gift.repository;
 
 import gift.entity.Product;
+import org.springframework.jdbc.core.simple.JdbcClient;
 import org.springframework.stereotype.Repository;
 
 import java.util.*;
@@ -8,13 +9,24 @@ import java.util.*;
 @Repository
 public class ProductRepository {
     private final Map<Long, Product> products = new HashMap<>();
+    private final JdbcClient jdbcClient;
     private long Id = 1;
 
+    public ProductRepository(JdbcClient jdbcClient) {
+        this.jdbcClient = jdbcClient;
+    }
     public Product save(Product product) {
         if (product.getId() == null) {
             product.setId(Id++);
         }
         products.put(product.getId(), product);
+        var sql ="insert into Product (id, name,price,imageUrl) values (:id, :name, :price, :imageUrl)";
+        jdbcClient.sql(sql)
+                .param("id", product.getId())
+                .param("name", product.getName())
+                .param("price", product.getPrice())
+                .param("imageUrl", product.getImageUrl())
+                .update();
         return product;
     }
 
