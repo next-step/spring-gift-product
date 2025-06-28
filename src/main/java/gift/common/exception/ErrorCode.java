7d8b0359ce0d0a8ad960e253b842a36b72d0668d
lwj0831 <wjl0831@gmail.com>
2025-06-28 22:54:28 +0900

    refactor(product): 에외 처리 리팩토링
    - 기존의 HttpStatus로 대체 가능했던 ErrorCode 변경
    - 서비스 내 자체 에러코드 필드 추가
    - 비즈니스적으로 의미있는 예외에 대한 HttpStatus, errorCode,
      errorMessage 정의

--- src/main/java/gift/common/exception/ErrorCode.java
+++ src/main/java/gift/common/exception/ErrorCode.java
@@ -4,10 +4,10 @@ import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
 public enum ErrorCode {
-  INTERNAL_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "G001", "내부 서버 오류가 발생했습니다"),
-  INVALID_SORT_FIELD_ERROR(HttpStatus.BAD_REQUEST, "G002", "정렬 필드 값이 올바르지 않습니다"),
-  INVALID_PRODUCT_DATA(HttpStatus.BAD_REQUEST,"P001", "상품 정보가 올바르지 않습니다"),
-  PRODUCT_NOT_FOUND(HttpStatus.NOT_FOUND,"P002", "상품 정보를 찾을 수 없습니다");
+  INVALID_ARGUMENT_ERROR(HttpStatus.BAD_REQUEST, "G001", "요청 값이 유효하지 않습니다"),
+  INTERNAL_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "G002", "내부 서버 오류가 발생했습니다"),
+  INVALID_SORT_FIELD_ERROR(HttpStatus.BAD_REQUEST, "G003", "정렬 필드 값이 올바르지 않습니다"),
+  PRODUCT_NOT_FOUND(HttpStatus.NOT_FOUND,"P001", "상품 정보를 찾을 수 없습니다");
 
   private final HttpStatus status;
   private final String errorCode;
--- src/main/java/gift/common/exception/GlobalExceptionHandler.java
+++ src/main/java/gift/common/exception/GlobalExceptionHandler.java
@@ -1,50 +1,44 @@
 package gift.common.exception;
 
-import gift.common.Response;
+import gift.common.ErrorResponse;
 import gift.product.exception.ProductNotFoundException;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
   @ExceptionHandler(MethodArgumentNotValidException.class)
-  public ResponseEntity<Response<Void>> handleMethodArgumentNotValidException(
-      MethodArgumentNotValidException exception) {
-    log.error(exception.getMessage(), exception);
-    return ResponseEntity
-        .status(HttpStatus.BAD_REQUEST)
-        .body(Response.error(ErrorCode.INVALID_INPUT_VALUE));
+  public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException() {
+    return createErrorResponse(ErrorCode.INVALID_ARGUMENT_ERROR);
   }
 
   @ExceptionHandler(IllegalArgumentException.class)
-  public ResponseEntity<Response<Void>> handleIllegalArgumentException(
-      IllegalArgumentException exception) {
-    log.error(exception.getMessage(), exception);
-    return ResponseEntity
-        .status(HttpStatus.BAD_REQUEST)
-        .body(Response.error(ErrorCode.INVALID_INPUT_VALUE));
+  public ResponseEntity<ErrorResponse> handleIllegalArgumentException() {
+    return createErrorResponse(ErrorCode.INVALID_ARGUMENT_ERROR);
+  }
+
+  @ExceptionHandler(InvalidSortFieldException.class)
+  public ResponseEntity<ErrorResponse> handleSortFieldException(InvalidSortFieldException exception) {
+    return createErrorResponse(exception.getErrorCode());
   }
 
   @ExceptionHandler(ProductNotFoundException.class)
-  public ResponseEntity<Response<Void>> handleNotFoundException(
+  public ResponseEntity<ErrorResponse> handleNotFoundException(
       ProductNotFoundException exception) {
-    log.error(exception.getMessage(), exception);
-    return ResponseEntity
-        .status(exception.getErrorCode().getCode()) // 404
-        .body(Response.error(exception.getErrorCode()));
+    return createErrorResponse(exception.getErrorCode());
   }
 
   @ExceptionHandler(Exception.class)
-  public ResponseEntity<Response<Void>> handleException(Exception exception) {
-    log.error(exception.getMessage(), exception);
+  public ResponseEntity<ErrorResponse> handleException() {
+    return createErrorResponse(ErrorCode.INTERNAL_ERROR);
+  }
+
+  private static ResponseEntity<ErrorResponse> createErrorResponse(ErrorCode errorCode){
     return ResponseEntity
-        .status(HttpStatus.INTERNAL_SERVER_ERROR)
-        .body(Response.error(ErrorCode.INTERNAL_ERROR));
+        .status(errorCode.getStatus())
+        .body(ErrorResponse.from(errorCode));
   }
 }
