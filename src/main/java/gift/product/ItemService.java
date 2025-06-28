3e0bdba030e21c4dc8bcde1d3d8468d52d14d14d
Lee Si Woong <silverttthin@naver.com>
2025-06-28 19:00:53 +0900

    feat[jdbc]: 생성 기능 구현 - 기존 createItem 서비스 함수는 generated pk를 리턴 - jdbc에서 이를 리턴하기위해 다음 2가지를 수행 1. preparedStatement 객체에 RETURN_GENERATED_KEYS 명시 2. 키 값을 받아올 keyHolder를 명시 (꽤 번거로운 작업인 것 같다. jpa는 save함수 리턴을 이용하면 매우 쉬웠는데)

--- src/main/java/gift/product/ItemService.java
+++ src/main/java/gift/product/ItemService.java
@@ -5,8 +5,12 @@ import gift.product.dto.GetItemResponse;
 import gift.product.dto.ItemRequest;
 import jakarta.annotation.PostConstruct;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Service;
 
+import java.sql.PreparedStatement;
+import java.sql.Statement;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -53,9 +57,19 @@ public class ItemService {
 		if(req.name() == null || req.price() == null || req.imageUrl() == null)
 			throw new RuntimeException("요청 데이터가 잘못됐습니다.");
 
-		Item item = new Item(req.name(), req.price(), req.imageUrl());
-		db.put(item.getId(), item);
-		return item.getId();
+		final String sql = "INSERT INTO item (name, price, image_url) VALUES (?, ?, ?)";
+
+		KeyHolder keyHolder = new GeneratedKeyHolder();
+
+		jdbcTemplate.update(connection -> {
+			PreparedStatement ps = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+			ps.setString(1, req.name());
+			ps.setInt(2, req.price());
+			ps.setString(3, req.imageUrl());
+			return ps;
+		}, keyHolder);
+
+		return keyHolder.getKey().longValue();
 	}
 
 	public List<GetItemResponse> getAllItems() {
