afc9b76ba30ba89ab79c8fb6bb6df36c3db80c5d
HyeonJun <osumaniaddict527@gmail.com>
2025-06-28 17:41:49 +0900

    refactor(ProductRepositoryImpl): Create와 Update 기능 마이그레이션 및 JDBC 활용으로 전환

--- src/main/java/gift/domain/product/repository/ProductRepositoryImpl.java
+++ src/main/java/gift/domain/product/repository/ProductRepositoryImpl.java
@@ -9,6 +9,8 @@ import gift.common.pagination.Pageable;
 import gift.common.pagination.PageImpl;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.simple.JdbcClient;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
 import gift.domain.product.model.Product;
@@ -67,14 +69,43 @@ public class ProductRepositoryImpl implements ProductRepository {
     }
 
     public Product save(Product product) {
-//        if (product.getId() != null) {
-//            products.put(product.getId(),product);
-//        }
-//        long id = sequence.incrementAndGet();
-//        Product newProduct = new Product(id, product.getName(), product.getPrice(), product.getImageUrl());
-//        products.put(id, newProduct);
-//        return newProduct;
-        return null;
+
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        Long id = product.getId();
+        if (id == null) {
+            jdbcClient.sql(
+                            """
+                                    INSERT INTO PRODUCT (name, price, image_url)
+                                    VALUES (:name, :price, :imageUrl)
+                                    """
+                    ).param("name", product.getName())
+                    .param("price", product.getPrice())
+                    .param("imageUrl", product.getImageUrl())
+                    .update(keyHolder);
+
+            id = Objects.requireNonNull(keyHolder.getKey()).longValue();
+        } else {
+            jdbcClient.sql(
+                            """
+                                         UPDATE PRODUCT
+                                         SET name = :name,
+                                             price = :price,
+                                             image_url = :imageUrl
+                                         WHERE id = :id
+                                    """
+                    ).param("id", id)
+                    .param("name", product.getName())
+                    .param("price", product.getPrice())
+                    .param("imageUrl", product.getImageUrl())
+                    .update();
+
+        }
+
+        return new Product(id,
+                product.getName(),
+                product.getPrice(),
+                product.getImageUrl());
+
     }
 
     public void deleteById(Long id) {
