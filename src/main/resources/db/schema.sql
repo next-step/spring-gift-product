c7bb8f5c80454559ff5336ca577271b25c2671c6
KNU-K <knukang334@gmail.com>
2025-06-28 23:48:13 +0900

    refactor : product table에 대한 설명 추가

--- src/main/resources/db/schema.sql
+++ src/main/resources/db/schema.sql
@@ -1,7 +1,7 @@
 CREATE TABLE product
 (
-    id        BIGINT AUTO_INCREMENT PRIMARY KEY,
-    name      VARCHAR(255) NOT NULL,
-    price     INT          NOT NULL,
-    image_url VARCHAR(255) NOT NULL
+    id        BIGINT AUTO_INCREMENT PRIMARY KEY, -- '상품 ID'
+    name      VARCHAR(255) NOT NULL,             -- '상품명'
+    price     INT          NOT NULL,             -- '가격'
+    image_url VARCHAR(255) NOT NULL              -- '이미지 URL'
 );
