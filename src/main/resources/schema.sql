0b8d347f3bd19b3972c936f37ff5604f168964ad
HyeonJun <osumaniaddict527@gmail.com>
2025-06-28 17:36:44 +0900

    fix(schema.sql): 스키마 에러 해결

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -2,5 +2,5 @@ CREATE TABLE Product(
     id BIGINT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(100) NOT NULL,
     price INT NOT NULL,
-    image_url VARCHAR(255) NOT NULL,
+    image_url VARCHAR(255) NOT NULL
 );
