ed6550f36b1e7191eb798b3497a7528d44b5b315
Jae-il <pkyj040410@gmail.com>
2025-06-29 12:38:29 +0900

    Step-02 관리자 화면 구성 (#148)
    * README 작성
    API 설계를 위한 파일 생성
    
    * 상품 정보 작성 및 getter, setter 구현
    
    * 상품 전체 조회 구현
    
    * 상품 단건 조회 구현
    
    * 상품 추가 구현
    
    * 상품 수정 구현
    
    * 상품 삭제 구현
    
    * 코드 리뷰 바탕으로 STEP1 수정 완료
    
    * STEP-02 관리자 화면 구성 완료
    
    * STEP-02 관리자 화면 피드백 반영

--- src/main/java/gift/controller/ProductController.java
+++ src/main/java/gift/controller/ProductController.java
@@ -1,56 +1,68 @@
 package gift.controller;
 
 import gift.dto.Product;
-import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.Collection;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicLong;
 
-@RestController
-@RequestMapping("/products")
+@Controller
+@RequestMapping("/admin/products")
+
 public class ProductController {
     private final Map<Long, Product> products = new ConcurrentHashMap<>();
     private final AtomicLong idGenerator = new AtomicLong();
-
-    @GetMapping
-    public Collection<Product> getProducts() {
-        return products.values();
+    private final String BASE_URL = "https://media.istockphoto.com/id/1667499762/ko/%EB%B2%A1%ED%84%B0/%EC%98%81%EC%97%85%EC%A4%91-%ED%8C%90%EC%A7%80-%EC%83%81%EC%9E%90.jpg?s=612x612&w=0&k=20&c=94uRFQLclgFtnDhE4OfO1tCJdETL3uuBM9ZHD_N4P4Y=";
+    @GetMapping // 전체 상품 조회 API
+    public String getProducts(Model model) {
+        Collection<Product> list = products.values();
+        model.addAttribute("products", list);
+        return "admin/product_list";
     }
-
-    @GetMapping("/{id}")
-    public ResponseEntity<Product> getProduct(@PathVariable Long id) {
-        Product product = products.get(id);
-        return (product != null) ? ResponseEntity.ok(product) : ResponseEntity.notFound().build();
+    @GetMapping("/new")
+    public String createForm(Model model) {
+        model.addAttribute("product",new Product());
+        model.addAttribute("editMode",false);
+        return "admin/product_form";
     }
 
     @PostMapping
-    public Product createProduct(@RequestBody Product product) {
+    public String createProduct(@ModelAttribute Product product) {
         long id = idGenerator.incrementAndGet();
+        if(product.getImageUrl() == null || product.getImageUrl().isEmpty()) {
+            product.setImageUrl(BASE_URL);
+        }
         product.setId(id);
+
         products.put(id, product);
-        return product;
+        return "redirect:/admin/products";
     }
 
-    @PutMapping("/{id}")
-    public ResponseEntity<Product> updateProduct(@PathVariable Long id, @RequestBody Product product) {
-        if(products.containsKey(id)) {
-            return ResponseEntity.notFound().build();
-        }
+    @GetMapping("/{id}/edit")
+    public String editProduct(@PathVariable Long id, Model model) {
+        Product product = products.get(id);
+        model.addAttribute("product", product);
+        model.addAttribute("editMode", true);
+        return "/admin/product_form";
+    }
+
+    @PostMapping("/{id}")
+    public String updateProduct(@PathVariable Long id, @ModelAttribute Product product) {
         Product oldProduct = products.get(id);
         oldProduct.setName(product.getName());
         oldProduct.setPrice(product.getPrice());
-        return ResponseEntity.ok(oldProduct);
+        oldProduct.setImageUrl(product.getImageUrl());
+        return "redirect:/admin/products";
     }
 
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Product> deleteProduct(@PathVariable Long id) {
-        if(products.remove(id)==null) {
-            return ResponseEntity.notFound().build();
-        }
-        return ResponseEntity.noContent().build();
+    @PostMapping("/{id}/delete")
+    public String deleteProduct(@PathVariable Long id) {
+        products.remove(id);
+        return "redirect:/admin/products";
+
     }
 
 }
--- src/main/java/gift/dto/Product.java
+++ src/main/java/gift/dto/Product.java
@@ -4,12 +4,14 @@ public class Product {
     private Long id;
     private String name;
     private int price;
+    private String imageUrl;
 
     public Product() {}
-    public Product(Long id, String name, int price) {
+    public Product(Long id, String name, int price, String imageUrl) {
         this.id = id;
         this.name = name;
         this.price = price;
+        this.imageUrl = imageUrl;
     }
 
     public Long getId() {return id;}
@@ -18,4 +20,7 @@ public class Product {
     public void setName(String name) {this.name = name;}
     public int getPrice() {return price;}
     public void setPrice(int price) {this.price = price;}
+    public String getImageUrl() {return imageUrl;}
+    public void setImageUrl(String imageUrl) {this.imageUrl = imageUrl;}
+
 }
