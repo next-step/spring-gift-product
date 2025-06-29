2e838767661892cdfbef4f7e97a6ffaef64b6105
Lee Si Woong <silverttthin@naver.com>
2025-06-29 23:33:24 +0900

    feat: ItemRequestDto에 입력값 검증 추가

--- src/main/java/gift/product/dto/ItemRequest.java
+++ src/main/java/gift/product/dto/ItemRequest.java
@@ -1,4 +1,20 @@
 package gift.product.dto;
 
-public record ItemRequest(String name, Integer price, String imageUrl) {
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+
+public record ItemRequest(
+
+	@NotBlank
+	String name,
+
+	@NotNull @Min(0)
+	Integer price,
+
+	@NotBlank @Size(max = 1000)
+	String imageUrl) {
 }
