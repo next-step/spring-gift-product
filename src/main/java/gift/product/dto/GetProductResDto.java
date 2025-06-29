42ed39a1fcfa641e60d87b1aba613727022506b1
lwj0831 <wjl0831@gmail.com>
2025-06-29 19:45:42 +0900

    chore(product): 상품 조회 Dto(GetProductResDto)에 누락된 imagUrl필드 추가

--- src/main/java/gift/product/dto/GetProductResDto.java
+++ src/main/java/gift/product/dto/GetProductResDto.java
@@ -6,11 +6,12 @@ public record GetProductResDto(
     Long id,
     String name,
     int price,
-    String description
+    String description,
+    String imageUrl
 ) {
 
   public static GetProductResDto from(Product product) {
     return new GetProductResDto(product.id(), product.name(), product.price(),
-        product.description());
+        product.description(),product.imageUrl());
   }
 }
