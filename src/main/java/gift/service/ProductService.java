c75cd5db24f3f057f7abc52a4b944a9ecbe41ef8
Yunseong <dev.yunseong@gmail.com>
2025-06-29 20:52:38 +0900

    feature ProductService에 대해 Transaction 처리

--- src/main/java/gift/service/ProductService.java
+++ src/main/java/gift/service/ProductService.java
@@ -6,10 +6,11 @@ import gift.exception.NotFoundByIdException;
 import gift.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
+import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 
 @Service
+@Transactional
 public class ProductService {
 
     private final ProductRepository productRepository;
