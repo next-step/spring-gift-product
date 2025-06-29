db51846eb3c8793b32ce504292f3834f78000914
lwj0831 <wjl0831@gmail.com>
2025-06-29 21:06:13 +0900

    chore: favicon.io 요청 처리 빈 값을 보낼 수 있게 추가

--- src/main/java/gift/common/config/WebConfig.java
+++ src/main/java/gift/common/config/WebConfig.java
@@ -4,6 +4,7 @@ import gift.common.resolver.PageParamArgumentResolver;
 import java.util.List;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -12,5 +13,12 @@ public class WebConfig implements WebMvcConfigurer {
   @Override
   public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
     resolvers.add(new PageParamArgumentResolver());
+  }
+
+  @Override
+  public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    registry.addResourceHandler("/favicon.ico")
+        .addResourceLocations("classpath:/static/")
+        .setCachePeriod(0);
   }
 }
