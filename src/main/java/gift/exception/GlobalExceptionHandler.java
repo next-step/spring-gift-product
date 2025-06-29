0c210e904aa39cd37057b9d116ef6b66cadd2353
hekengi <hy2894@icloud.com>
2025-06-29 21:40:51 +0900

    refactor: handleValidation 이름 수정

--- src/main/java/gift/exception/GlobalExceptionHandler.java
+++ src/main/java/gift/exception/GlobalExceptionHandler.java
@@ -14,7 +14,7 @@ import java.util.Map;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<Map<String, String>> handleValidation(MethodArgumentNotValidException ex) {
+    public ResponseEntity<Map<String, String>> handleValidationException(MethodArgumentNotValidException ex) {
         Map<String, String> errors = new HashMap<>();
         ex.getBindingResult().getFieldErrors()
                 .forEach(err -> errors.put(err.getField(), err.getDefaultMessage()));
