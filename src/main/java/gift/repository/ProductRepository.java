7c9e0ef726895f0c3a59080cb66cfe7cc9e548f2
KimHaejoong1 <hjkim2124@gmail.com>
2025-06-30 14:58:02 +0900

    refactor(repository): nextId 스레드 세이프한 카운팅을 위한 reentrantlock 사용

--- src/main/java/gift/repository/ProductRepository.java
+++ src/main/java/gift/repository/ProductRepository.java
@@ -7,27 +7,49 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.locks.ReentrantLock;
 
 @Repository
 public class ProductRepository {
     private final Map<Integer, Product> store = new HashMap<>();
     private Integer nextId = 1;
+    private final ReentrantLock lock = new ReentrantLock();
 
     public Product save(Product product) {
-        Product toSave = new Product(nextId++, product.getName(), product.getPrice(), product.getImageUrl());
-        store.put(toSave.getId(), toSave);
-        return toSave;
+        lock.lock();
+        try {
+            Product toSave = new Product(nextId++, product.getName(), product.getPrice(), product.getImageUrl());
+            store.put(toSave.getId(), toSave);
+            return toSave;
+        } finally {
+            lock.unlock();
+        }
     }
 
     public List<Product> findAll() {
-        return new ArrayList<Product>(store.values());
+        lock.lock();
+        try {
+            return new ArrayList<>(store.values());
+        } finally {
+            lock.unlock();
+        }
     }
 
     public Product findById(Integer id) {
-        return store.get(id);
+        lock.lock();
+        try {
+            return store.get(id);
+        } finally {
+            lock.unlock();
+        }
     }
 
     public void delete(Integer id) {
-        store.remove(id);
+        lock.lock();
+        try {
+            store.remove(id);
+        } finally {
+            lock.unlock();
+        }
     }
 }
