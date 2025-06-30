82543a9a96034d6621fd781a1f0cae80304570cf
oh010 <oh010224@gmail.com>
2025-06-30 17:52:35 +0900

    fix: 역직렬화를 위해 Setter 다시 추가

--- src/main/java/gift/Product.java
+++ src/main/java/gift/Product.java
@@ -43,4 +43,20 @@ public class Product {
     public String getImageUrl() {
         return imageUrl;
     }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
 }
