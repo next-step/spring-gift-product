9d4aa71ebefbc14a51e5ba1e224d19d89ce13353
hydrationn <suhwaparkk1031@gmail.com>
2025-06-30 17:21:37 +0900

    refact: JdbcClient 기반으로 리팩토링

--- src/main/java/gift/repository/ProductRepositoryImpl.java
+++ src/main/java/gift/repository/ProductRepositoryImpl.java
@@ -6,97 +6,98 @@ import gift.dto.ProductRequestDto;
 import gift.dto.ProductResponseDto;
 import gift.entity.Product;
 import gift.exception.ProductNotFoundException;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.simple.JdbcClient;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.atomic.AtomicLong;
 
 @Repository
 public class ProductRepositoryImpl implements ProductRepository {
 
-    private final ConcurrentHashMap<Long, Product> products = new ConcurrentHashMap<>();
-    private final AtomicLong initId = new AtomicLong();
+    private final JdbcClient jdbcClient;
+
+    public ProductRepositoryImpl(JdbcClient jdbcClient) {
+        this.jdbcClient = jdbcClient;
+    }
 
     @Override
     public ProductResponseDto createProduct(ProductRequestDto productRequestDto) {
-        Product product = new Product(initId.incrementAndGet(),
-                productRequestDto.name(),
-                productRequestDto.price(),
-                productRequestDto.imageUrl()
-        );
-        products.put(initId.get(), product);
-        return new ProductResponseDto(product.getId(),
-                product.getName(),
-                product.getPrice(),
-                product.getImageUrl());
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        var sql = "INSERT INTO product(name, price, image_url) VALUES (:name, :price, :imageUrl);";
+        jdbcClient.sql(sql)
+                .param("name", productRequestDto.name())
+                .param("price", productRequestDto.price())
+                .param("imageUrl", productRequestDto.imageUrl())
+                .update(keyHolder);
+
+        return new ProductResponseDto(keyHolder.getKey().longValue(), productRequestDto.name(), productRequestDto.price(), productRequestDto.imageUrl());
     }
 
     @Override
     public PageResult<ProductResponseDto> findAllProducts(PageRequestDto pageRequestDto) {
-        List<Product> allProducts = new ArrayList<>(products.values());
+        int page = Math.max(pageRequestDto.page(), 0);
+        int size = pageRequestDto.size();
+        int offset = page * size;
 
-        int total = allProducts.size();
-        int pageSize = pageRequestDto.size();
-        int currentPage = pageRequestDto.page();
-        int startIdx = pageSize * currentPage;
-        int endIdx = Math.min(startIdx + pageSize, total);
+        String sql = "SELECT id, name, price, image_url FROM product LIMIT :limit OFFSET :offset";
 
-        List<ProductResponseDto> pageList = new ArrayList<>();
-        if (startIdx < total) {
-            pageList = allProducts.subList(startIdx, endIdx).stream()
-                    .map(this::toResponseDto)
-                    .toList();
-        }
+        List<ProductResponseDto> content = jdbcClient.sql(sql)
+                .param("limit", size)
+                .param("offset", offset)
+                .query(getProductRowMapper())
+                .list();
 
-        int totalPages = (int) Math.ceil((double) total / pageSize);
+        int total = jdbcClient.sql("SELECT COUNT(*) FROM product")
+                .query(Integer.class)
+                .single();
 
-        return new PageResult<>(pageList, currentPage, totalPages, pageSize, total);
+
+        int totalPages = (int) Math.ceil((double) total / size);
+
+        return new PageResult<>(content, page, totalPages, size, total);
     }
 
     @Override
     public ProductResponseDto findProductById(Long id) {
-        Product product = products.get(id);
+        String sql = "SELECT id, name, price, image_url FROM product WHERE id = :id";
 
-        if (product == null) {
-            throw new ProductNotFoundException(id);
-        }
-
-        return new ProductResponseDto(product.getId(),
-                product.getName(),
-                product.getPrice(),
-                product.getImageUrl());
+        return jdbcClient.sql(sql)
+                .param("id", id)
+                .query(getProductRowMapper())
+                .optional()
+                .orElseThrow(() -> new ProductNotFoundException(id));
     }
 
     @Override
     public ProductResponseDto updateProduct(Long id, ProductRequestDto productRequestDto) {
-        Product product = products.get(id);
+        String sql = "UPDATE product SET name = :name, price = :price, image_url = :imageUrl WHERE id = :id";
 
-        if (product == null) {
+        int updated = jdbcClient.sql(sql)
+                .param("name", productRequestDto.name())
+                .param("price", productRequestDto.price())
+                .param("imageUrl", productRequestDto.imageUrl())
+                .param("id", id)
+                .update();
+
+        if (updated == 0)
             throw new ProductNotFoundException(id);
-        }
 
-        product.update(
-                productRequestDto.name(),
-                productRequestDto.price(),
-                productRequestDto.imageUrl()
-        );
-        return new ProductResponseDto(
-                product.getId(),
-                product.getName(),
-                product.getPrice(),
-                product.getImageUrl()
-        );
+        return new ProductResponseDto(id, productRequestDto.name(), productRequestDto.price(), productRequestDto.imageUrl());
     }
 
     @Override
     public void deleteProduct(Long id) {
-        if (!products.containsKey(id)) {
-            throw new ProductNotFoundException(id);
-        }
+        String sql = "DELETE FROM product WHERE id = :id";
 
-        products.remove(id);
+        int deleted = jdbcClient.sql(sql)
+                .param("id", id)
+                .update();
+
+        if (deleted == 0)
+            throw new ProductNotFoundException(id);
     }
 
     private ProductResponseDto toResponseDto(Product product) {
@@ -106,5 +107,15 @@ public class ProductRepositoryImpl implements ProductRepository {
                 product.getPrice(),
                 product.getImageUrl()
         );
+    }
+
+    private static RowMapper<ProductResponseDto> getProductRowMapper() {
+        return (rs, rowNum) -> {
+            var id = rs.getLong("id");
+            var name = rs.getString("name");
+            var price = rs.getInt("price");
+            var imageUrl = rs.getString("imageUrl");
+            return new ProductResponseDto(id, name, price, imageUrl);
+        };
     }
 }
