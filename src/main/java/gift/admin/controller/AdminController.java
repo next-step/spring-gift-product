f6fc9f3273068c5a0aeaa8983d7e42f1dcc7bc3a
LeeJaeRyun <pinixs3@naver.com>
2025-06-30 17:43:54 +0900

    refactor: /api prefix 제거

--- src/main/java/gift/admin/controller/AdminController.java
+++ src/main/java/gift/admin/controller/AdminController.java
@@ -21,52 +21,52 @@ public class AdminController {
     }
 
     //상품 전체 목록 조회 페이지
-    @GetMapping("/api/admin/products")
+    @GetMapping("/admin/products")
     public String getAllProducts(Model model) {
         model.addAttribute("products", itemService.findAllItems());
         return "list";
     }
 
     //특정 상품 조회 (단건 조회) 페이지
-    @GetMapping("/api/admin/products/{id}")
+    @GetMapping("/admin/products/{id}")
     public String getProductById(@PathVariable Long id, Model model) {
         model.addAttribute("product", itemService.findItem(id));
         return "detail";
     }
 
     //상품 등록 페이지
-    @GetMapping("/api/admin/products/create")
+    @GetMapping("/admin/products/create")
     public String createProduct(Model model) {
         model.addAttribute("product", new CreateItemDto("", 0, ""));
         return "create";
     }
 
     //상품 등록하기 기능
-    @PostMapping("/api/admin/products/create")
+    @PostMapping("/admin/products/create")
     public String createProduct(CreateItemDto dto) {
         itemService.saveItem(dto);
-        return "redirect:/api/admin/products";
+        return "redirect:/admin/products";
     }
 
     //상품 삭제하기 기능
-    @DeleteMapping("/api/admin/products/{id}")
+    @DeleteMapping("/admin/products/{id}")
     public String deleteProduct(@PathVariable Long id) {
         itemService.deleteItem(id);
-        return "redirect:/api/admin/products";
+        return "redirect:/admin/products";
     }
     
     //상품 수정 페이지
-    @GetMapping("/api/admin/products/edit/{id}")
+    @GetMapping("/admin/products/edit/{id}")
     public String updateProduct(@PathVariable Long id, Model model) {
         model.addAttribute("product", itemService.findItem(id));
         return "update";
     }
 
     //상품 수정 기능
-    @PutMapping("/api/admin/products/edit/{id}")
+    @PutMapping("/admin/products/edit/{id}")
     public String updateProduct(@PathVariable Long id, UpdateItemDto dto) {
         itemService.updateItem(id, dto);
-        return "redirect:/api/admin/products";
+        return "redirect:/admin/products";
     }
 
 }
