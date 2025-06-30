282f40efb2c791e4b0b8a1357163804ae10ea03d
yungyu <168967249+hwangyungyu@users.noreply.github.com>
2025-06-30 23:15:13 +0900

    전남대 BE 황윤규 - Step3 feedback (#276)
    * step1에 ã문제가 있어 우선 다른 branch로 작업 했습니다.
    
    * step2 admin menu
    
    * step2 feedback and step3
    
    * readme 작성
    
    * A
    step3 feedback submit
    
    * Update README.md

--- README.md
+++ README.md
@@ -1,8 +1,6 @@
 
-feat:
-  1. admincontroller에서 상품 목록 확인 기능 추가
-  2. admincontroller에서 상품 목록 수정 기능 추가
-  3. admincontroller에서 상품 목록 삭제 기능 추가
-  4. html list 작업
-  5. html addform 작업
-  6. html editform 작업
+refactor: ProductController 삭제
+            study 패키지 삭제
+            jdbcTemplate과 jdbcclient 혼용되던 문제를 jdbcClient로 통일
+            Dao로 책임 분리
+
--- src/main/java/gift/Application.java
+++ src/main/java/gift/Application.java
@@ -1,15 +1,14 @@
 package gift;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+
 }
 
