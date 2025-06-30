bd3ac41709e24d522f00e8a371718969e04bfe94
Jaehyeok Kim <bbabbasi0505@naver.com>
2025-06-30 11:40:10 +0900

    refactor: 불필요한 주석 제거
    - ProductAdminController
    2단계 피드백을 바탕으로 불필요한 주석 제거

--- src/main/java/gift/controller/ProductAdminController.java
+++ src/main/java/gift/controller/ProductAdminController.java
@@ -20,54 +20,36 @@ public class ProductAdminController {
         this.productService = productService;
     }
 
-    /**
-     * 상품 목록 페이지를 보여줌 - URL: GET /admin/products
-     */
     @GetMapping
     public String showProductList(Model model) {
         model.addAttribute("products", productService.getAllProducts());
         return "product/list"; // templates/product/list.html
     }
-
-    /**
-     * 상품 등록 폼을 보여줌 - URL: GET /admin/products/new
-     */
+    
     @GetMapping("/new")
     public String showAddForm(Model model) {
         model.addAttribute("product", new Product());
         return "product/create-form";
     }
 
-    /**
-     * 상품 등록 처리 - URL: POST /admin/products
-     */
     @PostMapping
     public String addProduct(@ModelAttribute Product product) {
         productService.addProduct(product);
         return "redirect:/admin/products";
     }
 
-    /**
-     * 상품 수정 폼을 보여줌 - URL: GET /admin/products/edit/{id}
-     */
     @GetMapping("/edit/{id}")
     public String showEditForm(@PathVariable Long id, Model model) {
         model.addAttribute("product", productService.getProduct(id));
         return "product/edit-form";
     }
 
-    /**
-     * 상품 수정 처리 - URL: POST /admin/products/{id}
-     */
     @PostMapping("/{id}")
     public String updateProduct(@PathVariable Long id, @ModelAttribute Product product) {
         productService.updateProduct(id, product);
         return "redirect:/admin/products";
     }
 
-    /**
-     * 상품 삭제 처리 - URL: POST /admin/products/delete/{id}
-     */
     @PostMapping("/delete/{id}")
     public String deleteProduct(@PathVariable Long id) {
         productService.deleteProduct(id);
