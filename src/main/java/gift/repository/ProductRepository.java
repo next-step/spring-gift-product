543dd0efec6333fbf580fa9a405b2545f183efbd
dmdkr55 <59314089+dmdkr55@users.noreply.github.com>
2025-06-30 22:04:01 +0900

    충남대 BE 김재혁. 상품관리 3단계 (#257)
    * refactor: 불필요한 주석 제거
    
    - ProductAdminController
    2단계 피드백을 바탕으로 불필요한 주석 제거
    
    * refactor: 데이터베이스 적용
    
    - ProductRepository
    데이터베이스를 적용한 코드로 수정
    - ProductController
    API 응답 형태 통일

--- src/main/java/gift/repository/ProductRepository.java
+++ src/main/java/gift/repository/ProductRepository.java
@@ -1,44 +1,61 @@
 package gift.repository;
 
 import gift.model.Product;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class ProductRepository {
 
-    private final Map<Long, Product> products = new HashMap<>();
-    private Long sequence = 0L;
+    private JdbcTemplate jdbcTemplate;
+
+    public ProductRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
 
     // 상품 저장
     public Product save(Product product) {
-        product.setId(++sequence);
-        products.put(product.getId(), product);
+        String sql = "INSERT INTO PRODUCT(name, price, imageUrl) VALUES (?, ?, ?)";
+        jdbcTemplate.update(sql, product.getName(), product.getPrice(), product.getImageUrl());
         return product;
     }
 
     // 상품 전체 조회
     public List<Product> findAll() {
-        return new ArrayList<>(products.values());
+        String sql = "SELECT id, name, price, imageUrl FROM PRODUCT";
+        return jdbcTemplate.query(sql, productRowMapper());
     }
 
     // 상품 단건 조회
     public Optional<Product> findById(Long id) {
-        return Optional.ofNullable(products.get(id));
+        String sql = "SELECT id, name, price, imageUrl FROM PRODUCT WHERE id = ?";
+        return Optional.of(jdbcTemplate.queryForObject(sql, productRowMapper(), id));
     }
 
     // 상품 수정
     public void update(Product product) {
-        products.put(product.getId(), product);
+        String sql = "UPDATE PRODUCT SET name = ?, price = ?, imageUrl = ? WHERE id = ?";
+        jdbcTemplate.update(sql, product.getName(), product.getPrice(), product.getImageUrl(),
+            product.getId());
     }
 
     // 상품 삭제
     public void delete(Long id) {
-        products.remove(id);
+        String sql = "DELETE FROM PRODUCT WHERE id = ?";
+        jdbcTemplate.update(sql, id);
+    }
+
+    private RowMapper<Product> productRowMapper() {
+        return ((rs, rowNum) -> {
+            Long id = rs.getLong("id");
+            String name = rs.getString("name");
+            int price = rs.getInt("price");
+            String imageUrl = rs.getString("imageUrl");
+            return new Product(id, name, price, imageUrl);
+        });
     }
 
 }
