791c0fc584c1e9cf1e53123268c64c53ef815828
Dohyun <sks020k@naver.com>
2025-06-30 16:12:23 +0900

    refactor(product) : finding product/products from collection to jdbc

--- src/main/java/gift/repository/ProductRepository.java
+++ src/main/java/gift/repository/ProductRepository.java
@@ -1,6 +1,7 @@
 package gift.repository;
 
 import gift.entity.Product;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
@@ -10,24 +11,49 @@ import java.util.Map;
 
 @Repository
 public class ProductRepository {
-    private Long id = 1L;
-    private final Map<Long,Product> products = new HashMap<>();
+    private final JdbcTemplate jdbcTemplate;
+
+    public ProductRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
 
     public Product findById(Long id) {
-        return products.get(id);
+       String sql = "select id, name, price,imageUrl from products where id = ?";
+        return jdbcTemplate.queryForObject(
+            sql, (result,rowNum) -> {
+                Product product = new Product(
+                    result.getLong("id"),
+                    result.getString("name"),
+                    result.getInt("price"),
+                    result.getString("imageUrl")
+                );
+                return product;
+            }
+        ,id);
     }
 
-    public Product saveProduct(String name, int price, String imageUrl) {
-        Product product = new Product(id++, name,price,imageUrl);
-        products.put(product.getId(),product);
-        return product;
-    }
-
-    public void deleteById(Long id) {
-        products.remove(id);
-    }
+//    public Product saveProduct(String name, int price, String imageUrl) {
+//        Product product = new Product(id++, name,price,imageUrl);
+//        products.put(product.getId(),product);
+//        return product;
+//    }
+//
+//    public void deleteById(Long id) {
+//        products.remove(id);
+//    }
 
     public List<Product> findAllProducts() {
-        return new ArrayList<>(products.values());
+        String sql = "select id, name, price, imageUrl from products";
+        List<Product> productList = jdbcTemplate.query(
+            sql, (resultSet,rowNum) -> {
+                Product product = new Product(
+                    resultSet.getLong("id"),
+                    resultSet.getString("name"),
+                    resultSet.getInt("price"),
+                    resultSet.getString("imageUrl")
+                );
+                return product;
+            });
+        return productList;
     }
 }
