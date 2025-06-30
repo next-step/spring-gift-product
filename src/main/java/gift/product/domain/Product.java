d62d3c3dfe8046cb35d387d334cf318de3fd027a
oh010 <oh010224@gmail.com>
2025-07-01 03:23:23 +0900

    refactor: 불필요한 생성자 제거

--- src/main/java/gift/product/domain/Product.java
+++ src/main/java/gift/product/domain/Product.java
@@ -1,7 +1,5 @@
 package gift.product.domain;
 
-import gift.product.dto.ProductDto;
-
 public class Product {
     private String id;
     private String name;
@@ -9,26 +7,6 @@ public class Product {
     private String imageUrl;
 
     public Product() {}
-
-    public Product(String id, String name, int price, String imageUrl) {
-        if(price < 0) {
-            throw new IllegalArgumentException("Price should be positive");
-        }
-        this.id = id;
-        this.name = name;
-        this.price = price;
-        this.imageUrl = imageUrl;
-    }
-
-    public Product(String id, ProductDto productdto) {
-        if(productdto.getPrice() < 0) {
-            throw new IllegalArgumentException("Price should be positive");
-        }
-        this.id = id;
-        this.name = productdto.getName();
-        this.price = productdto.getPrice();
-        this.imageUrl = productdto.getImageUrl();
-    }
 
     public String getId() {
         return id;
--- src/main/java/gift/product/dto/ProductDto.java
+++ src/main/java/gift/product/dto/ProductDto.java
@@ -1,31 +1,11 @@
 package gift.product.dto;
 
-import gift.product.domain.Product;
-
 public class ProductDto {
     private String name;
     private int price;
     private String imageUrl;
 
     public ProductDto() {}
-
-    public ProductDto(String name, int price, String imageUrl) {
-        if(price < 0) {
-            throw new IllegalArgumentException("Price should be positive");
-        }
-        this.name = name;
-        this.price = price;
-        this.imageUrl = imageUrl;
-    }
-
-    public ProductDto(Product product) {
-        if(product.getPrice() < 0) {
-            throw new IllegalArgumentException("Price should be positive");
-        }
-        this.name = product.getName();
-        this.price = product.getPrice();
-        this.imageUrl = product.getImageUrl();
-    }
 
     public String getName() {
         return name;
