e4557eee68399337947655c5afc361b17ef53ab2
oh010 <oh010224@gmail.com>
2025-06-30 17:34:47 +0900

    refactor: productdao 인스턴스 이름 변경

--- src/main/java/gift/ProductAdminController.java
+++ src/main/java/gift/ProductAdminController.java
@@ -12,15 +12,15 @@ import java.util.*;
 @Controller
 @RequestMapping("/api/admin")
 public class ProductAdminController {
-    private final ProductDao productDao;
+    private final ProductDao productdao;
 
-    public ProductAdminController(ProductDao productDao) {
-        this.productDao = productDao;
+    public ProductAdminController(ProductDao productdao) {
+        this.productdao = productdao;
     }
 
     @GetMapping("/product/list")
     public String findAll(Model model) {
-        List<Product> products = productDao.getAll();
+        List<Product> products = productdao.getAll();
         model.addAttribute("products", products);
         return "products";
     }
@@ -35,7 +35,7 @@ public class ProductAdminController {
     public String saveProduct(@ModelAttribute ProductDTO productdto) {
         ProductDTO productdto1;
         productdto1 = new ProductDTO(productdto.getName(), productdto.getPrice(), productdto.getImageUrl());
-        productDao.save(productdto1);
+        productdao.save(productdto1);
         return "redirect:/api/admin/product/list";
     }
 
@@ -43,13 +43,13 @@ public class ProductAdminController {
     @GetMapping("/product/{id}")
     public Product findById(@PathVariable Long id) {
         Product product = new Product();
-        product = productDao.findById(id);
+        product = productdao.findById(id);
         return product;
     }
 
     @GetMapping("/product/{id}/update")
     public String updateForm(@PathVariable Long id, Model model) {
-        Product product = productDao.findById(id);
+        Product product = productdao.findById(id);
         model.addAttribute("product", product);
         return "updateForm";
     }
@@ -58,18 +58,18 @@ public class ProductAdminController {
     public String updateProduct(@PathVariable Long id, @ModelAttribute ProductDTO updateProductdto) {
         ProductDTO productdto1;
         productdto1 = new ProductDTO(updateProductdto.getName(), updateProductdto.getPrice(), updateProductdto.getImageUrl());
-        Product product = productDao.findById(id);
+        Product product = productdao.findById(id);
         if( product != null) {
-            productDao.update(id, productdto1);
+            productdao.update(id, productdto1);
         }
         return "redirect:/api/admin/product/list";
     }
 
     @DeleteMapping("/product/{id}/delete")
     public String deleteById(@PathVariable Long id) {
-        Product product = productDao.findById(id);
+        Product product = productdao.findById(id);
         if( product != null) {
-            productDao.delete(id);
+            productdao.delete(id);
         }
         return "redirect:/api/admin/product/list";
     }
