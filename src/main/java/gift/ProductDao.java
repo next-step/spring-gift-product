264ca8c9190c5b7a8d51ae8f4e8454ab328cb1d0
oh010 <oh010224@gmail.com>
2025-06-30 12:56:53 +0900

    refactor: jdbcClient 사용

--- src/main/java/gift/ProductDao.java
+++ src/main/java/gift/ProductDao.java
@@ -1,92 +1,65 @@
 package gift;
 
-import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.simple.JdbcClient;
 import org.springframework.stereotype.Repository;
-import org.springframework.jdbc.core.JdbcTemplate;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.NoSuchElementException;
 
 @Repository
 public class ProductDao {
-    private final JdbcTemplate jdbcTemplate;
+    private final JdbcClient jdbcClient;
 
-    public ProductDao(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
+    public ProductDao(JdbcClient jdbcClient) {
+        this.jdbcClient = jdbcClient;
     }
 
     public void save(ProductDTO productDto) {
-        String sql = "INSERT INTO PRODUCTS (name, price, imageUrl) VALUES (?, ?, ?)";
-        jdbcTemplate.update(sql, productDto.getName(), productDto.getPrice(), productDto.getImageUrl());
+        jdbcClient.sql("INSERT INTO PRODUCTS (name, price, imageUrl) VALUES (:name, :price, :imageUrl)")
+                .param("name", productDto.getName())
+                .param("price", productDto.getPrice())
+                .param("imageUrl", productDto.getImageUrl())
+                .update();
     }
 
 
     public List<Product> getAll() {
-        String sql = "SELECT * FROM PRODUCTS";
-        return jdbcTemplate.query(sql, productRowMapper());
-
+        return jdbcClient.sql("SELECT * FROM PRODUCTS")
+                .query(Product.class)
+                .list();
     }
 
     public Product findById(Long id) {
-        String sql = "SELECT * FROM PRODUCTS WHERE id = ?";
-        List<Product> result = jdbcTemplate.query(sql, productRowMapper(), id);
-        if(result.isEmpty()) {
-            throw new NoSuchElementException("해당 데이터가 없습니다.");
-        }
-        return result.getFirst();
+        return jdbcClient.sql("SELECT * FROM PRODUCTS WHERE id = :id")
+                .param("id", id)
+                .query(Product.class)
+                .single();
     }
 
     public void update(Long id, ProductDTO productDto) {
-        String sql = "UPDATE PRODUCTS SET ";
-        List<Object> params = new ArrayList<>();
-        boolean bool = false;
-
-        if(productDto.getName() != null) {
-            sql += "name = ?";
-            params.add(productDto.getName());
-            bool = true;
+        if (productDto.getName() != null) {
+            jdbcClient.sql("UPDATE PRODUCTS SET name = :name WHERE id = :id")
+                    .param("name", productDto.getName())
+                    .param("id", id)
+                    .update();
         }
-        if(productDto.getPrice() != 0) {
-            if(bool) {
-                sql += ", ";
-            }
-            sql += "price = ?";
-            params.add(productDto.getPrice());
-            bool = true;
+        if (productDto.getPrice() != 0) {
+            jdbcClient.sql("UPDATE PRODUCTS SET price = :price WHERE id = :id")
+                    .param("price", productDto.getPrice())
+                    .param("id", id)
+                    .update();
         }
-        if(productDto.getImageUrl() != null) {
-            if(bool) {
-                sql += ", ";
-            }
-            sql += "imageUrl = ?";
-            params.add(productDto.getImageUrl());
+        if (productDto.getImageUrl() != null) {
+            jdbcClient.sql("UPDATE PRODUCTS SET imageUrl = :imageUrl WHERE id = :id")
+                    .param("imageUrl", productDto.getImageUrl())
+                    .param("id", id)
+                    .update();
         }
-        if(params.isEmpty()) {
-            throw new NoSuchElementException("업데이트 필요 없음");
-        }
-        sql += " WHERE id = ?";
-        params.add(id);
-
-        jdbcTemplate.update(sql, params.toArray());
     }
 
     public void delete(Long id) {
-        String sql = "DELETE FROM PRODUCTS WHERE id = ?";
-        jdbcTemplate.update(sql, id);
+        jdbcClient.sql("DELETE FROM PRODUCTS WHERE id = :id")
+                .param("id", id)
+                .update();
     }
 
-    private RowMapper<Product> productRowMapper() {
-        return new RowMapper<Product>() {
-            public Product mapRow(ResultSet rs, int rowNum) throws SQLException {
-                Long id = rs.getLong("id");
-                String name = rs.getString("name");
-                int price = rs.getInt("price");
-                String imageUrl = rs.getString("imageUrl");
-                return new Product(id,name,price,imageUrl);
-            }
-        };
-    }
 }
