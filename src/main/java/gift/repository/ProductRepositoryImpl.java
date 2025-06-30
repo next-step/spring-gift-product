d3708d7f6f79156a410f14aaf5ea052f6c9f3ea5
Hyun <hyun_7586@naver.com>
2025-06-30 12:35:58 +0900

    refactor: jdbcTemplate을 통해 데이터베이스에 접근하도록 함

--- src/main/java/gift/repository/ProductRepositoryImpl.java
+++ src/main/java/gift/repository/ProductRepositoryImpl.java
@@ -1,36 +1,59 @@
 package gift.repository;
 
 import gift.domain.Product;
+import gift.dto.ProductRequest;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class ProductRepositoryImpl implements ProductRepository {
 
-    private final Map<Long, Product> products = new HashMap<>();
+    private final JdbcTemplate jdbcTemplate;
+    private final RowMapper<Product> rowMapper = (rs, rowNum) -> new Product(
+        rs.getLong("id"),
+        rs.getString("name"),
+        rs.getInt("price"),
+        rs.getString("image_url")
+    );
+
+    public ProductRepositoryImpl(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
 
     @Override
     public Optional<Product> findById(Long productId) {
-        Product product = products.get(productId);
+        String sql = "select * from products where id = ?";
 
-        return Optional.ofNullable(product);
+        return Optional.of(jdbcTemplate.queryForObject(sql, rowMapper, productId));
     }
 
     @Override
     public void save(Product product) {
-        products.put(product.getId(), product);
+        String sql = "insert into products(name, price, image_url) "
+            + "values(?,?,?) ";
+
+        jdbcTemplate.update(sql, product.getName(), product.getPrice(), product.getImageUrl());
+    }
+
+    public void update(ProductRequest product){
+        String sql = "update products set name = ?, price = ?, image_url = ? where id = ?";
+        jdbcTemplate.update(sql,product.name(), product.price(), product.imageUrl(), product.id());
     }
 
     @Override
     public void deleteById(Long productId) {
-        products.remove(productId);
+        String sql = "delete from products where id = ? ";
+        jdbcTemplate.update(sql, productId);
     }
 
     @Override
     public List<Product> findAll() {
-        return products.values().stream().toList();
+        String sql = "select * from products";
+        return jdbcTemplate.query(sql, rowMapper);
     }
 }
