cc4ba2f761022dd6a3a8c9ec2026e04654bffb0a
BSY3510 <baeksy3510@gmail.com>
2025-06-30 16:28:28 +0900

    implement(repository): enhance ProductRepositoryImpl with JDBC and add update method

--- src/main/java/gift/repository/ProductRepositoryImpl.java
+++ src/main/java/gift/repository/ProductRepositoryImpl.java
@@ -1,19 +1,29 @@
 package gift.repository;
 
 import gift.entity.Product;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
-import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.simple.JdbcClient;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class ProductRepositoryImpl implements ProductRepository {
 
-    private final Map<Long, Product> products = new HashMap<>();
-    private final AtomicLong idGenerator = new AtomicLong(1);
+    private final JdbcClient jdbcClient;
+
+    public ProductRepositoryImpl(JdbcClient jdbcClient) {
+        this.jdbcClient = jdbcClient;
+    }
+
+    private static final RowMapper<Product> PRODUCT_ROW_MAPPER = (rs, rowNum) -> new Product(
+        rs.getLong("id"),
+        rs.getString("name"),
+        rs.getInt("price"),
+        rs.getString("image_url")
+    );
 
     @Override
     public Product save(Product product) {
@@ -25,32 +35,79 @@ public class ProductRepositoryImpl implements ProductRepository {
             throw new IllegalArgumentException("Required fields are missing");
         }
 
-        if (product.getId() == null) {
-            product = new Product(
-                idGenerator.getAndIncrement(),
-                product.getName(),
-                product.getPrice(),
-                product.getImageUrl()
-            );
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+
+        String sql = "INSERT INTO products (name, price, image_url) VALUES (?, ?, ?)";
+        jdbcClient.sql(sql)
+            .param(1, product.getName())
+            .param(2, product.getPrice())
+            .param(3, product.getImageUrl())
+            .update(keyHolder, new String[]{"id"});
+
+        Number key = keyHolder.getKey();
+        if (key == null) {
+            throw new IllegalArgumentException("Failed to get id");
+        }
+        Long id = key.longValue();
+
+        return new Product(
+            id,
+            product.getName(),
+            product.getPrice(),
+            product.getImageUrl()
+        );
+    }
+
+    @Override
+    public Product update(Product product) {
+        if (product == null) {
+            throw new IllegalArgumentException("product not found");
         }
 
-        products.put(product.getId(), product);
+        if (product.getId() == null || product.getName() == null || product.getPrice() == null) {
+            throw new IllegalArgumentException("Required fields are missing");
+        }
+
+        String sql = "UPDATE products SET name = ?, price = ?, image_url = ? WHERE id = ?";
+        int rowsCount = jdbcClient.sql(sql)
+            .param(1, product.getName())
+            .param(2, product.getPrice())
+            .param(3, product.getImageUrl())
+            .param(4, product.getId())
+            .update();
+        if (rowsCount == 0) {
+            throw new IllegalArgumentException("Product(id: " + product.getId() + ") not found");
+        }
+
         return product;
     }
 
     @Override
     public Optional<Product> findById(Long id) {
-        return Optional.ofNullable(products.get(id));
+        String sql = "SELECT id, name, price, image_url FROM products WHERE id = ?";
+        return jdbcClient.sql(sql)
+            .param(1, id)
+            .query(Product.class)
+            .optional();
     }
 
     @Override
     public void delete(Long id) {
-        products.remove(id);
+        String sql = "DELETE FROM products WHERE id = ?";
+        int rowsCount = jdbcClient.sql(sql)
+            .param(1, id)
+            .update();
+        if (rowsCount == 0) {
+            throw new IllegalArgumentException("Product(id: " + id + " ) not found");
+        }
     }
 
     @Override
     public List<Product> findAll() {
-        return new ArrayList<>(products.values());
+        String sql = "SELECT id, name, price, image_url FROM products";
+        return jdbcClient.sql(sql)
+            .query(Product.class)
+            .list();
     }
 
 }
