715e091da352a1188aa1d2c4a749d79b1dd579ae
Dozzang <74997120+dozzzang@users.noreply.github.com>
2025-07-01 13:13:44 +0900

    경북대 BE_김도현 3단계 - 데이터베이스적용 (#279)
    * style(product):java-google-style
    
    * refactor(product): removing redundancy in findById
    
    * refactor(product): utilizing lambda for replacing loop
    
    * docs(readme) : step3 specification
    
    * feat(product) : setting H2 DB
    
    * feat(product) : setting schema,initial data
    
    * refactor(product) : finding product/products from collection to jdbc
    
    * refactor(product) : saving product from collection to jdbc
    
    * refactor(product) : updating product from collectino to jdbc
    
    * refactor(product) : deleting product from collection to jdbc
    
    * refactor(product) : modifying find logic from jdbcTemplate to jdbcClient
    
    * chore : 오타 수정
    
    * refactor : JDBCtemplate -> JDBCClient
    
    * style : google style formatter
    
    * chore : 함수 이름 변경(findProductByIdFun -> findProductByIdOrFail)
    
    * refactor : 검증 로직 이동 : Service -> DTO 생성자

--- src/main/java/gift/repository/ProductRepository.java
+++ src/main/java/gift/repository/ProductRepository.java
@@ -1,6 +1,12 @@
 package gift.repository;
 
 import gift.entity.Product;
+import gift.exception.InvalidImageUrlException;
+import gift.exception.InvalidNameException;
+import gift.exception.InvalidPriceException;
+import gift.exception.ProductNotFoundException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.simple.JdbcClient;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
@@ -10,24 +16,106 @@ import java.util.Map;
 
 @Repository
 public class ProductRepository {
-    private Long id = 1L;
-    private final Map<Long,Product> products = new HashMap<>();
 
-    public Product findById(Long id) {
-        return products.get(id);
+  private final JdbcClient jdbcClient;
+
+  public ProductRepository(JdbcClient jdbcClient) {
+    this.jdbcClient = jdbcClient;
+  }
+
+
+  public Product findById(Long id) {
+    String sql = "select id, name, price, imageUrl from products where id = ?";
+    return jdbcClient.sql(sql)
+        .param(id)
+        .query((result, rowNum) -> {
+          Product product = new Product(
+              result.getLong("id"),
+              result.getString("name"),
+              result.getInt("price"),
+              result.getString("imageUrl")
+          );
+          return product;
+        })
+        .single();
+  }
+
+  public Product saveProduct(String name, int price, String imageUrl) {
+    String sql = "INSERT INTO products(name,price,imageUrl) VALUES(?,?,?)";
+    jdbcClient.sql(sql)
+        .param(name)
+        .param(price)
+        .param(imageUrl)
+        .update();
+
+    String sqlForFind = "SELECT id, name, price, imageUrl FROM products"
+        + " WHERE name = ? AND price = ? AND imageUrl = ?";
+    return jdbcClient.sql(sqlForFind)
+        .param(name)
+        .param(price)
+        .param(imageUrl)
+        .query((result, rowNum) -> {
+          Product product = new Product(
+              result.getLong("id"),
+              result.getString("name"),
+              result.getInt("price"),
+              result.getString("imageUrl")
+          );
+          return product;
+        })
+        .single();
+  }
+
+  public Product updateProduct(Long id, String name, int price, String imageUrl) {
+    String sql = "update products set name = ?, price = ?, imageUrl = ? where id = ?";
+    int updatedRow = jdbcClient.sql(sql)
+        .param(name)
+        .param(price)
+        .param(imageUrl)
+        .param(id)
+        .update();
+    if (updatedRow == 0) {
+      throw new ProductNotFoundException();
+    }
+    return findById(id);
+  }
+
+  public Product updatePrice(Long id, int price) {
+    String sql = "update products set price = ? where id = ?";
+    int updatedRows = jdbcClient.sql(sql)
+        .param(price)
+        .param(id)
+        .update();
+
+    if (updatedRows == 0) {
+      throw new ProductNotFoundException();
     }
 
-    public Product saveProduct(String name, int price, String imageUrl) {
-        Product product = new Product(id++, name,price,imageUrl);
-        products.put(product.getId(),product);
-        return product;
-    }
+    return findById(id);
+  }
 
-    public void deleteById(Long id) {
-        products.remove(id);
-    }
+  public void deleteById(Long id) {
+    String sql = "DELETE FROM products WHERE id = ?";
+    int deletedRows = jdbcClient.sql(sql)
+        .param(id)
+        .update();
 
-    public List<Product> findAllProducts() {
-        return new ArrayList<>(products.values());
+    if (deletedRows == 0) {
+      throw new ProductNotFoundException();
     }
+  }
+
+  public List<Product> findAllProducts() {
+    return jdbcClient.sql("select id, name, price, imageUrl from products")
+        .query((resultSet, rowNum) -> {
+          Product product = new Product(
+              resultSet.getLong("id"),
+              resultSet.getString("name"),
+              resultSet.getInt("price"),
+              resultSet.getString("imageUrl")
+          );
+          return product;
+        })
+        .list();
+  }
 }
--- src/main/java/gift/service/ProductService.java
+++ src/main/java/gift/service/ProductService.java
@@ -3,8 +3,12 @@ package gift.service;
 import gift.dto.ProductRequestDto;
 import gift.dto.ProductResponseDto;
 import gift.entity.Product;
+import gift.exception.InvalidImageUrlException;
+import gift.exception.InvalidNameException;
+import gift.exception.InvalidPriceException;
 import gift.exception.ProductNotFoundException;
 import gift.repository.ProductRepository;
+import java.util.stream.Collectors;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
@@ -15,58 +19,55 @@ import java.util.List;
 @Service
 public class ProductService {
 
-    private final ProductRepository productRepository;
+  private final ProductRepository productRepository;
 
-    public ProductService(ProductRepository productRepository) {
-        this.productRepository = productRepository;
-    }
+  public ProductService(ProductRepository productRepository) {
+    this.productRepository = productRepository;
+  }
 
-    public ProductResponseDto findProductById(Long productId) {
-        Product product = productRepository.findById(productId);
-        if (product == null)
-            throw new ProductNotFoundException();
-        return product.toDto();
+  private Product findProductByIdOrFail(Long id) {
+    Product product = productRepository.findById(id);
+    if (product == null) {
+      throw new ProductNotFoundException();
     }
+    return product;
+  }
 
-    public ProductResponseDto saveProduct(ProductRequestDto dto) {
-        Product product = productRepository.saveProduct(dto.name(),dto.price(),dto.imageUrl());
-        return product.toDto();
-    }
+  public ProductResponseDto findProductById(Long productId) {
+    return findProductByIdOrFail(productId).toDto();
+  }
 
-    public ProductResponseDto updateProduct(Long productId, ProductRequestDto dto) {
-        Product product = productRepository.findById(productId);
-        if(product == null) {
-            throw new ProductNotFoundException();
-        }
-        product.updateProductInfo(dto.name(), dto.price(), dto.imageUrl());
-        return product.toDto();
-    }
+  public ProductResponseDto saveProduct(ProductRequestDto dto) {
+    Product product = productRepository.saveProduct(dto.name(), dto.price(), dto.imageUrl());
+    return product.toDto();
+  }
 
-    //가격만 수정하는 것은 꽤 합리적이라고 생각
-    public ProductResponseDto updateProductPrice(Long productId, int price) {
-        Product product = productRepository.findById(productId);
-        if(product == null) {
-            throw new ProductNotFoundException();
-        }
-        product.updatePrice(price);
-        return product.toDto();
-    }
+  public ProductResponseDto updateProduct(Long productId, ProductRequestDto dto) {
+    Product updatedProduct = productRepository.updateProduct(productId, dto.name(), dto.price(),
+        dto.imageUrl());
+    return updatedProduct.toDto();
+  }
 
-    public void deleteProduct(Long productId) {
-        Product product = productRepository.findById(productId);
-        if(product == null) {
-            throw new ProductNotFoundException();
-        }
-        productRepository.deleteById(productId);
+  //가격만 수정하는 것은 꽤 합리적이라고 생각
+  public ProductResponseDto updateProductPrice(Long productId, int price) {
+    if (price < 0) {
+      throw new InvalidPriceException();
     }
+    Product updatedProduct = productRepository.updatePrice(productId, price);
+    return updatedProduct.toDto();
+  }
 
-    public List<ProductResponseDto> findAllProducts() {
-        List<Product> products = productRepository.findAllProducts();
-        List<ProductResponseDto> result = new ArrayList<>();
-        for (Product product : products) {
-            result.add(product.toDto());
-        }
-        return result;
-    }
+  public void deleteProduct(Long productId) {
+    findProductByIdOrFail(productId);
+    productRepository.deleteById(productId);
+  }
 
-    }
+  public List<ProductResponseDto> findAllProducts() {
+    return
+        productRepository.findAllProducts()
+            .stream()
+            .map(Product::toDto)
+            .collect(Collectors.toList());
+  }
+
+}
