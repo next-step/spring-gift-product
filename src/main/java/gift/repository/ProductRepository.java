f2769bf8fa059a58850dbada9fee6599d6e61877
Dohyun <sks020k@naver.com>
2025-07-01 10:58:36 +0900

    style : google style formatter

--- src/main/java/gift/repository/ProductRepository.java
+++ src/main/java/gift/repository/ProductRepository.java
@@ -16,105 +16,106 @@ import java.util.Map;
 
 @Repository
 public class ProductRepository {
-    private final JdbcClient jdbcClient;
 
-    public ProductRepository(JdbcClient jdbcClient) {
-        this.jdbcClient = jdbcClient;
+  private final JdbcClient jdbcClient;
+
+  public ProductRepository(JdbcClient jdbcClient) {
+    this.jdbcClient = jdbcClient;
+  }
+
+
+  public Product findById(Long id) {
+    String sql = "select id, name, price, imageUrl from products where id = ?";
+    return jdbcClient.sql(sql)
+        .param(id)
+        .query((result, rowNum) -> {
+          Product product = new Product(
+              result.getLong("id"),
+              result.getString("name"),
+              result.getInt("price"),
+              result.getString("imageUrl")
+          );
+          return product;
+        })
+        .single();
+  }
+
+  public Product saveProduct(String name, int price, String imageUrl) {
+    String sql = "INSERT INTO products(name,price,imageUrl) VALUES(?,?,?)";
+    jdbcClient.sql(sql)
+        .param(name)
+        .param(price)
+        .param(imageUrl)
+        .update();
+
+    String sqlForFind = "SELECT id, name, price, imageUrl FROM products"
+        + " WHERE name = ? AND price = ? AND imageUrl = ?";
+    return jdbcClient.sql(sqlForFind)
+        .param(name)
+        .param(price)
+        .param(imageUrl)
+        .query((result, rowNum) -> {
+          Product product = new Product(
+              result.getLong("id"),
+              result.getString("name"),
+              result.getInt("price"),
+              result.getString("imageUrl")
+          );
+          return product;
+        })
+        .single();
+  }
+
+  public Product updateProduct(Long id, String name, int price, String imageUrl) {
+    String sql = "update products set name = ?, price = ?, imageUrl = ? where id = ?";
+    int updatedRow = jdbcClient.sql(sql)
+        .param(name)
+        .param(price)
+        .param(imageUrl)
+        .param(id)
+        .update();
+    if (updatedRow == 0) {
+      throw new ProductNotFoundException();
+    }
+    return findById(id);
+  }
+
+  public Product updatePrice(Long id, int price) {
+    String sql = "update products set price = ? where id = ?";
+    int updatedRows = jdbcClient.sql(sql)
+        .param(price)
+        .param(id)
+        .update();
+
+    if (updatedRows == 0) {
+      throw new ProductNotFoundException();
     }
 
+    return findById(id);
+  }
 
-    public Product findById(Long id) {
-        String sql = "select id, name, price, imageUrl from products where id = ?";
-        return jdbcClient.sql(sql)
-            .param(id)
-            .query((result,rowNum) -> {
-                Product product = new Product(
-                    result.getLong("id"),
-                    result.getString("name"),
-                    result.getInt("price"),
-                    result.getString("imageUrl")
-                );
-                return product;
-            })
-            .single();
+  public void deleteById(Long id) {
+    String sql = "DELETE FROM products WHERE id = ?";
+    int deletedRows = jdbcClient.sql(sql)
+        .param(id)
+        .update();
+
+    if (deletedRows == 0) {
+      throw new ProductNotFoundException();
     }
+  }
 
-    public Product saveProduct(String name, int price, String imageUrl) {
-        String sql = "INSERT INTO products(name,price,imageUrl) VALUES(?,?,?)";
-        jdbcClient.sql(sql)
-            .param(name)
-            .param(price)
-            .param(imageUrl)
-            .update();
-
-        String sqlForFind = "SELECT id, name, price, imageUrl FROM products"
-            + " WHERE name = ? AND price = ? AND imageUrl = ?";
-        return jdbcClient.sql(sqlForFind)
-            .param(name)
-            .param(price)
-            .param(imageUrl)
-            .query((result,rowNum) -> {
-                Product product = new Product(
-                    result.getLong("id"),
-                    result.getString("name"),
-                    result.getInt("price"),
-                    result.getString("imageUrl")
-                );
-                return product;
-            })
-            .single();
-    }
-
-    public Product updateProduct(Long id, String name, int price, String imageUrl) {
-        String sql = "update products set name = ?, price = ?, imageUrl = ? where id = ?";
-        int updatedRow = jdbcClient.sql(sql)
-                        .param(name)
-                        .param(price)
-                        .param(imageUrl)
-                        .param(id)
-                        .update();
-        if(updatedRow == 0) {
-            throw new ProductNotFoundException();
-        }
-        return findById(id);
-    }
-
-    public Product updatePrice(Long id, int price) {
-        String sql = "update products set price = ? where id = ?";
-        int updatedRows = jdbcClient.sql(sql)
-            .param(price)
-            .param(id)
-            .update();
-
-        if(updatedRows == 0) {
-            throw new ProductNotFoundException();
-        }
-
-        return findById(id);
-    }
-
-    public void deleteById(Long id) {
-        String sql = "DELETE FROM products WHERE id = ?";
-        int deletedRows = jdbcClient.sql(sql)
-            .param(id)
-            .update();
-
-        if(deletedRows == 0) {
-            throw new ProductNotFoundException();
-        }
-    }
-
-    public List<Product> findAllProducts() {
-        return jdbcClient.sql("select id, name, price, imageUrl from products")
-            .query((resultSet, rowNum) -> {
-                Product product = new Product(
-                    resultSet.getLong("id"),
-                    resultSet.getString("name"),
-                    resultSet.getInt("price"),
-                    resultSet.getString("imageUrl")
-                );
-                return product;
-            })
-            .list();
-    }
+  public List<Product> findAllProducts() {
+    return jdbcClient.sql("select id, name, price, imageUrl from products")
+        .query((resultSet, rowNum) -> {
+          Product product = new Product(
+              resultSet.getLong("id"),
+              resultSet.getString("name"),
+              resultSet.getInt("price"),
+              resultSet.getString("imageUrl")
+          );
+          return product;
+        })
+        .list();
+  }
 }
--- src/main/java/gift/service/ProductService.java
+++ src/main/java/gift/service/ProductService.java
@@ -19,60 +19,64 @@ import java.util.List;
 @Service
 public class ProductService {
 
-    private final ProductRepository productRepository;
+  private final ProductRepository productRepository;
 
-    public ProductService(ProductRepository productRepository) {
-        this.productRepository = productRepository;
-    }
-    private Product findProductByIdFun(Long id) {
-        Product product =  productRepository.findById(id);
-        if(product == null)
-            throw new ProductNotFoundException();
-        return product;
-    }
-    public ProductResponseDto findProductById(Long productId) {
-        return findProductByIdFun(productId).toDto();
-    }
+  public ProductService(ProductRepository productRepository) {
+    this.productRepository = productRepository;
+  }
 
-    public ProductResponseDto saveProduct(ProductRequestDto dto) {
-        Product product = productRepository.saveProduct(dto.name(),dto.price(),dto.imageUrl());
-        return product.toDto();
-    }
+  private Product findProductByIdFun(Long id) {
+    Product product = productRepository.findById(id);
+      if (product == null) {
+          throw new ProductNotFoundException();
+      }
+    return product;
+  }
 
-    public ProductResponseDto updateProduct(Long productId, ProductRequestDto dto) {
-        if(dto.name() == null) {
-            throw new InvalidNameException();
-        }
-        if(dto.price() < 0) {
-            throw new InvalidPriceException();
-        }
-        if(dto.imageUrl() == null) {
-            throw new InvalidImageUrlException();
-        }
-        Product updatedProduct = productRepository.updateProduct(productId, dto.name(), dto.price(), dto.imageUrl());
-        return updatedProduct.toDto();
-    }
+  public ProductResponseDto findProductById(Long productId) {
+    return findProductByIdFun(productId).toDto();
+  }
 
-    //가격만 수정하는 것은 꽤 합리적이라고 생각
-    public ProductResponseDto updateProductPrice(Long productId, int price) {
-        if(price < 0) {
-            throw new InvalidPriceException();
-        }
-        Product updatedProduct = productRepository.updatePrice(productId, price);
-        return updatedProduct.toDto();
-    }
+  public ProductResponseDto saveProduct(ProductRequestDto dto) {
+    Product product = productRepository.saveProduct(dto.name(), dto.price(), dto.imageUrl());
+    return product.toDto();
+  }
 
-    public void deleteProduct(Long productId) {
-            findProductByIdFun(productId);
-            productRepository.deleteById(productId);
+  public ProductResponseDto updateProduct(Long productId, ProductRequestDto dto) {
+    if (dto.name() == null) {
+      throw new InvalidNameException();
     }
+    if (dto.price() < 0) {
+      throw new InvalidPriceException();
+    }
+    if (dto.imageUrl() == null) {
+      throw new InvalidImageUrlException();
+    }
+    Product updatedProduct = productRepository.updateProduct(productId, dto.name(), dto.price(),
+        dto.imageUrl());
+    return updatedProduct.toDto();
+  }
 
-    public List<ProductResponseDto> findAllProducts() {
-        return
-            productRepository.findAllProducts()
-                .stream()
-                .map(Product::toDto)
-                .collect(Collectors.toList());
+  //가격만 수정하는 것은 꽤 합리적이라고 생각
+  public ProductResponseDto updateProductPrice(Long productId, int price) {
+    if (price < 0) {
+      throw new InvalidPriceException();
     }
+    Product updatedProduct = productRepository.updatePrice(productId, price);
+    return updatedProduct.toDto();
+  }
 
-    }
+  public void deleteProduct(Long productId) {
+    findProductByIdFun(productId);
+    productRepository.deleteById(productId);
+  }
+
+  public List<ProductResponseDto> findAllProducts() {
+    return
+        productRepository.findAllProducts()
+            .stream()
+            .map(Product::toDto)
+            .collect(Collectors.toList());
+  }
+
+}
