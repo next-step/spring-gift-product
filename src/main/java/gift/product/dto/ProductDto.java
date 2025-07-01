8f8b3d9b3cc701084952fedf4466b59366cc9b27
OreoFlavor <80496872+OreoFlavor@users.noreply.github.com>
2025-07-01 09:31:32 +0900

    경북대 BE_오성식 Step3 - 리팩토링 (#242)
    * refactor: controller 내 Exception Handler 추가
    
    * docs: Step3 README 내용 추가
    
    * chore: h2 환경 설정
    
    * chore: 초기 DB 생성
    
    * refactor: jdbc 사용
    
    * refactor: jdbcTemplate 사용
    
    * refactor: jdbcClient 사용
    
    * feat: Restcontroller, 관리자 화면 controller 별도 구현
    
    * fix: form 경로 수정
    
    * fix: findById 예외 생성
    
    * feat: findById 관련 handler 추가
    
    * refactor: setter 제거
    
    * refactor: 불필요한 메서드 제거
    
    * refactor: productdao 인스턴스 이름 변경
    
    * refactor: 이름 변경, 중복 코드 제거
    
    * fix: 역직렬화를 위해 Setter 다시 추가
    
    * refactor: EmptyResultDataAccesException 예외 처리 수정
    
    * refactor: GlobalExceptionHandler 구현
    
    * refactor: try-catch -> throws로 변경
    
    * refactor: 트랜잭션 처리
    
    * refactor: id Auto-increment -> uuid로 수정
    
    * refactor: ProductDao 메서드 이름 수정 getAll() -> findAll()
    
    * refactor: packaging
    
    * feat: service layer 구현
    
    * refactor: dao 호출 -> service 호출
    
    * refactor: 불필요한 생성자 제거
    
    * refactor: productDto(Product product) 생성자 추가
    
    * refactor: product 대신 productDto 반환하도록 변경, product를 반환하는 admin 메서드 추가
    
    * refactor: 변경된 service 메서드 반영

--- src/main/java/gift/product/dto/ProductDto.java
+++ src/main/java/gift/product/dto/ProductDto.java
@@ -1,25 +1,15 @@
-package gift;
+package gift.product.dto;
 
-public class ProductDTO {
+import gift.product.domain.Product;
+
+public class ProductDto {
     private String name;
     private int price;
     private String imageUrl;
 
-    public ProductDTO() {};
+    public ProductDto() {}
 
-    public ProductDTO(String name, int price, String imageUrl) {
-        if(price < 0) {
-            throw new IllegalArgumentException("Price should be positive");
-        }
-        this.name = name;
-        this.price = price;
-        this.imageUrl = imageUrl;
-    }
-
-    public ProductDTO(Product product) {
-        if(product.getPrice() < 0) {
-            throw new IllegalArgumentException("Price should be positive");
-        }
+    public ProductDto(Product product) {
         this.name = product.getName();
         this.price = product.getPrice();
         this.imageUrl = product.getImageUrl();
@@ -32,7 +22,6 @@ public class ProductDTO {
     public int getPrice() {
         return price;
     }
-
     public String getImageUrl() {
         return imageUrl;
     }
@@ -48,5 +37,4 @@ public class ProductDTO {
     public void setImageUrl(String imageUrl) {
         this.imageUrl = imageUrl;
     }
-
 }
