2cc0ba90104a8e4be434d7184555e7b2d7393d55
doorcs <itsdoorcs@gmail.com>
2025-07-01 17:20:38 +0900

    refactor: 리포지토리 리팩토링
    - 쿼리 가독성 향상을 위해 raw string 적용
    - 엔티티 객체의 id를 데이터베이스가 부여하도록 쿼리 수정
    - save 메서드가 데이터베이스에서 부여한 엔티티 id를 리턴하도록 수정
    - update 메서드 구현
    - 예외 처리를 위해 delete 메서드가 query affected row를 리턴하도록 수정

--- src/main/java/gift/repository/ProductRepository.java
+++ src/main/java/gift/repository/ProductRepository.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.simple.JdbcClient;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.stereotype.Repository;
 
 import gift.domain.Product;
@@ -29,24 +30,57 @@ public class ProductRepository {
     }
 
     public List<Product> findAll() {
-        var sql = "SELECT id, name, price, imageUrl FROM product";
+        var sql = """
+        SELECT id, name, price, imageUrl
+        FROM product
+        """;
+
         return client.sql(sql)
             .query(getMemberRowMapper())
-            .stream()
-            .toList();
+            .list();
     }
 
     public Optional<Product> findById(Long id) {
-        var sql = "SELECT id, name, price, imageUrl FROM product WHERE id = :id";
+        var sql = """
+        SELECT id, name, price, imageUrl
+        FROM product
+        WHERE id = :id
+        """;
+
         return client.sql(sql)
             .param("id", id)
             .query(getMemberRowMapper())
             .optional();
     }
 
-    public void save(Product product) {
-        var sql = "MERGE INTO product (id, name, price, imageUrl) KEY(id) VALUES (:id, :name, :price, :imageUrl)";
+    public Long save(Product product) {
+        var sql = """
+        INSERT INTO product (name, price, imageUrl)
+        VALUES (:name, :price, :imageUrl)
+        """;
+        var keyHolder = new GeneratedKeyHolder();
+
         client.sql(sql)
+            .param("name", product.getName())
+            .param("price", product.getPrice())
+            .param("imageUrl", product.getImageUrl())
+            .update(keyHolder);
+
+        if (keyHolder.getKey() == null) {
+            return -1L;
+        }
+
+        return keyHolder.getKey().longValue();
+    }
+
+    public int update(Product product) {
+        var sql = """
+        UPDATE product
+        SET name = :name, price = :price, imageUrl = :imageUrl
+        WHERE id = :id
+        """;
+
+        return client.sql(sql)
             .param("id", product.getId())
             .param("name", product.getName())
             .param("price", product.getPrice())
@@ -54,9 +88,10 @@ public class ProductRepository {
             .update();
     }
 
-    public void deleteById(Long id) {
+    public int delete(Long id) {
         var sql = "DELETE FROM product WHERE id = :id";
-        client.sql(sql)
+
+        return client.sql(sql)
             .param("id", id)
             .update();
     }
