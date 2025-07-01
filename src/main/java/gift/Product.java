f7e1ea92f1ed3878e75a1609e67c81dda516e5e8
하석현 <haseokhyeon@haseoghyeons-MacBook-Air.local>
2025-07-01 15:34:52 +0900

    lombok 삭제, keyvalue 수정

--- src/main/java/gift/Product.java
+++ src/main/java/gift/Product.java
@@ -1,14 +1,5 @@
 package gift;
 
-
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@NoArgsConstructor
-@AllArgsConstructor
-@Data
-
 public class Product {
 
     private Long id;
@@ -16,4 +7,27 @@ public class Product {
     private int price;
     private String imageUrl;
 
+    public Product(Long id, String name, int price, String imageUrl) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.imageUrl = imageUrl;
+    }
+
+    public Product() {
+
+    }
+
+    // Getters & Setters
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public int getPrice() { return price; }
+    public void setPrice(int price) { this.price = price; }
+
+    public String getImageUrl() { return imageUrl; }
+    public void setImageUrl(String imageUrl) { this.imageUrl = imageUrl; }
 }
--- src/main/java/gift/ProductStorage.java
+++ src/main/java/gift/ProductStorage.java
@@ -1,51 +1,54 @@
 package gift;
 
 import org.springframework.jdbc.core.simple.JdbcClient;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
 import java.util.*;
-import java.util.concurrent.atomic.AtomicLong;
 
 @Repository
 public class ProductStorage {
-    private final JdbcClient products;
+    private final JdbcClient jdbc;
 
-    public ProductStorage(JdbcClient products) {
-        this.products = products;
+    public ProductStorage(JdbcClient jdbcClient) {
+        this.jdbc = jdbcClient;
     }
 
     public List<Product> getProducts() {
-        return products.sql("SELECT * FROM product")
+        return jdbc.sql("SELECT * FROM product")
                 .query(Product.class).list();
     }
 
     public Optional<Product> findById(Long id) {
-        return products.sql("SELECT * FROM product WHERE id = :id")
+        return jdbc.sql("SELECT * FROM product WHERE id = :id")
                 .param("id", id)
                 .query(Product.class).optional();
     }
 
     public Product save(Product product) {
-        products.sql("INSERT INTO product (name, price, image_url) VALUES (:name, :price, :image_url)")
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+
+        jdbc.sql("INSERT INTO product (name, price, image_url) VALUES (:name, :price, :image_url)")
                 .param("name", product.getName())
                 .param("price", product.getPrice())
-                .param("image_url", product.getImageUrl()).update();
+                .param("image_url", product.getImageUrl()).update(keyHolder);
 
-        Long id = products.sql("SELECT SCOPE_IDENTITY()").query(Long.class).single();
+        product.setId(keyHolder.getKey().longValue());
 
-        product.setId(id);
         return product;
     }
 
     public void update(Long id, Product updated) {
-        products.sql("UPDATE product SET name=:name, price=:price, imageURL=:imageUrl WHERE id=:id")
+        jdbc.sql("UPDATE product SET name=:name, price=:price, image_url=:image_url WHERE id=:id")
                 .param("name", updated.getName())
                 .param("price", updated.getPrice())
-                .param("imageUrl", updated.getImageUrl())
+                .param("image_url", updated.getImageUrl())
                 .param("id", id).update();
     }
+
     public void delete(Long id) {
-        products.sql("DELETE FROM product WHERE id = :id")
+        jdbc.sql("DELETE FROM product WHERE id = :id")
                 .param("id", id).update();
     }
 }
