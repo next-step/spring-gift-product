a589e28e6f9c6ad63ffadec7eb181e0f97d9a18f
Hyun <hyun_7586@naver.com>
2025-07-01 22:27:29 +0900

    refactor: 상품 생성 로직에서 생성된 상품의 id를 json 타입으로 반환하도록 수정

--- src/main/java/gift/controller/ProductApiController.java
+++ src/main/java/gift/controller/ProductApiController.java
@@ -1,5 +1,6 @@
 package gift.controller;
 
+import gift.dto.ProductCreateResponse;
 import gift.dto.ProductRequest;
 import gift.dto.ProductResponse;
 import gift.service.ProductService;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -44,10 +46,13 @@ public class ProductApiController {
     // 상품 생성
     @PostMapping("")
     @ResponseStatus(HttpStatus.CREATED)
-    public Long createProduct(
+    @ResponseBody
+    public ProductCreateResponse createProduct(
         @RequestBody ProductRequest request
     ) {
-        return productService.save(request);
+        Long id = productService.save(request);
+
+        return new ProductCreateResponse(id);
     }
 
     // 상품 수정
