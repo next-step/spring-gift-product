36a711635e2ef7170bab4db67c46add31e36eb2f
Dohyun <sks020k@naver.com>
2025-07-01 10:55:31 +0900

    refactor : JDBCtemplate -> JDBCClient

--- src/main/java/gift/repository/ProductRepository.java
+++ src/main/java/gift/repository/ProductRepository.java
@@ -16,19 +16,18 @@ import java.util.Map;
 
 @Repository
 public class ProductRepository {
-    private final JdbcTemplate jdbcTemplate;
     private final JdbcClient jdbcClient;
 
-    public ProductRepository(JdbcTemplate jdbcTemplate, JdbcClient jdbcClient) {
-        this.jdbcTemplate = jdbcTemplate;
+    public ProductRepository(JdbcClient jdbcClient) {
         this.jdbcClient = jdbcClient;
     }
 
 
     public Product findById(Long id) {
-        return jdbcClient.sql("select id, name, price,imageUrl from products where id = ?")
+        String sql = "select id, name, price, imageUrl from products where id = ?";
+        return jdbcClient.sql(sql)
             .param(id)
-            .query((result,rowNum) ->{
+            .query((result,rowNum) -> {
                 Product product = new Product(
                     result.getLong("id"),
                     result.getString("name"),
@@ -42,25 +41,38 @@ public class ProductRepository {
 
     public Product saveProduct(String name, int price, String imageUrl) {
         String sql = "INSERT INTO products(name,price,imageUrl) VALUES(?,?,?)";
-        jdbcTemplate.update(sql, name, price, imageUrl);
+        jdbcClient.sql(sql)
+            .param(name)
+            .param(price)
+            .param(imageUrl)
+            .update();
 
-        String sqlForFind = "select id, name, price, imageUrl "
-            + "from products where name = ? AND price = ? AND imageUrl = ?";
-        return jdbcTemplate.queryForObject(
-            sqlForFind, (result,rowNum) -> {
+        String sqlForFind = "SELECT id, name, price, imageUrl FROM products"
+            + " WHERE name = ? AND price = ? AND imageUrl = ?";
+        return jdbcClient.sql(sqlForFind)
+            .param(name)
+            .param(price)
+            .param(imageUrl)
+            .query((result,rowNum) -> {
                 Product product = new Product(
                     result.getLong("id"),
                     result.getString("name"),
                     result.getInt("price"),
                     result.getString("imageUrl")
                 );
-            return product;
-            }
-        ,name,price,imageUrl);
+                return product;
+            })
+            .single();
     }
+
     public Product updateProduct(Long id, String name, int price, String imageUrl) {
         String sql = "update products set name = ?, price = ?, imageUrl = ? where id = ?";
-        int updatedRow = jdbcTemplate.update(sql, name, price, imageUrl, id);
+        int updatedRow = jdbcClient.sql(sql)
+                        .param(name)
+                        .param(price)
+                        .param(imageUrl)
+                        .param(id)
+                        .update();
         if(updatedRow == 0) {
             throw new ProductNotFoundException();
         }
@@ -69,16 +81,27 @@ public class ProductRepository {
 
     public Product updatePrice(Long id, int price) {
         String sql = "update products set price = ? where id = ?";
-        int updatedRows = jdbcTemplate.update(sql, price, id);
+        int updatedRows = jdbcClient.sql(sql)
+            .param(price)
+            .param(id)
+            .update();
+
         if(updatedRows == 0) {
             throw new ProductNotFoundException();
         }
+
         return findById(id);
     }
 
     public void deleteById(Long id) {
         String sql = "DELETE FROM products WHERE id = ?";
-        int deletedRows = jdbcTemplate.update(sql, id);
+        int deletedRows = jdbcClient.sql(sql)
+            .param(id)
+            .update();
+
+        if(deletedRows == 0) {
+            throw new ProductNotFoundException();
+        }
     }
 
     public List<Product> findAllProducts() {
