0578e24288eb66dade44ea7c6c2aedb5612ad946
doorcs <itsdoorcs@gmail.com>
2025-07-02 10:05:30 +0900

    refactor: 관리자 페이지가 올바른 HTTP 메서드를 사용하도록 수정

--- src/main/java/gift/controller/AdminController.java
+++ src/main/java/gift/controller/AdminController.java
@@ -4,10 +4,12 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import gift.dto.CreateProductRequest;
@@ -55,7 +57,7 @@ public class AdminController {
         return "editProduct";
     }
 
-    @PostMapping("/{id}")
+    @PutMapping("/{id}")
     public String updateProduct(
         @PathVariable Long id,
         @ModelAttribute UpdateProductRequest request
@@ -64,7 +66,7 @@ public class AdminController {
         return "redirect:/admin/products";
     }
 
-    @GetMapping("/{id}/delete")
+    @DeleteMapping("/{id}")
     public String deleteProduct(
         @PathVariable Long id
     ) {
